3349bc3a97ecd6e9a0d5bb49cff2babaa5b00443
Mark Wiebe <mwwiebe@gmail.com>
2011-09-21 17:03:12 -0700

    Add initializer_list support to the ndarray

--- include/dnd/ndarray.hpp
+++ include/dnd/ndarray.hpp
@@ -9,6 +9,7 @@
 
 #include <iostream> // FOR DEBUG
 #include <stdexcept>
+#include <initializer_list>
 
 #include <boost/utility/enable_if.hpp>
 
@@ -54,6 +55,23 @@ public:
     /** Constructs a three-dimensional array */
     ndarray(intptr_t dim0, intptr_t dim1, intptr_t dim2, const dtype& dt);
 
+    /**
+     * Constructs an array from an initializer list.
+     *
+     * NOTE: These constructors are rather fragile, every initializer value
+     *       has to be of the same type, otherwise the compiler thinks the
+     *       initializer list is ambiguous and fails to bind to one of these
+     *       functions.
+     */
+    template<class T>
+    ndarray(std::initializer_list<T> il);
+    /** Constructs an array from a two-level nested initializer list */
+    template<class T>
+    ndarray(std::initializer_list<std::initializer_list<T> > il);
+    /** Constructs an array from a three-level nested initializer list */
+    template<class T>
+    ndarray(std::initializer_list<std::initializer_list<std::initializer_list<T> > > il);
+
     /** Copy constructor */
     ndarray(const ndarray& rhs)
         : m_dtype(rhs.m_dtype), m_ndim(rhs.m_ndim),
@@ -91,6 +109,16 @@ public:
 
         return *this;
     }
+    /** Initializer list assignment operator */
+    template <class T>
+    ndarray& operator=(const std::initializer_list<T>& il) {
+        *this = ndarray(il);
+        return *this;
+    }
+
+    const dtype& get_dtype() const {
+        return m_dtype;
+    }
 
     int ndim() const {
         return m_ndim;
@@ -105,11 +133,11 @@ public:
     }
 
     char *data() {
-        return m_buffer->data();
+        return m_buffer->data() + m_baseoffset;
     }
 
     const char *data() const {
-        return m_buffer->data();
+        return m_buffer->data() + m_baseoffset;
     }
 
     /** Does a value-assignment from the rhs array. */
@@ -146,6 +174,142 @@ public:
     }
     
 };
+
+namespace detail {
+    // Computes the number of dimensions in a nested initializer list constructor
+    template<class T>
+    struct initializer_list_ndim {static const int value = 0;};
+    template<class T>
+    struct initializer_list_ndim<std::initializer_list<T> > {
+        static const int value = initializer_list_ndim<T>::value + 1;
+    };
+
+    // Computes the array dtype of a nested initializer list constructor
+    template<class T>
+    struct initializer_list_dtype {typedef T type;};
+    template<class T>
+    struct initializer_list_dtype<std::initializer_list<T> > {
+        typedef typename initializer_list_dtype<T>::type type;
+    };
+
+    // Gets the shape of the nested initializer list constructor, and validates that
+    // it isn't ragged
+    template <class T>
+    struct initializer_list_shape;
+    // Base case, an initializer list parameterized by a non-initializer list
+    template<class T>
+    struct initializer_list_shape<std::initializer_list<T> > {
+        static void compute(intptr_t *out_shape, const std::initializer_list<T>& il) {
+            out_shape[0] = il.size();
+        }
+        static void validate(const intptr_t *shape, const std::initializer_list<T>& il) {
+            if (il.size() != shape[0]) {
+                throw std::runtime_error("initializer list for ndarray is ragged, must be "
+                                        "nested in a regular fashion");
+            }
+        }
+        static void copy_data(T **dataptr, const std::initializer_list<T>& il) {
+            memcpy(*dataptr, il.begin(), il.size() * sizeof(T));
+            *dataptr += il.size();
+        }
+    };
+    // Recursive case, an initializer list parameterized by an initializer list
+    template<class T>
+    struct initializer_list_shape<std::initializer_list<std::initializer_list<T> > > {
+        static void compute(intptr_t *out_shape,
+                        const std::initializer_list<std::initializer_list<T> >& il) {
+            out_shape[0] = il.size();
+            if (out_shape[0] > 0) {
+                // Recursively compute the rest of the shape
+                initializer_list_shape<std::initializer_list<T> >::
+                                                compute(out_shape + 1, *il.begin());
+                // Validate the shape for the nested initializer lists
+                for (auto i = il.begin() + 1; i != il.end(); ++i) {
+                    initializer_list_shape<std::initializer_list<T> >::
+                                                        validate(out_shape + 1, *i);
+                }
+            }
+        }
+        static void validate(const intptr_t *shape,
+                        const std::initializer_list<std::initializer_list<T> >& il) {
+            if (il.size() != shape[0]) {
+                throw std::runtime_error("initializer list for ndarray is ragged, must be "
+                                        "nested in a regular fashion");
+            }
+            // Validate the shape for the nested initializer lists
+            for (auto i = il.begin(); i != il.end(); ++i) {
+                initializer_list_shape<std::initializer_list<T> >::validate(shape + 1, *i);
+            }
+        }
+        static void copy_data(typename initializer_list_dtype<T>::type **dataptr,
+                        const std::initializer_list<std::initializer_list<T> >& il) {
+            for (auto i = il.begin(); i != il.end(); ++i) {
+                initializer_list_shape<std::initializer_list<T> >::copy_data(dataptr, *i);
+            }
+        }
+    };
+} // namespace detail
+
+// Implementation of initializer list construction
+template<class T>
+dnd::ndarray::ndarray(std::initializer_list<T> il)
+    : m_dtype(type_id_of<T>::value), m_ndim(1), m_shape(1), m_strides(1), m_baseoffset(0)
+{
+    intptr_t size = il.size();
+    m_shape[0] = size;
+    m_strides[0] = (size == 1) ? 0 : sizeof(T);
+    if (size > 0) {
+        // Allocate the storage buffer and copy the data
+        m_buffer.reset(new membuffer(m_dtype, size));
+        memcpy(m_buffer->data(), il.begin(), sizeof(T)*size);
+    }
+}
+template<class T>
+dnd::ndarray::ndarray(std::initializer_list<std::initializer_list<T> > il)
+    : m_dtype(type_id_of<T>::value), m_ndim(2), m_shape(2), m_strides(2), m_baseoffset(0)
+{
+    typedef std::initializer_list<std::initializer_list<T> > S;
+
+    // Get and validate that the shape is regular
+    detail::initializer_list_shape<S>::compute(m_shape.get(), il);
+    // Compute the number of elements in the array, and the strides at the same time
+    intptr_t size = 1, stride = sizeof(T);
+    for (int i = m_ndim-1; i >= 0; --i) {
+        m_strides[i] = (m_shape[i] == 1) ? 0 : stride;
+        size *= m_shape[i];
+        stride *= m_shape[i];
+    }
+    if (size > 0) {
+        // Allocate the storage buffer
+        m_buffer.reset(new membuffer(m_dtype, size));
+        // Populate the storage buffer from the nested initializer list
+        T *dataptr = reinterpret_cast<T *>(m_buffer->data());
+        detail::initializer_list_shape<S>::copy_data(&dataptr, il);
+    }
+}
+template<class T>
+dnd::ndarray::ndarray(std::initializer_list<std::initializer_list<std::initializer_list<T> > > il)
+    : m_dtype(type_id_of<T>::value), m_ndim(3), m_shape(3), m_strides(3), m_baseoffset(0)
+{
+    typedef std::initializer_list<std::initializer_list<std::initializer_list<T> > > S;
+
+    // Get and validate that the shape is regular
+    detail::initializer_list_shape<S>::compute(m_shape.get(), il);
+    // Compute the number of elements in the array, and the strides at the same time
+    intptr_t size = 1, stride = sizeof(T);
+    for (int i = m_ndim-1; i >= 0; --i) {
+        m_strides[i] = (m_shape[i] == 1) ? 0 : stride;
+        size *= m_shape[i];
+        stride *= m_shape[i];
+    }
+    if (size > 0) {
+        // Allocate the storage buffer
+        m_buffer.reset(new membuffer(m_dtype, size));
+        // Populate the storage buffer from the nested initializer list
+        T *dataptr = reinterpret_cast<T *>(m_buffer->data());
+        detail::initializer_list_shape<S>::copy_data(&dataptr, il);
+    }
+}
 
 } // namespace dnd
 
--- tests/ndarray_test.cpp
+++ tests/ndarray_test.cpp
@@ -29,3 +29,55 @@ TEST(NDArray, AsScalar) {
     EXPECT_EQ(3.141592653589f, a.as_scalar<float>());
 }
 
+TEST(NDArray, InitializerLists) {
+    ndarray a = {1, 2, 3, 4, 5};
+    EXPECT_EQ(make_dtype<int>(), a.get_dtype());
+    EXPECT_EQ(1, a.ndim());
+    EXPECT_EQ(5, a.shape()[0]);
+    EXPECT_EQ(sizeof(int), a.strides()[0]);
+    int *ptr_i = (int *)a.data();
+    EXPECT_EQ(1, ptr_i[0]);
+    EXPECT_EQ(2, ptr_i[1]);
+    EXPECT_EQ(3, ptr_i[2]);
+    EXPECT_EQ(4, ptr_i[3]);
+    EXPECT_EQ(5, ptr_i[4]);
+
+    ndarray b = {{1., 2., 3.}, {4., 5., 6.25}};
+    EXPECT_EQ(make_dtype<double>(), b.get_dtype());
+    EXPECT_EQ(2, b.ndim());
+    EXPECT_EQ(2, b.shape()[0]);
+    EXPECT_EQ(3, b.shape()[1]);
+    EXPECT_EQ(3*sizeof(double), b.strides()[0]);
+    EXPECT_EQ(sizeof(double), b.strides()[1]);
+    double *ptr_d = (double *)b.data();
+    EXPECT_EQ(1, ptr_d[0]);
+    EXPECT_EQ(2, ptr_d[1]);
+    EXPECT_EQ(3, ptr_d[2]);
+    EXPECT_EQ(4, ptr_d[3]);
+    EXPECT_EQ(5, ptr_d[4]);
+    EXPECT_EQ(6.25, ptr_d[5]);
+
+    // Testing assignment operator with initializer list (and 3D nested list)
+    a = {{{1LL, 2LL}, {-1LL, -2LL}}, {{4LL, 5LL}, {6LL, 1LL}}};
+    EXPECT_EQ(make_dtype<long long>(), a.get_dtype());
+    EXPECT_EQ(3, a.ndim());
+    EXPECT_EQ(2, a.shape()[0]);
+    EXPECT_EQ(2, a.shape()[1]);
+    EXPECT_EQ(2, a.shape()[2]);
+    EXPECT_EQ(4*sizeof(long long), a.strides()[0]);
+    EXPECT_EQ(2*sizeof(long long), a.strides()[1]);
+    EXPECT_EQ(sizeof(long long), a.strides()[2]);
+    long long *ptr_ll = (long long *)a.data();
+    EXPECT_EQ(1, ptr_ll[0]);
+    EXPECT_EQ(2, ptr_ll[1]);
+    EXPECT_EQ(-1, ptr_ll[2]);
+    EXPECT_EQ(-2, ptr_ll[3]);
+    EXPECT_EQ(4, ptr_ll[4]);
+    EXPECT_EQ(5, ptr_ll[5]);
+    EXPECT_EQ(6, ptr_ll[6]);
+    EXPECT_EQ(1, ptr_ll[7]);
+
+    // If the shape is jagged, should throw an error
+    EXPECT_THROW((a = {{1,2,3}, {1,2}}), runtime_error);
+    EXPECT_THROW((a = {{{1},{2},{3}}, {{1},{2},{3, 4}}}), runtime_error);
+}
