5242a2cfaa16c6fc3b92cf7ac4e9d11cb24933e3
Mark Wiebe <mwwiebe@gmail.com>
2012-06-18 16:30:46 -0500

    Add some structure for unary operation specialization via lookup table

--- include/dnd/kernels/unary_kernel_instance.hpp
+++ include/dnd/kernels/unary_kernel_instance.hpp
@@ -6,21 +6,9 @@
 #ifndef _DND__KERNEL_INSTANCE_HPP_
 #define _DND__KERNEL_INSTANCE_HPP_
 
-#include <dnd/auxiliary_data.hpp>
+#include <dnd/kernels/kernel_instance.hpp>
 
 namespace dnd {
-
-typedef void (*nullary_operation_t)(char *dst, intptr_t dst_stride,
-                        intptr_t count, const AuxDataBase *auxdata);
-
-typedef void (*unary_operation_t)(char *dst, intptr_t dst_stride,
-                        const char *src0, intptr_t src0_stride,
-                        intptr_t count, const AuxDataBase *auxdata);
-
-typedef void (*binary_operation_t)(char *dst, intptr_t dst_stride,
-                        const char *src0, intptr_t src0_stride,
-                        const char *src1, intptr_t src1_stride,
-                        intptr_t count, const AuxDataBase *auxdata);
 
 /**
  * Unary operations support four specializations as a standardized
@@ -56,33 +44,28 @@ inline unary_specialization_t get_unary_specialization(intptr_t dst_stride, intp
 }
 
 /**
- * This class holds an instance of a kernel function, with its
- * associated auxiliary data. The object is non-copyable, just
- * like the auxiliary_data object, to avoid inefficient copies.
+ * This struct holds a pointer to a specialized_unary_operation_table_t
+ * array, as well as auxiliary data for the kernels.
  */
-template<typename FT>
-class kernel_instance {
-    kernel_instance& operator=(const kernel_instance&);
-public:
-    kernel_instance()
-        : kernel(0)
-    {
-    }
-    // Copying a kernel_instance clones the auxiliary data
-    kernel_instance(const kernel_instance& rhs)
-        : kernel(rhs.kernel)
-    {
-        rhs.auxdata.clone_into(auxdata);
-    }
-
-    void swap(kernel_instance& rhs) {
-        std::swap(kernel, rhs.kernel);
-        auxdata.swap(rhs.auxdata);
-    }
-
-    FT kernel;
+struct unary_specialization_kernel_instance {
+    unary_operation_t *unary_specializations;
     auxiliary_data auxdata;
+
+    /**
+     * This grabs the requested unary operation specialization, and
+     * borrows the auxiliary data into the output kernel.
+     *
+     * IMPORTANT: Because the auxiliary data is borrowed, the kernel
+     *            instance this writes to should have a shorter lifetime
+     *            than this unary_specialization_kernel_instance.
+     */
+    void borrow_specialization(unary_specialization_t specialization, kernel_instance<unary_operation_t>& out_kernel)
+    {
+        out_kernel.kernel = unary_specializations[specialization];
+        auxdata.borrow_into(out_kernel.auxdata);
+    }
 };
+
 
 } // namespace dnd;
 
