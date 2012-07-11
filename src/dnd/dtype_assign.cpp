f3643f5feac0901709613ba4b4fdf25675538b5e
Mark Wiebe <mwwiebe@gmail.com>
2012-07-10 22:02:34 -0700

    Miscellaneous code cleanups

--- src/dnd/dtype_assign.cpp
+++ src/dnd/dtype_assign.cpp
@@ -4,7 +4,6 @@
 //
 
 #include <iostream> // FOR DEBUG
-#include <typeinfo> // FOR DEBUG
 
 #include <sstream>
 #include <stdexcept>
@@ -16,37 +15,27 @@
 #include <dnd/kernels/assignment_kernels.hpp>
 #include <dnd/diagnostics.hpp>
 
-#ifdef __GNUC__
-// The -Weffc++ flag warns about derived classes not having a virtual destructor.
-// Here, this is explicitly done, because we are only using derived classes
-// to inherit a static function, they are never instantiated.
-//
-// NOTE: The documentation says this is only for g++ 4.6.0 and up.
-#pragma GCC diagnostic ignored "-Weffc++"
-#endif
-
-
 using namespace std;
 using namespace dnd;
 
 std::ostream& dnd::operator<<(ostream& o, assign_error_mode errmode)
 {
     switch (errmode) {
-    case assign_error_none:
-        o << "none";
-        break;
-    case assign_error_overflow:
-        o << "overflow";
-        break;
-    case assign_error_fractional:
-        o << "fractional";
-        break;
-    case assign_error_inexact:
-        o << "inexact";
-        break;
-    default:
-        o << "invalid error mode(" << (int)errmode << ")";
-        break;
+        case assign_error_none:
+            o << "none";
+            break;
+        case assign_error_overflow:
+            o << "overflow";
+            break;
+        case assign_error_fractional:
+            o << "fractional";
+            break;
+        case assign_error_inexact:
+            o << "inexact";
+            break;
+        default:
+            o << "invalid error mode(" << (int)errmode << ")";
+            break;
     }
 
     return o;
@@ -75,8 +64,6 @@ bool dnd::is_lossless_assignment(const dtype& dst_dt, const dtype& src_dt)
                     case real_kind:
                     case complex_kind:
                         return true;
-                    case string_kind:
-                        return dst_dt.element_size() > 0;
                     case bytes_kind:
                         return false;
                     default:
@@ -95,10 +82,6 @@ bool dnd::is_lossless_assignment(const dtype& dst_dt, const dtype& src_dt)
                         return dst_dt.element_size() > src_dt.element_size();
                     case complex_kind:
                         return dst_dt.element_size() > 2 * src_dt.element_size();
-                    case string_kind:
-                        // Conservative value for 64-bit, could
-                        // check speciifically based on the type_id.
-                        return dst_dt.element_size() >= 21;
                     case bytes_kind:
                         return false;
                     default:
@@ -117,10 +100,6 @@ bool dnd::is_lossless_assignment(const dtype& dst_dt, const dtype& src_dt)
                         return dst_dt.element_size() > src_dt.element_size();
                     case complex_kind:
                         return dst_dt.element_size() > 2 * src_dt.element_size();
-                    case string_kind:
-                        // Conservative value for 64-bit, could
-                        // check speciifically based on the type_id.
-                        return dst_dt.element_size() >= 21;
                     case bytes_kind:
                         return false;
                     default:
@@ -137,8 +116,6 @@ bool dnd::is_lossless_assignment(const dtype& dst_dt, const dtype& src_dt)
                         return dst_dt.element_size() >= src_dt.element_size();
                     case complex_kind:
                         return dst_dt.element_size() >= 2 * src_dt.element_size();
-                    case string_kind:
-                        return dst_dt.element_size() >= 32;
                     case bytes_kind:
                         return false;
                     default:
@@ -153,8 +130,6 @@ bool dnd::is_lossless_assignment(const dtype& dst_dt, const dtype& src_dt)
                         return false;
                     case complex_kind:
                         return dst_dt.element_size() >= src_dt.element_size();
-                    case string_kind:
-                        return dst_dt.element_size() >= 64;
                     case bytes_kind:
                         return false;
                     default:
@@ -168,9 +143,6 @@ bool dnd::is_lossless_assignment(const dtype& dst_dt, const dtype& src_dt)
                     case real_kind:
                     case complex_kind:
                         return false;
-                    case string_kind:
-                        return src_dt.type_id() == dst_dt.type_id() &&
-                                dst_dt.element_size() >= src_dt.element_size();
                     case bytes_kind:
                         return false;
                     default:
@@ -201,6 +173,10 @@ void dnd::dtype_assign(const dtype& dst_dt, char *dst, const dtype& src_dt, cons
 {
     DND_ASSERT_ALIGNED(dst, 0, dst_dt.alignment(), "dst dtype: " << dst_dt << ", src dtype: " << src_dt);
     DND_ASSERT_ALIGNED(src, 0, src_dt.alignment(), "src dtype: " << src_dt << ", dst dtype: " << dst_dt);
+    if (dst_dt.get_memory_management() != pod_memory_management) {
+        throw runtime_error("dtype_assign can only be used with POD destination memory");
+    }
+
     if (dst_dt.extended() == NULL && src_dt.extended() == NULL) {
         // Try to use the simple single-value assignment for built-in types
         assignment_function_t asn = get_builtin_dtype_assignment_function(dst_dt.type_id(), src_dt.type_id(), errmode);
@@ -221,45 +197,14 @@ void dnd::dtype_assign(const dtype& dst_dt, char *dst, const dtype& src_dt, cons
     }
 }
 
-/*
-
-// A multiple unaligned assignment function which uses one of the single assignment functions as proxy
-namespace {
-    struct multiple_unaligned_auxiliary_data {
-        assign_function_t assign;
-        int dst_element_size, src_element_size;
-    };
-}
-static void assign_multiple_unaligned(char *dst, intptr_t dst_stride, const char *src, intptr_t src_stride,
-                                    intptr_t count, const AuxDataBase *auxdata)
-{
-    const multiple_unaligned_auxiliary_data &mgdata = get_auxiliary_data<multiple_unaligned_auxiliary_data>(auxdata);
-
-    char *dst_cached = reinterpret_cast<char *>(dst);
-    const char *src_cached = reinterpret_cast<const char *>(src);
-
-    int dst_element_size = mgdata.dst_element_size, src_element_size = mgdata.src_element_size;
-    // TODO: Probably want to relax the assumption of at most 8 bytes
-    int64_t d;
-    int64_t s;
-
-    assign_function_t asn = mgdata.assign;
-
-    for (intptr_t i = 0; i < count; ++i) {
-        memcpy(&s, src_cached, src_element_size);
-        asn(&d, &s);
-        memcpy(dst_cached, &d, dst_element_size);
-        dst_cached += dst_stride;
-        src_cached += src_stride;
-    }
-}
-*/
-
-
 void dnd::dtype_strided_assign(const dtype& dst_dt, char *dst, intptr_t dst_stride,
                             const dtype& src_dt, const char *src, intptr_t src_stride,
                             intptr_t count, assign_error_mode errmode)
 {
+    if (dst_dt.get_memory_management() != pod_memory_management) {
+        throw runtime_error("dtype_strided_assign can only be used with POD destination memory");
+    }
+
     unary_specialization_kernel_instance op;
     get_dtype_assignment_kernel(dst_dt, src_dt,
                                 errmode, op);
