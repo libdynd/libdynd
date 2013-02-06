7d4f92b03198443524ffd307f7c4ac9f64411121
Mark Wiebe <mwwiebe@gmail.com>
2013-02-06 14:04:45 -0800

    Add void pointer hierarchical assignment kernel

--- src/dynd/dtypes/void_pointer_dtype.cpp
+++ src/dynd/dtypes/void_pointer_dtype.cpp
@@ -12,38 +12,63 @@
 using namespace std;
 using namespace dynd;
 
-void dynd::void_pointer_dtype::print_data(std::ostream& o, const char *DYND_UNUSED(metadata), const char *data) const
+void void_pointer_dtype::print_data(std::ostream& o, const char *DYND_UNUSED(metadata), const char *data) const
 {
     uintptr_t target_ptr = *reinterpret_cast<const uintptr_t *>(data);
     o << "0x";
     hexadecimal_print(o, target_ptr);
 }
 
-void dynd::void_pointer_dtype::print_dtype(std::ostream& o) const {
+void void_pointer_dtype::print_dtype(std::ostream& o) const {
 
     o << "pointer<void>";
 }
 
-void dynd::void_pointer_dtype::get_shape(size_t DYND_UNUSED(i),
+void void_pointer_dtype::get_shape(size_t DYND_UNUSED(i),
                 intptr_t *DYND_UNUSED(out_shape)) const
 {
 }
 
-void dynd::void_pointer_dtype::get_shape(size_t DYND_UNUSED(i),
+void void_pointer_dtype::get_shape(size_t DYND_UNUSED(i),
                 intptr_t *DYND_UNUSED(out_shape), const char *DYND_UNUSED(metadata)) const
 {
 }
 
-bool dynd::void_pointer_dtype::is_lossless_assignment(const dtype& DYND_UNUSED(dst_dt), const dtype& DYND_UNUSED(src_dt)) const
+bool void_pointer_dtype::is_lossless_assignment(const dtype& DYND_UNUSED(dst_dt), const dtype& DYND_UNUSED(src_dt)) const
 {
     return false;
 }
 
-void dynd::void_pointer_dtype::get_single_compare_kernel(kernel_instance<compare_operations_t>& DYND_UNUSED(out_kernel)) const {
+void void_pointer_dtype::get_single_compare_kernel(kernel_instance<compare_operations_t>& DYND_UNUSED(out_kernel)) const {
     throw std::runtime_error("void_pointer_dtype::get_single_compare_kernel not supported yet");
 }
 
-bool dynd::void_pointer_dtype::operator==(const base_dtype& rhs) const
+bool void_pointer_dtype::operator==(const base_dtype& rhs) const
 {
     return rhs.get_type_id() == void_pointer_type_id;
+}
+
+size_t void_pointer_dtype::make_assignment_kernel(
+                hierarchical_kernel<unary_single_operation_t> *out,
+                size_t offset_out,
+                const dtype& dst_dt, const char *dst_metadata,
+                const dtype& src_dt, const char *src_metadata,
+                assign_error_mode errmode,
+                const eval::eval_context *ectx) const
+{
+    if (this == dst_dt.extended()) {
+        if (src_dt.get_type_id() == void_pointer_type_id) {
+            return ::make_pod_dtype_assignment_kernel(out, offset_out,
+                    get_data_size(), get_alignment());
+        } else if (!src_dt.is_builtin()) {
+            src_dt.extended()->make_assignment_kernel(out, offset_out,
+                            dst_dt, dst_metadata,
+                            src_dt, src_metadata,
+                            errmode, ectx);
+        }
+    }
+
+    stringstream ss;
+    ss << "Cannot assign from " << src_dt << " to " << dst_dt;
+    throw runtime_error(ss.str());
 }
