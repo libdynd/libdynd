22a039021edc9b7ddd53999acf1c02d8ce1703bd
Mark Wiebe <mwwiebe@gmail.com>
2013-05-17 11:10:49 -0700

    Fix mixed up test

--- tests/ndobject/test_ndobject_cast.cpp
+++ tests/ndobject/test_ndobject_cast.cpp
@@ -16,5 +16,5 @@ using namespace dynd;
 
 TEST(NDObjectCast, BasicCast) {
     ndobject n = 3.5;
-    EXPECT_EQ(n.cast(3.5f, make_dtype<float>()).as<float>())
+    EXPECT_EQ(3.5f, n.cast(make_dtype<float>()).as<float>());
 }
