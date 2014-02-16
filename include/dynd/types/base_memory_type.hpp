eea58ed4d2eecaed22c6622101e6c0956c5dc2db
Irwin <hi@irwinzaid.com>
2014-02-16 20:02:59 +0000

    More for base_memory_type

---
+++ include/dynd/types/base_memory_type.hpp
@@ -0,0 +1,32 @@
+//
+// Copyright (C) 2011-14 Irwin Zaid, Mark Wiebe, DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#ifndef _DYND__BASE_MEMORY_TYPE_HPP_
+#define _DYND__BASE_MEMORY_TYPE_HPP_
+
+#include <dynd/types/base_type.hpp>
+#include <dynd/type.hpp>
+
+namespace dynd {
+
+/**
+ * Base class for all types of memory_kind.
+ */
+class base_memory_type : public base_type {
+protected:
+    ndt::type m_target_tp;
+public:
+    inline base_memory_type(type_id_t type_id, type_kind_t kind, size_t data_size,
+            size_t alignment, flags_type flags, size_t metadata_size, size_t undim = 0)
+        : base_type(type_id, kind, data_size, alignment, flags, metadata_size, undim)
+    {
+    }
+
+    virtual ~base_memory_type();
+};
+
+} // namespace dynd
+
+#endif // _DYND__BASE_MEMORY_TYPE_HPP_
---
+++ src/dynd/types/base_memory_type.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright (C) 2011-14 Irwin Zaid, Mark Wiebe, DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <dynd/types/base_memory_type.hpp>
+
+using namespace std;
+using namespace dynd;
+
+base_memory_type::~base_memory_type()
+{
+}
