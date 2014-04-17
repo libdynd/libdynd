ed60f30a85d14a906b34a33d7f403b3e5be56fe8
Mark Wiebe <mwiebe@continuum.io>
2014-04-17 00:48:11 -0700

    Try something different for the CUDA isfinite workaround

--- include/dynd/cuda_config.hpp
+++ include/dynd/cuda_config.hpp
@@ -34,7 +34,8 @@ namespace dynd {
 // Prevent isfinite from nvcc clashing with isfinite from cmath
 template <typename T>
 inline bool isfinite(T arg) {
-    return ::isfinite(arg);
+    using namespace std;
+    return (isfinite)(arg);
 }
 } // namespace dynd
 
