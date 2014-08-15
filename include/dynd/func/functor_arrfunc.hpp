36b7fcd5c1f32349c0d4be09ed68e572a9d5cfa7
Mark Wiebe <mwiebe@continuum.io>
2014-08-15 14:57:54 -0700

    Massage the make_functor_arrfunc macro metaprogramming code
    Move complicated bits into individual macros, so the macros
    defining the main code don't have complicated macro
    metaprogramming in them.
    
    Make the functor type a template parameter, to be able to
    reuse the code across multiple function object types.

--- include/dynd/func/functor_arrfunc.hpp
+++ include/dynd/func/functor_arrfunc.hpp
@@ -14,58 +14,157 @@
 
 // TODO: Move these into the PP library
 
-// Generates "NAME0, NAME1, ..., NAME`N-1`"
+/**
+ * Generates comma-separated names typically used for arguments.
+ *
+ * NAME0, NAME1, ...
+ */
 #define DYND_PP_ARG_RANGE_1(NAME, N)                                           \
-  DYND_PP_JOIN_MAP_1(DYND_PP_ID, (, ), DYND_PP_META_NAME_RANGE(NAME, N))
+  DYND_PP_JOIN_1((, ), DYND_PP_META_NAME_RANGE(NAME, N))
 
-// Generates "static_assert(TYPE_FUNC<NAME#>::value, MSG);" For # from 0 to N-1
-#define DYND_PP_STATIC_ASSERT_RANGE_1(MSG, TYPE_FUNC, NAME, N)                 \
-  DYND_PP_JOIN_OUTER_1(DYND_PP_META_STATIC_ASSERT, (;),                        \
-                       \
-DYND_PP_OUTER_1(DYND_PP_META_TEMPLATE_INSTANTIATION_SCOPE, (TYPE_FUNC),        \
-                       DYND_PP_META_NAME_RANGE(NAME, N), (value)),             \
-                       (MSG));
+/**
+ * Applies a template metafunction to a range of named types.
+ *
+ * (TMPFUNC<NAME0>::value, TMPFUNC<NAME1>::value, ...)
+ */
+#define DYND_PP_APPLY_TMPFUNC_RANGE_1(TMPFUNC, NAME, N)                        \
+  DYND_PP_ELWISE_1(DYND_PP_META_TEMPLATE_INSTANTIATION_SCOPE,                  \
+                   DYND_PP_REPEAT(TMPFUNC, N),                                 \
+                   DYND_PP_META_NAME_RANGE(NAME, N), DYND_PP_REPEAT(value, N))
 
-// Generates "*reinterpret_cast<const TYPE0 *>(NAME0), ..."
-#define DYND_PP_DEREF_ARG_RANGE_1(TYPE, NAME, N)                               \
+/**
+ * Greates a range of named types which are cleaned versions of an input
+ * range of named types.
+ *
+ * typedef remove_const<remove_reference<TYPE0>::type>::type CLEANED_TYPE0;
+ * typedef remove_const<remove_reference<TYPE1>::type>::type CLEANED_TYPE1;
+ * ...
+ */
+#define DYND_PP_CLEAN_TYPE_RANGE_1(CLEANED_TYPE, TYPE, N)                      \
+  DYND_PP_JOIN_ELWISE_1(                                                       \
+      DYND_PP_META_TYPEDEF_TYPENAME, (;),                                      \
+      DYND_PP_ELWISE_1(                                                        \
+          DYND_PP_META_TEMPLATE_INSTANTIATION_SCOPE,                           \
+          DYND_PP_REPEAT(remove_const, N),                                     \
+          DYND_PP_ELWISE_1(DYND_PP_META_TYPENAME_TEMPLATE_INSTANTIATION_SCOPE, \
+                           DYND_PP_REPEAT(remove_reference, N),                \
+                           DYND_PP_META_NAME_RANGE(TYPE, N),                   \
+                           DYND_PP_REPEAT(type, N)),                           \
+          DYND_PP_REPEAT(type, N)),                                            \
+      DYND_PP_META_NAME_RANGE(CLEANED_TYPE, N));
+
+/**
+ * Static_assert that a template metafunction returns true for each type in a
+ * range of named types.
+ *
+ * static_assert(TMPFUNC<NAME0>::value, MSG);
+ * static_assert(TMPFUNC<NAME1>::value, MSG);
+ * ...
+ */
+#define DYND_PP_STATIC_ASSERT_RANGE_1(MSG, TMPFUNC, NAME, N)                   \
+  DYND_PP_JOIN_ELWISE_1(DYND_PP_META_STATIC_ASSERT, (;),                       \
+                        DYND_PP_APPLY_TMPFUNC_RANGE_1(TMPFUNC, NAME, N),       \
+                        DYND_PP_REPEAT(MSG, N));
+
+/**
+ * For generating ckernel function calls, casts each ``ARG_NAME[#]`` input to
+ * the type ``TYPE#`` and dereferences it, output with a comma separator.
+ *
+ * *reinterpret_cast<const TYPE0 *>(ARG_NAME[0]), ...",
+ * *reinterpret_cast<const TYPE1 *>(ARG_NAME[1]), ...",
+ * ...
+ */
+#define DYND_PP_DEREF_CAST_ARRAY_RANGE_1(TYPE, ARG_NAME, N)                    \
   DYND_PP_JOIN_MAP_1(                                                          \
       DYND_PP_META_DEREFERENCE, (, ),                                          \
       DYND_PP_ELWISE_1(DYND_PP_META_REINTERPRET_CAST,                          \
                        DYND_PP_MAP_1(DYND_PP_META_MAKE_CONST_PTR,              \
                                      DYND_PP_META_NAME_RANGE(TYPE, N)),        \
-                       DYND_PP_META_AT_RANGE(NAME, N)))
+                       DYND_PP_META_AT_RANGE(ARG_NAME, N)))
+
+/**
+ * For generating ckernel function calls, casts each ``ARG_NAME#`` input to
+ * the type ``TYPE#`` and dereferences it, output with a comma separator.
+ *
+ * *reinterpret_cast<const TYPE0 *>(ARG_NAME0), ...",
+ * *reinterpret_cast<const TYPE1 *>(ARG_NAME1), ...",
+ * ...
+ */
+#define DYND_PP_DEREF_CAST_ARG_RANGE_1(TYPE, ARG_NAME, N)                      \
+  DYND_PP_JOIN_MAP_1(                                                          \
+      DYND_PP_META_DEREFERENCE, (, ),                                          \
+      DYND_PP_ELWISE_1(DYND_PP_META_REINTERPRET_CAST,                          \
+                       DYND_PP_MAP_1(DYND_PP_META_MAKE_CONST_PTR,              \
+                                     DYND_PP_META_NAME_RANGE(TYPE, N)),        \
+                       DYND_PP_META_NAME_RANGE(ARG_NAME, N)))
+
+/**
+ * Declares a range of ``TYPE`` variables ``ARG_NAME#`` and
+ * initializes them to ``ARG_NAME[#]``.
+ *
+ * const char *ARG_NAME0 = ARG_NAME[0];
+ * const char *ARG_NAME1 = ARG_NAME[1];
+ * ...
+ */
+#define DYND_PP_VARIABLE_RANGE_FROM_ARRAY_1(TYPE, ARG_NAME, N)                 \
+  DYND_PP_JOIN_ELWISE_1(DYND_PP_META_DECL_ASGN, (;),                           \
+                        DYND_PP_REPEAT_1(TYPE, N),                             \
+                        DYND_PP_META_NAME_RANGE(ARG_NAME, N),                  \
+                        DYND_PP_META_AT_RANGE(ARG_NAME, N));
+
+/**
+ * Initializes ``src#`` and ``src_stride#`` variables from the ``src`` and
+ * ``src_stride`` input arrays.
+ *
+ * const char *src0 = src[0];
+ * const char *src1 = src[1];
+ * ...
+ * intptr_t src_stride0 = src_stride[0];
+ * intptr_t src_stride1 = src_stride[1];
+ * ...
+ *
+ */
+#define DYND_PP_INIT_SRC_VARIABLES(N)                                          \
+  DYND_PP_VARIABLE_RANGE_FROM_ARRAY_1(const char *, src, N);                   \
+  DYND_PP_VARIABLE_RANGE_FROM_ARRAY_1(intptr_t, src_stride, N);
+
+/**
+ * Increment ``dst``, ``src0``, etc. with ``dst_stride``, ``src_stride0``, etc.
+ *
+ * dst += dst_stride;
+ * src0 += src_stride0;
+ * src1 += src_stride1;
+ */
+#define DYND_PP_STRIDED_INCREMENT(N)                                           \
+  dst += dst_stride;                                                           \
+  DYND_PP_JOIN_ELWISE_1(DYND_PP_META_ADD_ASGN, (;),                            \
+                        DYND_PP_META_NAME_RANGE(src, N),                       \
+                        DYND_PP_META_NAME_RANGE(src_stride, N));
 
 namespace dynd { namespace nd {
 
 namespace detail {
 
-  template<typename T>
+  template<typename Functor, typename FuncProto>
   struct functor_ckernel_instantiator;
 
 #define DYND_CODE(NSRC)                                                        \
-  template <typename R, DYND_PP_ARG_RANGE_1(typename A, NSRC)>                 \
-  struct functor_ckernel_instantiator<R (*)(DYND_PP_ARG_RANGE_1(A, NSRC))> {   \
+  template <typename Functor, typename R,                                      \
+            DYND_PP_ARG_RANGE_1(typename A, NSRC)>                             \
+  struct functor_ckernel_instantiator<Functor,                                 \
+                                      R (*)(DYND_PP_ARG_RANGE_1(A, NSRC))> {   \
     typedef functor_ckernel_instantiator self_type;                            \
                                                                                \
-    typedef R (*func_type)(DYND_PP_ARG_RANGE_1(A, NSRC));                      \
-    DYND_PP_JOIN_ELWISE_1(                                                     \
-        DYND_PP_META_TYPEDEF_TYPENAME, (;),                                    \
-        DYND_PP_OUTER_1(                                                       \
-            DYND_PP_META_TEMPLATE_INSTANTIATION_SCOPE, (remove_const),         \
-            DYND_PP_OUTER_1(                                                   \
-                DYND_PP_META_TYPENAME_TEMPLATE_INSTANTIATION_SCOPE,            \
-                (remove_reference), DYND_PP_META_NAME_RANGE(A, NSRC), (type)), \
-            (type)),                                                           \
-        DYND_PP_META_NAME_RANGE(D, NSRC));                                     \
+    DYND_PP_CLEAN_TYPE_RANGE_1(D, A, NSRC);                                    \
                                                                                \
     ckernel_prefix base;                                                       \
-    func_type func;                                                            \
+    Functor func;                                                              \
                                                                                \
     static void single(char *dst, const char *const *src, ckernel_prefix *ckp) \
     {                                                                          \
       self_type *e = reinterpret_cast<self_type *>(ckp);                       \
       *reinterpret_cast<R *>(dst) =                                            \
-          e->func(DYND_PP_DEREF_ARG_RANGE_1(D, src, NSRC));                    \
+          e->func(DYND_PP_DEREF_CAST_ARRAY_RANGE_1(D, src, NSRC));             \
     }                                                                          \
                                                                                \
     static void strided(char *dst, intptr_t dst_stride,                        \
@@ -73,27 +172,17 @@ namespace detail {
                         size_t count, ckernel_prefix *ckp)                     \
     {                                                                          \
       self_type *e = reinterpret_cast<self_type *>(ckp);                       \
-      func_type func = e->func;                                                \
+      Functor func = e->func;                                                  \
       /* const char *src# = src[#]; */                                         \
-      DYND_PP_JOIN_ELWISE_1(DYND_PP_META_DECL_ASGN, (;),                       \
-                            DYND_PP_REPEAT_1(const char *, NSRC),              \
-                            DYND_PP_META_NAME_RANGE(src, NSRC),                \
-                            DYND_PP_META_AT_RANGE(src, NSRC));                 \
       /* intptr_t src_stride# = src_stride[#]; */                              \
-      DYND_PP_JOIN_ELWISE_1(DYND_PP_META_DECL_ASGN, (;),                       \
-                            DYND_PP_REPEAT_1(intptr_t, NSRC),                  \
-                            DYND_PP_META_NAME_RANGE(src_stride, NSRC),         \
-                            DYND_PP_META_AT_RANGE(src_stride, NSRC));          \
+      DYND_PP_INIT_SRC_VARIABLES(NSRC);                                        \
       for (size_t i = 0; i < count; ++i) {                                     \
         /* *(R *)dst = func(*(const D0 *)src0, ...); */                        \
         *reinterpret_cast<R *>(dst) =                                          \
-            func(DYND_PP_DEREF_ARG_RANGE_1(D, src, NSRC));                     \
+            func(DYND_PP_DEREF_CAST_ARG_RANGE_1(D, src, NSRC));                \
                                                                                \
-        dst += dst_stride;                                                     \
-        /* src# += src_stride# */                                              \
-        DYND_PP_JOIN_ELWISE_1(DYND_PP_META_ADD_ASGN, (;),                      \
-                              DYND_PP_META_NAME_RANGE(src, NSRC),              \
-                              DYND_PP_META_NAME_RANGE(src_stride, NSRC));      \
+        /* Increment ``dst``, ``src#`` by their respective strides */          \
+        DYND_PP_STRIDED_INCREMENT(NSRC);                                       \
       }                                                                        \
     }                                                                          \
                                                                                \
@@ -122,7 +211,7 @@ namespace detail {
       }                                                                        \
       self_type *e = ckb->alloc_ck_leaf<self_type>(ckb_offset);                \
       e->base.template set_expr_function<self_type>(kernreq);                  \
-      e->func = *af_self->get_data_as<func_type>();                            \
+      e->func = *af_self->get_data_as<Functor>();                              \
                                                                                \
       return ckb_offset;                                                       \
     }                                                                          \
@@ -150,15 +239,7 @@ DYND_PP_JOIN_MAP(DYND_CODE, (), DYND_PP_RANGE(1, DYND_PP_INC(DYND_ELWISE_MAX)))
     DYND_PP_STATIC_ASSERT_RANGE_1("all reference arguments must be const",     \
                                   detail::is_suitable_input, A, NSRC)          \
     typedef R (*func_type)(DYND_PP_ARG_RANGE_1(A, NSRC));                      \
-    DYND_PP_JOIN_ELWISE_1(                                                     \
-        DYND_PP_META_TYPEDEF_TYPENAME, (;),                                    \
-        DYND_PP_OUTER_1(                                                       \
-            DYND_PP_META_TEMPLATE_INSTANTIATION_SCOPE, (remove_const),         \
-            DYND_PP_OUTER_1(                                                   \
-                DYND_PP_META_TYPENAME_TEMPLATE_INSTANTIATION_SCOPE,            \
-                (remove_reference), DYND_PP_META_NAME_RANGE(A, NSRC), (type)), \
-            (type)),                                                           \
-        DYND_PP_META_NAME_RANGE(D, NSRC));                                     \
+    DYND_PP_CLEAN_TYPE_RANGE_1(D, A, NSRC);                                    \
                                                                                \
     ndt::type dst_tp = ndt::cfixed_dim_from_array<R>::make();                  \
     ndt::type src_tp[NSRC] = {DYND_PP_JOIN_ELWISE_1(                           \
@@ -170,8 +251,8 @@ DYND_PP_JOIN_MAP(DYND_CODE, (), DYND_PP_RANGE(1, DYND_PP_INC(DYND_ELWISE_MAX)))
                                                                                \
     out_af->func_proto = ndt::make_funcproto(src_tp, dst_tp);                  \
     *out_af->get_data_as<func_type>() = func;                                  \
-    out_af->instantiate =                                                      \
-        &detail::functor_ckernel_instantiator<func_type>::instantiate;         \
+    out_af->instantiate = &detail::functor_ckernel_instantiator<               \
+                              func_type, func_type>::instantiate;              \
     out_af->free_func = NULL;                                                  \
   }
 
