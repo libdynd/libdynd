0a87e84995d7acb6b409ef4ad5caec1b8cda611e
Mark Wiebe <mwiebe@continuum.io>
2014-08-15 15:33:38 -0700

    Put the creation of ndt::type from the functor types in a macro

--- include/dynd/func/functor_arrfunc.hpp
+++ include/dynd/func/functor_arrfunc.hpp
@@ -47,7 +47,7 @@
           DYND_PP_META_TEMPLATE_INSTANTIATION_SCOPE,                           \
           DYND_PP_REPEAT(remove_const, N),                                     \
           DYND_PP_ELWISE_1(DYND_PP_META_TYPENAME_TEMPLATE_INSTANTIATION_SCOPE, \
-                           DYND_PP_REPEAT(remove_reference, N),                \
+                           DYND_PP_REPEAT(typename remove_reference, N),       \
                            DYND_PP_META_NAME_RANGE(TYPE, N),                   \
                            DYND_PP_REPEAT(type, N)),                           \
           DYND_PP_REPEAT(type, N)),                                            \
@@ -140,6 +140,20 @@
   DYND_PP_JOIN_ELWISE_1(DYND_PP_META_ADD_ASGN, (;),                            \
                         DYND_PP_META_NAME_RANGE(src, N),                       \
                         DYND_PP_META_NAME_RANGE(src_stride, N));
+
+/**
+ * Create ``dst_tp`` and ``src_tp[N]``, etc. from the types DST_TYPE, SRC_TYPE0,
+ * etc.
+ *
+ */
+#define DYND_PP_NDT_TYPES_FROM_TYPES(DST_TYPE, SRC_TYPE, N)                    \
+  ndt::type dst_tp = ndt::cfixed_dim_from_array<DST_TYPE>::make();             \
+  ndt::type src_tp[N] = {DYND_PP_JOIN_ELWISE_1(                                \
+      DYND_PP_META_SCOPE_CALL, (, ),                                           \
+      DYND_PP_ELWISE_1(DYND_PP_META_TEMPLATE_INSTANTIATION,                    \
+                       DYND_PP_REPEAT_1(ndt::cfixed_dim_from_array, N),        \
+                       DYND_PP_META_NAME_RANGE(SRC_TYPE, N)),                  \
+      DYND_PP_REPEAT_1(make, N))};
 
 namespace dynd { namespace nd {
 
@@ -322,15 +336,11 @@ DYND_PP_JOIN_MAP(DYND_CODE, (), DYND_PP_RANGE(1, DYND_PP_INC(DYND_ELWISE_MAX)))
     {                                                                          \
       DYND_PP_STATIC_ASSERT_RANGE_1("all reference arguments must be const",   \
                                     detail::is_suitable_input, A, NSRC)        \
+      /* Create D0, D1, ... as cleaned version of A0, A1, ... */               \
       DYND_PP_CLEAN_TYPE_RANGE_1(D, A, NSRC);                                  \
                                                                                \
-      ndt::type dst_tp = ndt::cfixed_dim_from_array<R>::make();                \
-      ndt::type src_tp[NSRC] = {DYND_PP_JOIN_ELWISE_1(                         \
-          DYND_PP_META_SCOPE_CALL, (, ),                                       \
-          DYND_PP_OUTER(DYND_PP_META_TEMPLATE_INSTANTIATION,                   \
-                        (ndt::cfixed_dim_from_array),                          \
-                        DYND_PP_META_NAME_RANGE(D, NSRC)),                     \
-          DYND_PP_REPEAT(make, NSRC))};                                        \
+      /* Create dst_tp and the src_tp array from R and D0, D1, ... */          \
+      DYND_PP_NDT_TYPES_FROM_TYPES(R, D, NSRC);                                \
                                                                                \
       out_af->func_proto = ndt::make_funcproto(src_tp, dst_tp);                \
       *out_af->get_data_as<func_type>() = func;                                \
@@ -355,15 +365,11 @@ DYND_PP_JOIN_MAP(DYND_CODE, (), DYND_PP_RANGE(1, DYND_PP_INC(DYND_ELWISE_MAX)))
     {                                                                          \
       DYND_PP_STATIC_ASSERT_RANGE_1("all reference arguments must be const",   \
                                     detail::is_suitable_input, A, NSRC)        \
+      /* Create D0, D1, ... as cleaned version of A0, A1, ... */               \
       DYND_PP_CLEAN_TYPE_RANGE_1(D, A, NSRC);                                  \
                                                                                \
-      ndt::type dst_tp = ndt::cfixed_dim_from_array<R>::make();                \
-      ndt::type src_tp[NSRC] = {DYND_PP_JOIN_ELWISE_1(                         \
-          DYND_PP_META_SCOPE_CALL, (, ),                                       \
-          DYND_PP_OUTER(DYND_PP_META_TEMPLATE_INSTANTIATION,                   \
-                        (ndt::cfixed_dim_from_array),                          \
-                        DYND_PP_META_NAME_RANGE(D, NSRC)),                     \
-          DYND_PP_REPEAT(make, NSRC))};                                        \
+      /* Create dst_tp and the src_tp array from R and D0, D1, ... */          \
+      DYND_PP_NDT_TYPES_FROM_TYPES(R, D, NSRC);                                \
                                                                                \
       out_af->func_proto = ndt::make_funcproto(src_tp, dst_tp);                \
       *out_af->get_data_as<func_type>() = func;                                \
