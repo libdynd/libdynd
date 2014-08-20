45e96d3b989867ef957a7008d9d7479d3f2a42b9
Mark Wiebe <mwiebe@continuum.io>
2014-08-19 18:20:18 -0700

    Change simple example to a functor_arrfunc call

--- examples/simple_example.cpp
+++ examples/simple_example.cpp
@@ -14,39 +14,28 @@
 #include <dynd/array_range.hpp>
 #include <dynd/kernels/assignment_kernels.hpp>
 #include <dynd/json_parser.hpp>
-#include <dynd/func/elwise.hpp>
+#include <dynd/func/functor_arrfunc.hpp>
 #include <dynd/func/lift_reduction_arrfunc.hpp>
 #include <dynd/kernels/reduction_kernels.hpp>
 
 using namespace std;
 using namespace dynd;
 
+double myfunc(int x, float y)
+{
+  return x + 2.5 * y;
+}
+
 int main()
 {
-    try {
-        /*
-        nd::array a = nd::empty<double[4][3]>();
-        a(0).vals() = nd::range(3);
-        a(1).vals() = nd::range(3) + 5;
-        a(2).vals() = nd::range(3) + 10;
-        a(3).vals() = nd::range(3) + 12;
-
-        bool reduction_dims[2] = {true, true};
-        nd::arrfunc sum = lift_reduction_arrfunc(
-            kernels::make_builtin_sum_reduction_arrfunc(float64_type_id),
-            ndt::type("strided * strided * float64"), nd::arrfunc(), false, 2,
-            reduction_dims, true, true, false, nd::array());
-
-        cout << a << endl;
-        nd::array b = nd::elwise([&](double x) -> double {
-            return sum(a + x).as<double>();
-        }, a);
-        cout << b << endl;
-        */
-
-    } catch(const std::exception& e) {
-        cout << "Error: " << e.what() << "\n";
-        return 1;
-    }
-    return 0;
+  try {
+    nd::arrfunc af = nd::make_functor_arrfunc(myfunc);
+    cout << af(3, 1.1f) << endl;
+  }
+  catch (const std::exception &e)
+  {
+    cout << "Error: " << e.what() << "\n";
+    return 1;
+  }
+  return 0;
 }
