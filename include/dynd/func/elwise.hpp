fecce20361071ee18f629007ec5c53f79f1eb9ec
Mark Wiebe <mwiebe@continuum.io>
2014-10-13 15:41:35 -0700

    Remove the 'bool copy = true' parameter from nd::elwise
    This looks to be an error-prone construct, because values
    intended to be array parameters could be passed in as
    the value for 'copy' instead.

--- include/dynd/func/elwise.hpp
+++ include/dynd/func/elwise.hpp
@@ -13,29 +13,35 @@
 
 namespace dynd { namespace nd {
 
-#define ELWISE(N) \
-    template <typename func_type> \
-    nd::array elwise DYND_PP_PREPEND(func_type func, DYND_PP_APPEND(bool copy = true, DYND_PP_META_NAME_RANGE(const nd::array &a, N))) { \
-        nd::arrfunc af = make_functor_arrfunc(func, copy); \
-        nd::arrfunc laf = lift_arrfunc(af); \
-\
-        nd::array args[N] = {DYND_PP_JOIN((,), DYND_PP_META_NAME_RANGE(a, N))}; \
-        return laf.call(N, args, &eval::default_eval_context); \
-    } \
+#define ELWISE(N)                                                              \
+  template <typename func_type>                                                \
+  nd::array elwise DYND_PP_PREPEND(                                            \
+      func_type func, DYND_PP_META_NAME_RANGE(const nd::array &a, N))          \
+  {                                                                            \
+    nd::arrfunc af = make_functor_arrfunc(func, false);                        \
+    nd::arrfunc laf = lift_arrfunc(af);                                        \
+                                                                               \
+    nd::array args[N] = {DYND_PP_JOIN((, ), DYND_PP_META_NAME_RANGE(a, N))};   \
+    return laf.call(N, args, &eval::default_eval_context);                     \
+  }
 
 DYND_PP_JOIN_MAP(ELWISE, (), DYND_PP_RANGE(1, DYND_PP_INC(DYND_SRC_MAX)))
 
 #undef ELWISE
 
-#define ELWISE(N) \
-    template <typename obj_type, typename mem_func_type> \
-    nd::array elwise DYND_PP_PREPEND(obj_type obj, DYND_PP_PREPEND(mem_func_type mem_func, DYND_PP_APPEND(bool copy = true, DYND_PP_META_NAME_RANGE(const nd::array &a, N)))) { \
-        nd::arrfunc af = make_functor_arrfunc(obj, mem_func, copy); \
-        nd::arrfunc laf = lift_arrfunc(af); \
-\
-        nd::array args[N] = {DYND_PP_JOIN((,), DYND_PP_META_NAME_RANGE(a, N))}; \
-        return laf.call(N, args, &eval::default_eval_context); \
-    } \
+#define ELWISE(N)                                                              \
+  template <typename obj_type, typename mem_func_type>                         \
+  nd::array elwise DYND_PP_PREPEND(                                            \
+      obj_type obj,                                                            \
+      DYND_PP_PREPEND(mem_func_type mem_func,                                  \
+                      DYND_PP_META_NAME_RANGE(const nd::array &a, N)))         \
+  {                                                                            \
+    nd::arrfunc af = make_functor_arrfunc(obj, mem_func, false);               \
+    nd::arrfunc laf = lift_arrfunc(af);                                        \
+                                                                               \
+    nd::array args[N] = {DYND_PP_JOIN((, ), DYND_PP_META_NAME_RANGE(a, N))};   \
+    return laf.call(N, args, &eval::default_eval_context);                     \
+  }
 
 DYND_PP_JOIN_MAP(ELWISE, (), DYND_PP_RANGE(1, DYND_PP_INC(DYND_SRC_MAX)))
 
