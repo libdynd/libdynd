74b8ed7746fa0ac7364c3d03dc6d29a4401414e4
izaid <hi@irwinzaid.com>
2014-11-24 20:36:29 +0000

    Unified the CUDA expr_ck

--- include/dynd/kernels/expr_kernels.hpp
+++ include/dynd/kernels/expr_kernels.hpp
@@ -10,139 +10,86 @@
 namespace dynd {
 
 namespace kernels {
-    /**
-     * A CRTP (curiously recurring template pattern) base class to help
-     * create ckernels.
-     */
-    template <class CKT, int Nsrc, class CKBT = ckernel_builder>
-    struct expr_ck;
+  /**
+   * A CRTP (curiously recurring template pattern) base class to help
+   * create ckernels.
+   */
+  template <class CKT, int Nsrc, class CKBT = ckernel_builder>
+  struct expr_ck;
 
-    template <class CKT, int Nsrc>
-    struct expr_ck<CKT, Nsrc, ckernel_builder> : public general_ck<CKT, ckernel_builder> {
-        typedef CKT self_type;
-        typedef general_ck<CKT, ckernel_builder> parent_type;
+#ifdef __CUDA_ARCH__
+#define THROW_INVALID_ARGUMENT(KERNREQ)
+#else
+#define THROW_INVALID_ARGUMENT(KERNREQ)                                        \
+  std::stringstream ss;                                                        \
+  ss << "expr ckernel init: unrecognized ckernel request " << (int)KERNREQ;    \
+  throw std::invalid_argument(ss.str());
+#endif
 
-        /**
-         * Initializes just the base.function member
-         */
-        void init_kernfunc(kernel_request_t kernreq)
-        {
-            switch (kernreq) {
-            case kernel_request_single:
-                this->base.template set_function<expr_single_t>(
-                    &self_type::single_wrapper);
-                break;
-            case kernel_request_strided:
-                this->base.template set_function<expr_strided_t>(
-                    &self_type::strided_wrapper);
-                break;
-            default: {
-                std::stringstream ss;
-                ss << "expr ckernel init: unrecognized ckernel request " << (int)kernreq;
-                throw std::invalid_argument(ss.str());
-            }
-            }
-        }
+#define EXPR_CK(CKBT, ...)                                                     \
+  template <class CKT, int Nsrc>                                               \
+  struct expr_ck<CKT, Nsrc, CKBT> : public general_ck<CKT, CKBT> {             \
+    typedef CKT self_type;                                                     \
+    typedef general_ck<CKT, CKBT> parent_type;                                 \
+                                                                               \
+    /** Initializes just the base.function member. */                          \
+    __VA_ARGS__ void init_kernfunc(kernel_request_t kernreq)                   \
+    {                                                                          \
+      switch (kernreq) {                                                       \
+      case kernel_request_single:                                              \
+        this->base.template set_function<expr_single_t>(                       \
+            &self_type::single_wrapper);                                       \
+        break;                                                                 \
+      case kernel_request_strided:                                             \
+        this->base.template set_function<expr_strided_t>(                      \
+            &self_type::strided_wrapper);                                      \
+        break;                                                                 \
+      default: {                                                               \
+        THROW_INVALID_ARGUMENT(kernreq)                                        \
+      }                                                                        \
+      }                                                                        \
+    }                                                                          \
+                                                                               \
+    __VA_ARGS__ static void single_wrapper(char *dst, char **src,              \
+                                           ckernel_prefix *rawself)            \
+    {                                                                          \
+      return parent_type::get_self(rawself)->single(dst, src);                 \
+    }                                                                          \
+                                                                               \
+    __VA_ARGS__ static void strided_wrapper(char *dst, intptr_t dst_stride,    \
+                                            char **src,                        \
+                                            const intptr_t *src_stride,        \
+                                            size_t count,                      \
+                                            ckernel_prefix *rawself)           \
+    {                                                                          \
+      return parent_type::get_self(rawself)                                    \
+          ->strided(dst, dst_stride, src, src_stride, count);                  \
+    }                                                                          \
+                                                                               \
+    /* Default strided implementation calls single repeatedly. */              \
+    __VA_ARGS__ void strided(char *dst, intptr_t dst_stride, char **src,       \
+                             const intptr_t *src_stride, size_t count)         \
+    {                                                                          \
+      self_type *self = parent_type::get_self(&this->base);                    \
+      char *src_copy[Nsrc ? Nsrc : 1];                                         \
+      memcpy(src_copy, src, Nsrc ? sizeof(src_copy) : 0);                      \
+      for (size_t i = 0; i != count; ++i) {                                    \
+        self->single(dst, src_copy);                                           \
+        dst += dst_stride;                                                     \
+        for (int j = 0; j < Nsrc; ++j) {                                       \
+          src_copy[j] += src_stride[j];                                        \
+        }                                                                      \
+      }                                                                        \
+    }                                                                          \
+  };
 
-        static void single_wrapper(char *dst, char **src,
-                                   ckernel_prefix *rawself)
-        {
-            return parent_type::get_self(rawself)->single(dst, src);
-        }
-
-        static void strided_wrapper(char *dst, intptr_t dst_stride,
-                                    char **src, const intptr_t *src_stride, size_t count,
-                                    ckernel_prefix *rawself)
-        {
-            return parent_type::get_self(rawself)
-                ->strided(dst, dst_stride, src, src_stride, count);
-        }
-
-        /**
-         * Default strided implementation calls single repeatedly.
-         */
-        void strided(char *dst, intptr_t dst_stride,
-                            char **src, const intptr_t *src_stride,
-                            size_t count)
-        {
-            self_type *self = parent_type::get_self(&this->base);
-            char *src_copy[Nsrc];
-            memcpy(src_copy, src, sizeof(src_copy));
-            for (size_t i = 0; i != count; ++i) {
-                self->single(dst, src_copy);
-                dst += dst_stride;
-                for (int j = 0; j < Nsrc; ++j) {
-                    src_copy[j] += src_stride[j];
-                }
-            }
-        }
-    };
-
+  EXPR_CK(ckernel_builder)
 #ifdef __CUDACC__
+  EXPR_CK(cuda_device_ckernel_builder, __device__)
+#endif
 
-    template <class CKT, int Nsrc>
-    struct expr_ck<CKT, Nsrc, cuda_device_ckernel_builder> : public general_ck<CKT, cuda_device_ckernel_builder> {
-        typedef CKT self_type;
-        typedef general_ck<CKT, cuda_device_ckernel_builder> parent_type;
-
-        /**
-         * Initializes just the base.function member
-         */
-        __device__ void init_kernfunc(kernel_request_t kernreq)
-        {
-            switch (kernreq) {
-            case kernel_request_single:
-                this->base.template set_function<expr_single_t>(
-                    &self_type::single_wrapper);
-                break;
-            case kernel_request_strided:
-                this->base.template set_function<expr_strided_t>(
-                    &self_type::strided_wrapper);
-                break;
-            default: {
-                printf("error\n");
-//                std::stringstream ss;
-  //              ss << "expr ckernel init: unrecognized ckernel request " << (int)kernreq;
-    //            throw std::invalid_argument(ss.str());
-            }
-            }
-        }
-
-        __device__ static void single_wrapper(char *dst, char **src,
-                                   ckernel_prefix *rawself)
-        {
-            return parent_type::get_self(rawself)->single(dst, src);
-        }
-
-        __device__ static void strided_wrapper(char *dst, intptr_t dst_stride,
-                                    char **src, const intptr_t *src_stride, size_t count,
-                                    ckernel_prefix *rawself)
-        {
-            return parent_type::get_self(rawself)
-                ->strided(dst, dst_stride, src, src_stride, count);
-        }
-
-        /**
-         * Default strided implementation calls single repeatedly.
-         */
-        __device__ void strided(char *dst, intptr_t dst_stride,
-                            char **src, const intptr_t *src_stride,
-                            size_t count)
-        {
-            self_type *self = parent_type::get_self(&this->base);
-            char *src_copy[Nsrc + 1];
-            memcpy(src_copy, src, sizeof(src_copy) - 1);
-            for (size_t i = 0; i != count; ++i) {
-                self->single(dst, src_copy);
-                dst += dst_stride;
-                for (int j = 0; j < Nsrc; ++j) {
-                    src_copy[j] += src_stride[j];
-                }
-            }
-        }
-    };
-
-#endif // __CUDACC__
+#undef EXPR_CK
+#undef THROW_INVALID_ARGUMENT
 
 } // namespace kernels
 
@@ -154,10 +101,10 @@ class expr_kernel_generator;
  * types on to the handler to build the rest of the
  * kernel.
  */
-size_t make_expression_type_expr_kernel(ckernel_builder *ckb, intptr_t ckb_offset,
-                const ndt::type& dst_tp, const char *dst_arrmeta,
-                size_t src_count, const ndt::type *src_dt, const char **src_arrmeta,
-                kernel_request_t kernreq, const eval::eval_context *ectx,
-                const expr_kernel_generator *handler);
+size_t make_expression_type_expr_kernel(
+    ckernel_builder *ckb, intptr_t ckb_offset, const ndt::type &dst_tp,
+    const char *dst_arrmeta, size_t src_count, const ndt::type *src_dt,
+    const char **src_arrmeta, kernel_request_t kernreq,
+    const eval::eval_context *ectx, const expr_kernel_generator *handler);
 
 } // namespace dynd
