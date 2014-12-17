c7641940c3087b3939735934924682c46fc9fa7c
izaid <hi@irwinzaid.com>
2014-12-17 18:32:37 +0000

    Added some new files

---
+++ include/dynd/func/fft.hpp
@@ -0,0 +1,85 @@
+//
+// Copyright (C) 2011-14 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#pragma once
+
+#include <dynd/func/arrfunc.hpp>
+#include <dynd/func/multidispatch_arrfunc.hpp>
+#include <dynd/kernels/fft.hpp>
+
+namespace dynd {
+namespace decl {
+  namespace nd {
+
+    /*
+    class fftw : public arrfunc<fftw> {
+    public:
+        static dynd::nd::arrfunc make() {
+            dynd::nd::arrfunc af[2] = {arrfunc<fftw_ck<fftwf_complex,
+    fftwf_complex, FFTW_FORWARD> >::make(),
+                arrfunc<fftw_ck<fftw_complex, fftw_complex, FFTW_FORWARD>
+    >::make()};
+
+            dynd::nd::arrfunc af2 = make_multidispatch_arrfunc(2, af);;
+            std::cout << arrfunc<fftw_ck<fftwf_complex, fftwf_complex,
+    FFTW_FORWARD> >::make().get()->func_proto << std::endl;
+
+            return af2;
+        }
+    };
+    */
+
+    typedef arrfunc<fftw_ck<fftw_complex, fftw_complex, FFTW_FORWARD>> fftw;
+    typedef arrfunc<fftw_ck<fftw_complex, double>> rfftw;
+
+    typedef arrfunc<fftw_ck<fftw_complex, fftw_complex, FFTW_BACKWARD>> ifftw;
+    typedef arrfunc<fftw_ck<double, fftw_complex>> irfftw;
+  }
+} // namespace dynd::decl::nd
+
+namespace nd {
+
+  extern decl::nd::fftw fftw;
+  extern decl::nd::rfftw rfftw;
+
+  extern decl::nd::ifftw ifftw;
+  extern decl::nd::irfftw irfftw;
+
+} // namespace dynd::nd
+
+namespace decl {
+  namespace nd {
+
+    class fft : public arrfunc<fft> {
+    public:
+      static dynd::nd::arrfunc make() { return dynd::nd::fftw; }
+    };
+
+    class rfft : public arrfunc<rfft> {
+    public:
+      static dynd::nd::arrfunc make() { return dynd::nd::rfftw; }
+    };
+
+    class ifft : public arrfunc<ifft> {
+    public:
+      static dynd::nd::arrfunc make() { return dynd::nd::ifftw; }
+    };
+
+    class irfft : public arrfunc<irfft> {
+    public:
+      static dynd::nd::arrfunc make() { return dynd::nd::irfftw; }
+    };
+  }
+} // namespace dynd::decl::nd
+
+namespace nd {
+
+  extern decl::nd::fft fft;
+  extern decl::nd::rfft rfft;
+
+  extern decl::nd::ifft ifft;
+  extern decl::nd::irfft irfft;
+}
+} // namespace dynd::nd
---
+++ src/dynd/func/fft.cpp
@@ -0,0 +1,18 @@
+//
+// Copyright (C) 2011-14 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <dynd/func/fft.hpp>
+
+using namespace std;
+using namespace dynd;
+
+decl::nd::fftw nd::fftw;
+decl::nd::rfftw nd::rfftw;
+decl::nd::ifftw nd::ifftw;
+decl::nd::irfftw nd::irfftw;
+decl::nd::fft nd::fft;
+decl::nd::rfft nd::rfft;
+decl::nd::ifft nd::ifft;
+decl::nd::irfft nd::irfft;
