5ef7ea6a70b689741fef9dee11a3d8b3a6eb4cac
Mark Wiebe <mwwiebe@gmail.com>
2014-12-28 12:56:43 -0800

    Code reformatting

--- src/dynd/types/ctuple_type.cpp
+++ src/dynd/types/ctuple_type.cpp
@@ -21,43 +21,41 @@ ctuple_type::ctuple_type(const nd::array &field_types)
     : base_tuple_type(ctuple_type_id, field_types, type_flag_none, false),
       m_data_offsets(nd::empty(m_field_count, ndt::make_type<uintptr_t>()))
 {
-    uintptr_t *data_offsets = reinterpret_cast<uintptr_t *>(
-        m_data_offsets.get_readwrite_originptr());
+  uintptr_t *data_offsets =
+      reinterpret_cast<uintptr_t *>(m_data_offsets.get_readwrite_originptr());
 
-    size_t offs = 0;
-    for (intptr_t i = 0; i < m_field_count; ++i) {
-        const ndt::type& field_tp = get_field_type(i);
-        offs = inc_to_alignment(offs, field_tp.get_data_alignment());
-        data_offsets[i] = offs;
-        size_t field_element_size = field_tp.get_data_size();
-        if (field_element_size == 0 && !field_tp.is_symbolic()) {
-            stringstream ss;
-            ss << "Cannot create dynd ctuple type with type " << field_tp;
-            ss << " for field at index " << i << ", as it does not have a fixed size";
-            throw runtime_error(ss.str());
-        }
-        offs += field_element_size;
+  size_t offs = 0;
+  for (intptr_t i = 0; i < m_field_count; ++i) {
+    const ndt::type &field_tp = get_field_type(i);
+    offs = inc_to_alignment(offs, field_tp.get_data_alignment());
+    data_offsets[i] = offs;
+    size_t field_element_size = field_tp.get_data_size();
+    if (field_element_size == 0 && !field_tp.is_symbolic()) {
+      stringstream ss;
+      ss << "Cannot create dynd ctuple type with type " << field_tp;
+      ss << " for field at index " << i << ", as it does not have a fixed size";
+      throw runtime_error(ss.str());
     }
-    m_members.data_size = inc_to_alignment(offs, m_members.data_alignment);
+    offs += field_element_size;
+  }
+  m_members.data_size = inc_to_alignment(offs, m_members.data_alignment);
 
-    m_data_offsets.flag_as_immutable();
+  m_data_offsets.flag_as_immutable();
 }
 
-ctuple_type::~ctuple_type()
-{
-}
+ctuple_type::~ctuple_type() {}
 
-void ctuple_type::print_type(std::ostream& o) const
+void ctuple_type::print_type(std::ostream &o) const
 {
-    // Use the tuple datashape syntax prefixed with a "c"
-    o << "c(";
-    for (intptr_t i = 0, i_end = m_field_count; i != i_end; ++i) {
-        if (i != 0) {
-            o << ", ";
-        }
-        o << get_field_type(i);
+  // Use the tuple datashape syntax prefixed with a "c"
+  o << "c(";
+  for (intptr_t i = 0, i_end = m_field_count; i != i_end; ++i) {
+    if (i != 0) {
+      o << ", ";
     }
-    o << ")";
+    o << get_field_type(i);
+  }
+  o << ")";
 }
 
 void ctuple_type::transform_child_types(type_transform_fn_t transform_fn,
@@ -65,67 +63,68 @@ void ctuple_type::transform_child_types(type_transform_fn_t transform_fn,
                                         ndt::type &out_transformed_tp,
                                         bool &out_was_transformed) const
 {
-    nd::array tmp_field_types(
-        nd::empty(m_field_count, ndt::make_type()));
-    ndt::type *tmp_field_types_raw = reinterpret_cast<ndt::type *>(
-        tmp_field_types.get_readwrite_originptr());
+  nd::array tmp_field_types(nd::empty(m_field_count, ndt::make_type()));
+  ndt::type *tmp_field_types_raw =
+      reinterpret_cast<ndt::type *>(tmp_field_types.get_readwrite_originptr());
 
-    bool was_transformed = false;
-    for (intptr_t i = 0, i_end = m_field_count; i != i_end; ++i) {
-      transform_fn(get_field_type(i), arrmeta_offset + get_arrmeta_offset(i),
-                   extra, tmp_field_types_raw[i], was_transformed);
-    }
-    if (was_transformed) {
-        tmp_field_types.flag_as_immutable();
-        out_transformed_tp = ndt::make_ctuple(tmp_field_types);
-        out_was_transformed = true;
-    } else {
-        out_transformed_tp = ndt::type(this, true);
-    }
+  bool was_transformed = false;
+  for (intptr_t i = 0, i_end = m_field_count; i != i_end; ++i) {
+    transform_fn(get_field_type(i), arrmeta_offset + get_arrmeta_offset(i),
+                 extra, tmp_field_types_raw[i], was_transformed);
+  }
+  if (was_transformed) {
+    tmp_field_types.flag_as_immutable();
+    out_transformed_tp = ndt::make_ctuple(tmp_field_types);
+    out_was_transformed = true;
+  }
+  else {
+    out_transformed_tp = ndt::type(this, true);
+  }
 }
 
 ndt::type ctuple_type::get_canonical_type() const
 {
-    nd::array tmp_field_types(
-        nd::empty(m_field_count, ndt::make_type()));
-    ndt::type *tmp_field_types_raw = reinterpret_cast<ndt::type *>(
-        tmp_field_types.get_readwrite_originptr());
+  nd::array tmp_field_types(nd::empty(m_field_count, ndt::make_type()));
+  ndt::type *tmp_field_types_raw =
+      reinterpret_cast<ndt::type *>(tmp_field_types.get_readwrite_originptr());
 
-    for (intptr_t i = 0, i_end = m_field_count; i != i_end; ++i) {
-        tmp_field_types_raw[i] = get_field_type(i).get_canonical_type();
-    }
+  for (intptr_t i = 0, i_end = m_field_count; i != i_end; ++i) {
+    tmp_field_types_raw[i] = get_field_type(i).get_canonical_type();
+  }
 
-    tmp_field_types.flag_as_immutable();
-    return ndt::make_ctuple(tmp_field_types);
+  tmp_field_types.flag_as_immutable();
+  return ndt::make_ctuple(tmp_field_types);
 }
 
-ndt::type ctuple_type::at_single(intptr_t i0,
-                const char **inout_arrmeta, const char **inout_data) const
+ndt::type ctuple_type::at_single(intptr_t i0, const char **inout_arrmeta,
+                                 const char **inout_data) const
 {
-    // Bounds-checking of the index
-    i0 = apply_single_index(i0, m_field_count, NULL);
-    if (inout_arrmeta) {
-        // Modify the arrmeta
-        *inout_arrmeta += get_arrmeta_offsets_raw()[i0];
-        // If requested, modify the data
-        if (inout_data) {
-            *inout_data += get_data_offsets_raw()[i0];
-        }
+  // Bounds-checking of the index
+  i0 = apply_single_index(i0, m_field_count, NULL);
+  if (inout_arrmeta) {
+    // Modify the arrmeta
+    *inout_arrmeta += get_arrmeta_offsets_raw()[i0];
+    // If requested, modify the data
+    if (inout_data) {
+      *inout_data += get_data_offsets_raw()[i0];
     }
-    return get_field_type(i0);
+  }
+  return get_field_type(i0);
 }
 
-bool ctuple_type::is_lossless_assignment(const ndt::type& dst_tp, const ndt::type& src_tp) const
+bool ctuple_type::is_lossless_assignment(const ndt::type &dst_tp,
+                                         const ndt::type &src_tp) const
 {
-    if (dst_tp.extended() == this) {
-        if (src_tp.extended() == this) {
-            return true;
-        } else if (src_tp.get_type_id() == ctuple_type_id) {
-            return *dst_tp.extended() == *src_tp.extended();
-        }
+  if (dst_tp.extended() == this) {
+    if (src_tp.extended() == this) {
+      return true;
     }
+    else if (src_tp.get_type_id() == ctuple_type_id) {
+      return *dst_tp.extended() == *src_tp.extended();
+    }
+  }
 
-    return false;
+  return false;
 }
 
 intptr_t ctuple_type::make_assignment_kernel(
@@ -182,60 +181,68 @@ size_t ctuple_type::make_comparison_kernel(void *ckb, intptr_t ckb_offset,
   throw not_comparable_error(src0_tp, src1_tp, comptype);
 }
 
-bool ctuple_type::operator==(const base_type& rhs) const
+bool ctuple_type::operator==(const base_type &rhs) const
 {
-    if (this == &rhs) {
-        return true;
-    } else if (rhs.get_type_id() != ctuple_type_id) {
-        return false;
-    } else {
-        const ctuple_type *dt = static_cast<const ctuple_type*>(&rhs);
-        return get_data_alignment() == dt->get_data_alignment() &&
-                m_field_types.equals_exact(dt->m_field_types);
-    }
+  if (this == &rhs) {
+    return true;
+  }
+  else if (rhs.get_type_id() != ctuple_type_id) {
+    return false;
+  }
+  else {
+    const ctuple_type *dt = static_cast<const ctuple_type *>(&rhs);
+    return get_data_alignment() == dt->get_data_alignment() &&
+           m_field_types.equals_exact(dt->m_field_types);
+  }
 }
 
-void ctuple_type::arrmeta_debug_print(const char *arrmeta, std::ostream& o, const std::string& indent) const
+void ctuple_type::arrmeta_debug_print(const char *arrmeta, std::ostream &o,
+                                      const std::string &indent) const
 {
-    o << indent << "ctuple arrmeta\n";
-    const uintptr_t *arrmeta_offsets = get_arrmeta_offsets_raw();
-    for (intptr_t i = 0; i < m_field_count; ++i) {
-        const ndt::type& field_dt = get_field_type(i);
-        if (!field_dt.is_builtin() && field_dt.extended()->get_arrmeta_size() > 0) {
-            o << indent << " field " << i << " arrmeta:\n";
-            field_dt.extended()->arrmeta_debug_print(arrmeta + arrmeta_offsets[i], o, indent + "  ");
-        }
+  o << indent << "ctuple arrmeta\n";
+  const uintptr_t *arrmeta_offsets = get_arrmeta_offsets_raw();
+  for (intptr_t i = 0; i < m_field_count; ++i) {
+    const ndt::type &field_dt = get_field_type(i);
+    if (!field_dt.is_builtin() && field_dt.extended()->get_arrmeta_size() > 0) {
+      o << indent << " field " << i << " arrmeta:\n";
+      field_dt.extended()->arrmeta_debug_print(arrmeta + arrmeta_offsets[i], o,
+                                               indent + "  ");
     }
+  }
 }
 
 ///////// properties on the type
 
-static nd::array property_get_field_types(const ndt::type& tp) {
-    return tp.extended<ctuple_type>()->get_field_types();
-}
-
-static nd::array property_get_data_offsets(const ndt::type& tp) {
-    return tp.extended<ctuple_type>()->get_data_offsets();
-}
-
-static nd::array property_get_arrmeta_offsets(const ndt::type& tp) {
-    return tp.extended<ctuple_type>()->get_arrmeta_offsets();
-}
-
-void ctuple_type::get_dynamic_type_properties(const std::pair<std::string, gfunc::callable> **out_properties, size_t *out_count) const
+static nd::array property_get_field_types(const ndt::type &tp)
 {
-    static pair<string, gfunc::callable> type_properties[] = {
-        pair<string, gfunc::callable>(
-            "field_types",
-            gfunc::make_callable(&property_get_field_types, "self")),
-        pair<string, gfunc::callable>(
-            "data_offsets",
-            gfunc::make_callable(&property_get_data_offsets, "self")),
-        pair<string, gfunc::callable>(
-            "arrmeta_offsets",
-            gfunc::make_callable(&property_get_arrmeta_offsets, "self"))};
-
-    *out_properties = type_properties;
-    *out_count = sizeof(type_properties) / sizeof(type_properties[0]);
+  return tp.extended<ctuple_type>()->get_field_types();
 }
 
+static nd::array property_get_data_offsets(const ndt::type &tp)
+{
+  return tp.extended<ctuple_type>()->get_data_offsets();
+}
+
+static nd::array property_get_arrmeta_offsets(const ndt::type &tp)
+{
+  return tp.extended<ctuple_type>()->get_arrmeta_offsets();
+}
+
+void ctuple_type::get_dynamic_type_properties(
+    const std::pair<std::string, gfunc::callable> **out_properties,
+    size_t *out_count) const
+{
+  static pair<string, gfunc::callable> type_properties[] = {
+      pair<string, gfunc::callable>(
+          "field_types",
+          gfunc::make_callable(&property_get_field_types, "self")),
+      pair<string, gfunc::callable>(
+          "data_offsets",
+          gfunc::make_callable(&property_get_data_offsets, "self")),
+      pair<string, gfunc::callable>(
+          "arrmeta_offsets",
+          gfunc::make_callable(&property_get_arrmeta_offsets, "self"))};
+
+  *out_properties = type_properties;
+  *out_count = sizeof(type_properties) / sizeof(type_properties[0]);
+}
