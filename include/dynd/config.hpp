78500878d248b0d99bfc0b89a80465e06f95558e
izaid <hi@irwinzaid.com>
2015-06-15 14:22:24 +0100

    Another attempt to fix MSVC

--- include/dynd/config.hpp
+++ include/dynd/config.hpp
@@ -9,6 +9,7 @@
 
 #include <cassert>
 #include <cmath>
+#include <cstdint>
 #include <cstdlib>
 #include <cctype>
 #include <initializer_list>
@@ -565,14 +566,14 @@ struct is_arithmetic {
   static const bool value = false;
 };
 
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
+typedef std::int8_t int8;
+typedef std::int16_t int16;
+typedef std::int32_t int32;
+typedef std::int64_t int64;
+typedef std::uint8_t uint8;
+typedef std::uint16_t uint16;
+typedef std::uint32_t uint32;
+typedef std::uint64_t uint64;
 typedef float float32;
 typedef double float64;
 
