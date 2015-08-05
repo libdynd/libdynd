697cad88c506e9f56d534ec0e2d60591103ec7ca
Irwin Zaid <izaid@users.noreply.github.com>
2015-08-05 09:42:27 +0100

    Update build.sh

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -27,6 +27,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release  \
     -DDYND_SHARED_LIB=ON \
     -DDYND_INSTALL_LIB=ON \
+    -DDYND_BUILD_BENCHMARKS=OFF \
     -DCMAKE_INSTALL_PREFIX=$PREFIX .. || exit 1
 echo Building with make...
 make || exit 1
