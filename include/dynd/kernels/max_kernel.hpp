13ccc8ab6a175cf9c7dda34e03122548f92ba48b
izaid <hi@irwinzaid.com>
2015-08-18 21:35:43 +0100

    Added max files

---
+++ include/dynd/kernels/max_kernel.hpp
@@ -0,0 +1,64 @@
+//
+// Copyright (C) 2011-15 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#pragma once
+
+#include <dynd/kernels/base_kernel.hpp>
+
+namespace dynd {
+namespace nd {
+
+  template <type_id_t Src0TypeID>
+  struct max_kernel
+      : base_kernel<max_kernel<Src0TypeID>, kernel_request_host, 1> {
+    typedef typename type_of<Src0TypeID>::type src0_type;
+    typedef src0_type dst_type;
+
+    static const std::size_t data_size = 0;
+
+    void single(char *dst, char *const *src)
+    {
+      if (*reinterpret_cast<src0_type *>(src[0]) >
+          *reinterpret_cast<dst_type *>(dst)) {
+        *reinterpret_cast<dst_type *>(dst) =
+            *reinterpret_cast<src0_type *>(src[0]);
+      }
+    }
+
+    void strided(char *dst, intptr_t dst_stride, char *const *src,
+                 const intptr_t *src_stride, size_t count)
+    {
+
+      char *src0 = src[0];
+      std::cout << *reinterpret_cast<src0_type *>(src0) << std::endl;
+      intptr_t src0_stride = src_stride[0];
+      for (size_t i = 0; i < count; ++i) {
+        if (*reinterpret_cast<src0_type *>(src0) >
+            *reinterpret_cast<dst_type *>(dst)) {
+          *reinterpret_cast<dst_type *>(dst) =
+              *reinterpret_cast<src0_type *>(src0);
+        }
+        dst += dst_stride;
+        src0 += src0_stride;
+      }
+    }
+  };
+
+} // namespace dynd::nd
+
+namespace ndt {
+
+  template <type_id_t Src0TypeID>
+  struct type::equivalent<nd::max_kernel<Src0TypeID>> {
+    static type make()
+    {
+      return callable_type::make(
+          ndt::type::make<typename nd::max_kernel<Src0TypeID>::dst_type>(),
+          type(Src0TypeID));
+    }
+  };
+
+} // namespace dynd::ndt
+} // namespace dynd
---
+++ src/dynd/func/max.cpp
@@ -0,0 +1,36 @@
+//
+// Copyright (C) 2011-15 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <dynd/kernels/max_kernel.hpp>
+#include <dynd/func/multidispatch.hpp>
+#include <dynd/func/reduction.hpp>
+#include <dynd/func/max.hpp>
+#include <dynd/types/scalar_kind_type.hpp>
+
+using namespace std;
+using namespace dynd;
+
+nd::callable nd::max::make()
+{
+  auto children = callable::make_all<max_kernel, arithmetic_type_ids>();
+
+  return functional::reduction(functional::multidispatch(
+      ndt::callable_type::make(ndt::scalar_kind_type::make(),
+                               ndt::scalar_kind_type::make()),
+      [children](const ndt::type & DYND_UNUSED(dst_tp),
+                 intptr_t DYND_UNUSED(nsrc),
+                 const ndt::type * src_tp) mutable->callable &
+  {
+        callable &child = children[src_tp[0].get_type_id()];
+        if (child.is_null()) {
+          throw runtime_error("no suitable child found for nd::sum");
+        }
+
+        return child;
+      },
+      data_size_max(children)));
+}
+
+struct nd::max nd::max;
