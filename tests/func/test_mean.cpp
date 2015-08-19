6fc93fc26f8554dffa79d89879a9f5cb1314cb64
izaid <hi@irwinzaid.com>
2015-08-19 14:36:51 +0100

    Forgot test file

---
+++ tests/func/test_mean.cpp
@@ -0,0 +1,30 @@
+//
+// Copyright (C) 2011-15 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <cmath>
+
+#include "inc_gtest.hpp"
+#include "dynd_assertions.hpp"
+
+#include <dynd/func/mean.hpp>
+
+using namespace std;
+using namespace dynd;
+
+TEST(Mean, 1D)
+{
+  EXPECT_ARRAY_EQ(1.0, nd::mean(nd::array{1.0}));
+  EXPECT_ARRAY_EQ(4.5, nd::mean(nd::array{0.0, 1.0, 2.0, 3.0, 4.0,
+                                          5.0, 6.0, 7.0, 8.0, 9.0}));
+}
+
+TEST(Mean, 2D)
+{
+  EXPECT_ARRAY_EQ(4.5, nd::mean(nd::array{{0.0, 1.0, 2.0, 3.0, 4.0},
+                                          {5.0, 6.0, 7.0, 8.0, 9.0}}));
+}
