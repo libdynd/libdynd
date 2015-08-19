52fbda801e76651c984534cb8873210863c689d6
izaid <hi@irwinzaid.com>
2015-08-19 17:18:08 +0100

    Trying with a different MSVC strategy

--- include/dynd/kernels/compound_add_kernel.hpp
+++ include/dynd/kernels/compound_add_kernel.hpp
@@ -6,17 +6,14 @@ namespace dynd {
 namespace nd {
   namespace detail {
 
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : 4244)
-#endif
+    template <type_id_t DstTypeID, type_id_t Src0TypeID, bool UseBinaryOperator>
+    struct compound_add_kernel;
 
-    template <type_id_t DstTypeID, type_kind_t DstTypeKind,
-              type_id_t Src0TypeID, type_kind_t Src0TypeKind>
-    struct compound_add_kernel
-        : base_kernel<compound_add_kernel<DstTypeID, DstTypeKind, Src0TypeID,
-                                          Src0TypeKind>,
-                      kernel_request_host, 1> {
+    template <type_id_t DstTypeID, type_id_t Src0TypeID>
+    struct compound_add_kernel<
+        DstTypeID, Src0TypeID,
+        false> : base_kernel<compound_add_kernel<DstTypeID, Src0TypeID, false>,
+                             kernel_request_host, 1> {
       typedef typename type_of<DstTypeID>::type dst_type;
       typedef typename type_of<Src0TypeID>::type src0_type;
 
@@ -28,33 +25,25 @@ namespace nd {
             *reinterpret_cast<src0_type *>(src[0]);
       }
 
-      /*
-          void strided(char *dst, std::intptr_t dst_stride, char *const *src,
-                       const std::intptr_t *src_stride, std::size_t count)
-          {
-            char *src0 = src[0];
-            std::intptr_t src0_stride = src_stride[0];
-            for (std::size_t i = 0; i < count; ++i) {
-              *reinterpret_cast<dst_type *>(dst) +=
-                  *reinterpret_cast<src0_type *>(src0);
-              dst += dst_stride;
-              src0 += src0_stride;
-            }
-          }
-      */
+      void strided(char *dst, std::intptr_t dst_stride, char *const *src,
+                   const std::intptr_t *src_stride, std::size_t count)
+      {
+        char *src0 = src[0];
+        std::intptr_t src0_stride = src_stride[0];
+        for (std::size_t i = 0; i < count; ++i) {
+          *reinterpret_cast<dst_type *>(dst) +=
+              *reinterpret_cast<src0_type *>(src0);
+          dst += dst_stride;
+          src0 += src0_stride;
+        }
+      }
     };
 
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-
-/*
     template <type_id_t DstTypeID, type_id_t Src0TypeID>
     struct compound_add_kernel<
-        DstTypeID, sint_kind, Src0TypeID,
-        real_kind> : base_kernel<compound_add_kernel<DstTypeID, sint_kind,
-                                                     Src0TypeID, real_kind>,
-                                 kernel_request_host, 1> {
+        DstTypeID, Src0TypeID,
+        true> : base_kernel<compound_add_kernel<DstTypeID, Src0TypeID, true>,
+                            kernel_request_host, 1> {
       typedef typename type_of<DstTypeID>::type dst_type;
       typedef typename type_of<Src0TypeID>::type src0_type;
 
@@ -62,37 +51,20 @@ namespace nd {
 
       void single(char *dst, char *const *src)
       {
-        *reinterpret_cast<dst_type *>(dst) +=
-            static_cast<dst_type>(*reinterpret_cast<src0_type *>(src[0]));
+        *reinterpret_cast<dst_type *>(dst) =
+            static_cast<dst_type>(*reinterpret_cast<dst_type *>(dst) +
+                                  *reinterpret_cast<src0_type *>(src[0]));
       }
     };
-
-    template <>
-    struct compound_add_kernel<
-        int32_type_id, sint_kind, int64_type_id,
-        sint_kind> : base_kernel<compound_add_kernel<int32_type_id, sint_kind,
-                                                     int64_type_id, sint_kind>,
-                                 kernel_request_host, 1> {
-      typedef int32 dst_type;
-      typedef int64 src0_type;
-
-      static const std::size_t data_size = 0;
-
-      void single(char *dst, char *const *src)
-      {
-        *reinterpret_cast<dst_type *>(dst) +=
-            static_cast<dst_type>(*reinterpret_cast<src0_type *>(src[0]));
-      }
-    };
-*/
 
   } // namespace dynd::nd::detail
 
   template <type_id_t DstTypeID, type_id_t Src0TypeID>
   struct compound_add_kernel
-      : detail::compound_add_kernel<DstTypeID, type_kind_of<DstTypeID>::value,
-                                    Src0TypeID,
-                                    type_kind_of<Src0TypeID>::value> {
+      : detail::compound_add_kernel<
+            DstTypeID, Src0TypeID,
+            type_kind_of<DstTypeID>::value ==
+                sint_kind &&type_kind_of<Src0TypeID>::value == real_kind> {
   };
 
 } // namespace dynd::nd
