da353d35fd180017272c7029f2aa3c08e7bfcf1d
izaid <hi@irwinzaid.com>
2015-08-31 10:55:28 +0100

    Removed preprocessor headers

--- include/dynd/types/struct_type.hpp
+++ include/dynd/types/struct_type.hpp
@@ -13,8 +13,6 @@
 #include <dynd/types/pointer_type.hpp>
 #include <dynd/types/type_type.hpp>
 #include <dynd/memblock/memory_block.hpp>
-#include <dynd/pp/list.hpp>
-#include <dynd/pp/meta.hpp>
 
 namespace dynd {
 namespace ndt {
@@ -103,56 +101,5 @@ namespace ndt {
  * Concatenates the fields of two structs together into one.
  */
 nd::array struct_concat(nd::array lhs, nd::array rhs);
-
-#define PACK_ARG(N)                                                            \
-  DYND_PP_ELWISE_1(DYND_PP_ID,                                                 \
-                   DYND_PP_MAP_1(DYND_PP_META_DECL_CONST_STR_REF,              \
-                                 DYND_PP_META_NAME_RANGE(name, N)),            \
-                   DYND_PP_ELWISE_1(DYND_PP_META_DECL_CONST_REF,               \
-                                    DYND_PP_META_NAME_RANGE(A, N),             \
-                                    DYND_PP_META_NAME_RANGE(a, N)))
-
-#define PACK(N)                                                                \
-  template <DYND_PP_JOIN_MAP_1(DYND_PP_META_TYPENAME, (, ),                    \
-                               DYND_PP_META_NAME_RANGE(A, N))>                 \
-  nd::array pack PACK_ARG(N)                                                   \
-  {                                                                            \
-    const std::string *field_names[N] = {                                      \
-        DYND_PP_JOIN_1((, ), DYND_PP_META_NAME_RANGE(&name, N))};              \
-    const ndt::type field_types[N] = {DYND_PP_JOIN_MAP_1(                      \
-        ndt::get_forward_type, (, ), DYND_PP_META_NAME_RANGE(a, N))};          \
-                                                                               \
-    /* Allocate res with empty_shell, leaves unconstructed arrmeta */          \
-    nd::array res =                                                            \
-        nd::empty_shell(ndt::struct_type::make(field_names, field_types));     \
-    /* The struct's arrmeta includes data offsets, init them to default */     \
-    ndt::struct_type::fill_default_data_offsets(                               \
-        N, field_types, reinterpret_cast<uintptr_t *>(res.get_arrmeta()));     \
-    const uintptr_t *field_arrmeta_offsets =                                   \
-        res.get_type()                                                         \
-            .extended<ndt::base_struct_type>()                                 \
-            ->get_arrmeta_offsets_raw();                                       \
-    const uintptr_t *field_data_offsets =                                      \
-        res.get_type().extended<ndt::base_struct_type>()->get_data_offsets(    \
-            res.get_arrmeta());                                                \
-    char *res_arrmeta = res.get_arrmeta();                                     \
-    char *res_data = res.get_readwrite_originptr();                            \
-    /* Each pack_insert initializes the arrmeta and the data */                \
-    DYND_PP_JOIN_ELWISE_1(                                                     \
-        nd::forward_as_array, (;), DYND_PP_META_AT_RANGE(field_types, N),      \
-        DYND_PP_ELWISE_1(DYND_PP_META_ADD, DYND_PP_REPEAT_1(res_arrmeta, N),   \
-                         DYND_PP_META_AT_RANGE(field_arrmeta_offsets, N)),     \
-        DYND_PP_ELWISE_1(DYND_PP_META_ADD, DYND_PP_REPEAT_1(res_data, N),      \
-                         DYND_PP_META_AT_RANGE(field_data_offsets, N)),        \
-        DYND_PP_META_NAME_RANGE(a, N));                                        \
-                                                                               \
-    return res;                                                                \
-  }
-
-DYND_PP_JOIN_MAP(PACK, (), DYND_PP_RANGE(1, DYND_PP_INC(DYND_ARG_MAX)))
-
-#undef PACK
-
-#undef PACK_ARG
 
 } // namespace dynd
