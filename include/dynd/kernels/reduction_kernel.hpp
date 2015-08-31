7f73035fa3001c2e66f8d76f37bcff9de6c7a83c
izaid <hi@irwinzaid.com>
2015-08-31 01:04:47 +0100

    Refactored var dim reductions a bit

--- include/dynd/kernels/reduction_kernel.hpp
+++ include/dynd/kernels/reduction_kernel.hpp
@@ -499,53 +499,54 @@ namespace nd {
     template <>
     struct reduction_kernel<var_dim_type_id, false,
                             true> : base_reduction_kernel<reduction_kernel<var_dim_type_id, false, true>> {
-      std::intptr_t src0_element_stride;
-      std::intptr_t src0_element_stride_first;
-
+      intptr_t src0_inner_stride;
+      intptr_t src0_inner_stride_first;
       intptr_t init_offset;
 
-      reduction_kernel(std::intptr_t src0_element_stride, bool with_identity = false)
-          : src0_element_stride(src0_element_stride)
+      reduction_kernel(std::intptr_t src0_inner_stride, bool with_identity = false)
+          : src0_inner_stride(src0_inner_stride)
       {
         if (with_identity) {
-          src0_element_stride_first = 0;
+          src0_inner_stride_first = 0;
         } else {
-          src0_element_stride_first = src0_element_stride;
+          src0_inner_stride_first = src0_inner_stride;
         }
       }
 
       ~reduction_kernel()
       {
+        get_child(init_offset)->destroy();
+        get_child()->destroy();
       }
 
       void single_first(char *dst, char *const *src)
       {
-        std::size_t size = reinterpret_cast<var_dim_type_data *>(src[0])->size;
-        if (src0_element_stride_first != 0) {
-          --size;
+        size_t inner_size = reinterpret_cast<ndt::var_dim_type::data_type *>(src[0])->size;
+        if (src0_inner_stride_first != 0) {
+          --inner_size;
         }
 
-        char *src0_data = reinterpret_cast<var_dim_type_data *>(src[0])->begin;
+        char *src0_data = reinterpret_cast<ndt::var_dim_type::data_type *>(src[0])->begin;
         get_child(init_offset)->single(dst, &src0_data);
-        src0_data += src0_element_stride_first;
+        src0_data += src0_inner_stride_first;
 
-        get_child()->strided(dst, 0, &src0_data, &src0_element_stride, size);
+        get_child()->strided(dst, 0, &src0_data, &src0_inner_stride, inner_size);
       }
 
       void strided_first(char *DYND_UNUSED(dst), intptr_t DYND_UNUSED(dst_stride), char *const *DYND_UNUSED(src),
                          const intptr_t *DYND_UNUSED(src_stride), size_t DYND_UNUSED(count))
       {
-        std::cout << "strided_first" << std::endl;
+        throw std::runtime_error("strided_first is not supported for variable-sized dimensions");
       }
 
-      void strided_followup(char *dst, intptr_t dst_stride, char *const *src, const intptr_t *src_stride, size_t count)
+      void strided_followup(char *dst, intptr_t dst_stride, char *const *src, const intptr_t *src_stride, size_t size)
       {
-        ckernel_prefix *reduce_child = get_child();
+        ckernel_prefix *child = get_child();
 
         char *src0 = src[0];
-        for (std::size_t i = 0; i != count; ++i) {
-          reduce_child->strided(dst, 0, &reinterpret_cast<var_dim_type_data *>(src0)->begin, &src0_element_stride,
-                                reinterpret_cast<var_dim_type_data *>(src0)->size);
+        for (size_t i = 0; i != size; ++i) {
+          child->strided(dst, 0, &reinterpret_cast<ndt::var_dim_type::data_type *>(src0)->begin, &src0_inner_stride,
+                         reinterpret_cast<ndt::var_dim_type::data_type *>(src0)->size);
           dst += dst_stride;
           src0 += src_stride[0];
         }
@@ -557,8 +558,6 @@ namespace nd {
                                   const eval::eval_context *ectx, intptr_t nkwd, const array *kwds,
                                   const std::map<std::string, ndt::type> &tp_vars)
       {
-        std::cout << "var_dim instantiate" << std::endl;
-
         const ndt::type &src0_element_tp = src_tp[0].extended<ndt::var_dim_type>()->get_element_type();
         const char *src0_element_arrmeta = src_arrmeta[0] + sizeof(var_dim_type_arrmeta);
 
--- include/dynd/types/var_dim_type.hpp
+++ include/dynd/types/var_dim_type.hpp
@@ -31,15 +31,17 @@ struct var_dim_type_data {
 namespace ndt {
 
   class var_dim_type : public base_dim_type {
-    std::vector<std::pair<std::string, gfunc::callable>> m_array_properties,
-        m_array_functions;
+    std::vector<std::pair<std::string, gfunc::callable>> m_array_properties, m_array_functions;
 
   public:
     var_dim_type(const type &element_tp);
 
     virtual ~var_dim_type();
 
-    size_t get_default_data_size() const { return sizeof(var_dim_type_data); }
+    size_t get_default_data_size() const
+    {
+      return sizeof(var_dim_type_data);
+    }
 
     /** Alignment of the data being pointed to. */
     size_t get_target_alignment() const
@@ -47,40 +49,29 @@ namespace ndt {
       return m_element_tp.get_data_alignment();
     }
 
-    void print_data(std::ostream &o, const char *arrmeta,
-                    const char *data) const;
+    void print_data(std::ostream &o, const char *arrmeta, const char *data) const;
 
     void print_type(std::ostream &o) const;
 
     bool is_expression() const;
     bool is_unique_data_owner(const char *arrmeta) const;
-    void transform_child_types(type_transform_fn_t transform_fn,
-                               intptr_t arrmeta_offset, void *extra,
-                               type &out_transformed_tp,
-                               bool &out_was_transformed) const;
+    void transform_child_types(type_transform_fn_t transform_fn, intptr_t arrmeta_offset, void *extra,
+                               type &out_transformed_tp, bool &out_was_transformed) const;
     type get_canonical_type() const;
 
-    type apply_linear_index(intptr_t nindices, const irange *indices,
-                            size_t current_i, const type &root_tp,
+    type apply_linear_index(intptr_t nindices, const irange *indices, size_t current_i, const type &root_tp,
                             bool leading_dimension) const;
-    intptr_t apply_linear_index(intptr_t nindices, const irange *indices,
-                                const char *arrmeta, const type &result_tp,
-                                char *out_arrmeta,
-                                memory_block_data *embedded_reference,
-                                size_t current_i, const type &root_tp,
-                                bool leading_dimension, char **inout_data,
+    intptr_t apply_linear_index(intptr_t nindices, const irange *indices, const char *arrmeta, const type &result_tp,
+                                char *out_arrmeta, memory_block_data *embedded_reference, size_t current_i,
+                                const type &root_tp, bool leading_dimension, char **inout_data,
                                 memory_block_data **inout_dataref) const;
-    type at_single(intptr_t i0, const char **inout_arrmeta,
-                   const char **inout_data) const;
+    type at_single(intptr_t i0, const char **inout_arrmeta, const char **inout_data) const;
 
-    type get_type_at_dimension(char **inout_arrmeta, intptr_t i,
-                               intptr_t total_ndim = 0) const;
+    type get_type_at_dimension(char **inout_arrmeta, intptr_t i, intptr_t total_ndim = 0) const;
 
     intptr_t get_dim_size(const char *arrmeta, const char *data) const;
-    void get_shape(intptr_t ndim, intptr_t i, intptr_t *out_shape,
-                   const char *arrmeta, const char *data) const;
-    void get_strides(size_t i, intptr_t *out_strides,
-                     const char *arrmeta) const;
+    void get_shape(intptr_t ndim, intptr_t i, intptr_t *out_shape, const char *arrmeta, const char *data) const;
+    void get_strides(size_t i, intptr_t *out_strides, const char *arrmeta) const;
 
     axis_order_classification_t classify_axis_order(const char *arrmeta) const;
 
@@ -94,37 +85,27 @@ namespace ndt {
     void arrmeta_reset_buffers(char *arrmeta) const;
     void arrmeta_finalize_buffers(char *arrmeta) const;
     void arrmeta_destruct(char *arrmeta) const;
-    void arrmeta_debug_print(const char *arrmeta, std::ostream &o,
-                             const std::string &indent) const;
-    size_t
-    arrmeta_copy_construct_onedim(char *dst_arrmeta, const char *src_arrmeta,
-                                  memory_block_data *embedded_reference) const;
+    void arrmeta_debug_print(const char *arrmeta, std::ostream &o, const std::string &indent) const;
+    size_t arrmeta_copy_construct_onedim(char *dst_arrmeta, const char *src_arrmeta,
+                                         memory_block_data *embedded_reference) const;
 
     size_t get_iterdata_size(intptr_t ndim) const;
-    size_t iterdata_construct(iterdata_common *iterdata,
-                              const char **inout_arrmeta, intptr_t ndim,
-                              const intptr_t *shape,
-                              type &out_uniform_tp) const;
+    size_t iterdata_construct(iterdata_common *iterdata, const char **inout_arrmeta, intptr_t ndim,
+                              const intptr_t *shape, type &out_uniform_tp) const;
     size_t iterdata_destruct(iterdata_common *iterdata, intptr_t ndim) const;
 
-    intptr_t make_assignment_kernel(void *ckb, intptr_t ckb_offset,
-                                    const type &dst_tp, const char *dst_arrmeta,
-                                    const type &src_tp, const char *src_arrmeta,
-                                    kernel_request_t kernreq,
+    intptr_t make_assignment_kernel(void *ckb, intptr_t ckb_offset, const type &dst_tp, const char *dst_arrmeta,
+                                    const type &src_tp, const char *src_arrmeta, kernel_request_t kernreq,
                                     const eval::eval_context *ectx) const;
 
-    void foreach_leading(const char *arrmeta, char *data, foreach_fn_t callback,
-                         void *callback_data) const;
+    void foreach_leading(const char *arrmeta, char *data, foreach_fn_t callback, void *callback_data) const;
 
-    void get_dynamic_type_properties(
-        const std::pair<std::string, nd::callable> **out_properties,
-        size_t *out_count) const;
-    void get_dynamic_array_properties(
-        const std::pair<std::string, gfunc::callable> **out_properties,
-        size_t *out_count) const;
-    void get_dynamic_array_functions(
-        const std::pair<std::string, gfunc::callable> **out_functions,
-        size_t *out_count) const;
+    void get_dynamic_type_properties(const std::pair<std::string, nd::callable> **out_properties,
+                                     size_t *out_count) const;
+    void get_dynamic_array_properties(const std::pair<std::string, gfunc::callable> **out_properties,
+                                      size_t *out_count) const;
+    void get_dynamic_array_functions(const std::pair<std::string, gfunc::callable> **out_functions,
+                                     size_t *out_count) const;
 
     virtual type with_element_type(const type &element_tp) const;
 
@@ -142,6 +123,9 @@ namespace ndt {
 
       return result;
     }
+
+    typedef var_dim_type_data data_type;
+    typedef var_dim_type_arrmeta metadata_type;
   };
 
   /**
@@ -155,8 +139,7 @@ namespace ndt {
    *              is written to.
    * \param count  The number of elements to start off with.
    */
-  void var_dim_element_initialize(const type &tp, const char *arrmeta,
-                                  char *data, intptr_t count);
+  void var_dim_element_initialize(const type &tp, const char *arrmeta, char *data, intptr_t count);
 
   /**
    * A helper function for resizing the allocated space in a var dim
@@ -171,8 +154,7 @@ namespace ndt {
    *              is written to.
    * \param count  The number of elements to resize to.
    */
-  void var_dim_element_resize(const type &tp, const char *arrmeta, char *data,
-                              intptr_t count);
+  void var_dim_element_resize(const type &tp, const char *arrmeta, char *data, intptr_t count);
 
 } // namespace dynd::ndt
 } // namespace dynd
