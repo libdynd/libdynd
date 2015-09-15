d39e8a38ac67aec00eba1ddb390f1defa10935b2
Irwin Zaid <izaid@users.noreply.github.com>
2015-09-14 18:40:10 -0700

    Update trigger_python.sh

--- trigger_python.sh
+++ trigger_python.sh
@@ -1,3 +1,8 @@
+declare exitCode
+curl -sSL https://raw.githubusercontent.com/alrra/travis-after-all/1.4.1/lib/travis-after-all.js | node
+exitCode=$?
+#if [ $exitCode -eq 1 ]; then ./trigger_python.sh $TRAVIS_TOKEN; fi
+
 body='{
 "request": {
   "branch":"master"
@@ -7,6 +12,6 @@ curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
-  -H "Authorization: token $1" \
+  -H "Authorization: token $TRAVIS_TOKEN" \
   -d "$body" \
   https://api.travis-ci.org/repo/libdynd%2Fdynd-python/requests
