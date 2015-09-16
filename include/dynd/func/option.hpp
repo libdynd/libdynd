a120a4a9e75193834078b8f5f30ef1841991e0e4
Ian Henriksen <insertinterestingnamehere@gmail.com>
2015-09-16 14:24:22 -0600

    Export is_avail arrfunc.

--- include/dynd/func/option.hpp
+++ include/dynd/func/option.hpp
@@ -10,9 +10,9 @@
 namespace dynd {
 namespace nd {
 
-  extern struct is_avail : declfunc<is_avail> {
-    static callable children[DYND_TYPE_ID_MAX + 1];
-    static callable dim_children[2];
+  extern DYND_API struct is_avail : declfunc<is_avail> {
+    static DYND_API callable children[DYND_TYPE_ID_MAX + 1];
+    static DYND_API callable dim_children[2];
 
     static callable &get_child(const ndt::type &value_tp)
     {
@@ -20,11 +20,11 @@ namespace nd {
       return children[value_tp.get_type_id()];
     }
 
-    static callable make();
+    static DYND_API callable make();
   } is_avail;
 
-  extern struct assign_na_decl : declfunc<assign_na_decl> {
-    static callable children[DYND_TYPE_ID_MAX + 1];
+  extern DYND_API struct assign_na_decl : declfunc<assign_na_decl> {
+    static DYND_API callable children[DYND_TYPE_ID_MAX + 1];
 
     static callable &get_child(const ndt::type &value_tp)
     {
@@ -32,7 +32,7 @@ namespace nd {
       return children[value_tp.get_type_id()];
     }
 
-    static callable make();
+    static DYND_API callable make();
   } assign_na_decl;
 
 } // namespace dynd::nd
