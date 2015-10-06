03ba72e0b482f68eb579dfa9bbad143d70bed41f
izaid <hi@irwinzaid.com>
2015-10-06 11:10:03 +0100

    Travis CI Clang attempt #15

--- thirdparty/benchmark/include/benchmark/benchmark_api.h
+++ thirdparty/benchmark/include/benchmark/benchmark_api.h
@@ -143,7 +143,7 @@ BENCHMARK(BM_MultiThreaded)->Threads(4);
 #define BENCHMARK_BENCHMARK_API_H_
 
 #include <assert.h>
-#include <cstddef>
+#include <stddef.h>
 #include <stdint.h>
 
 #include "macros.h"
