c75a49571311a34c382149c239dfd75564ed1119
Ian Henriksen <insertinterestingnamehere@gmail.com>
2015-10-16 15:45:40 -0600

    Added bitwise not operator for bool1.

--- include/dynd/bool1.hpp
+++ include/dynd/bool1.hpp
@@ -46,6 +46,10 @@ public:
     return bool1(m_value == 0);
   }
 
+  DYND_CUDA_HOST_DEVICE inline bool1 operator~() const {
+    return bool1(m_value == 0);
+  }
+
   DYND_CUDA_HOST_DEVICE bool1 &operator+=(bool1 rhs)
   {
     m_value += rhs.m_value;
