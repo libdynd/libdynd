9ea71e130d353abcd11bdbe56ac4ce32638bdda7
Irwin Zaid <hi@irwinzaid.com>
2015-11-25 05:53:19 -0600

    Removed some gfuncs

--- src/dynd/types/builtin_type_properties.cpp
+++ src/dynd/types/builtin_type_properties.cpp
@@ -11,19 +11,176 @@
 using namespace std;
 using namespace dynd;
 
+namespace dynd {
+namespace nd {
+
+  struct complex_real_kernel : nd::base_kernel<complex_real_kernel> {
+    static const size_t data_size = 0;
+
+    array self;
+
+    complex_real_kernel(const array &self) : self(self)
+    {
+    }
+
+    void single(array *dst, array *const *DYND_UNUSED(src))
+    {
+      *dst = helper(self);
+    }
+
+    static void resolve_dst_type(char *DYND_UNUSED(static_data), size_t DYND_UNUSED(data_size), char *DYND_UNUSED(data),
+                                 ndt::type &dst_tp, intptr_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
+                                 intptr_t DYND_UNUSED(nkwd), const array *kwds,
+                                 const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars))
+    {
+      dst_tp = helper(kwds[0]).get_type();
+    }
+
+    static intptr_t instantiate(char *DYND_UNUSED(static_data), size_t DYND_UNUSED(data_size), char *DYND_UNUSED(data),
+                                void *ckb, intptr_t ckb_offset, const ndt::type &DYND_UNUSED(dst_tp),
+                                const char *DYND_UNUSED(dst_arrmeta), intptr_t DYND_UNUSED(nsrc),
+                                const ndt::type *DYND_UNUSED(src_tp), const char *const *DYND_UNUSED(src_arrmeta),
+                                kernel_request_t kernreq, const eval::eval_context *DYND_UNUSED(ectx),
+                                intptr_t DYND_UNUSED(nkwd), const array *kwds,
+                                const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars))
+    {
+      complex_real_kernel::make(ckb, kernreq, ckb_offset, kwds[0]);
+      return ckb_offset;
+    }
+
+    static nd::array helper(const nd::array &n)
+    {
+      return n.replace_dtype(ndt::property_type::make(n.get_dtype(), "real"));
+    }
+  };
+
+  struct complex_imag_kernel : nd::base_kernel<complex_imag_kernel> {
+    static const size_t data_size = 0;
+
+    array self;
+
+    complex_imag_kernel(const array &self) : self(self)
+    {
+    }
+
+    void single(array *dst, array *const *DYND_UNUSED(src))
+    {
+      *dst = helper(self);
+    }
+
+    static void resolve_dst_type(char *DYND_UNUSED(static_data), size_t DYND_UNUSED(data_size), char *DYND_UNUSED(data),
+                                 ndt::type &dst_tp, intptr_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
+                                 intptr_t DYND_UNUSED(nkwd), const array *kwds,
+                                 const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars))
+    {
+      dst_tp = helper(kwds[0]).get_type();
+    }
+
+    static intptr_t instantiate(char *DYND_UNUSED(static_data), size_t DYND_UNUSED(data_size), char *DYND_UNUSED(data),
+                                void *ckb, intptr_t ckb_offset, const ndt::type &DYND_UNUSED(dst_tp),
+                                const char *DYND_UNUSED(dst_arrmeta), intptr_t DYND_UNUSED(nsrc),
+                                const ndt::type *DYND_UNUSED(src_tp), const char *const *DYND_UNUSED(src_arrmeta),
+                                kernel_request_t kernreq, const eval::eval_context *DYND_UNUSED(ectx),
+                                intptr_t DYND_UNUSED(nkwd), const array *kwds,
+                                const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars))
+    {
+      complex_imag_kernel::make(ckb, kernreq, ckb_offset, kwds[0]);
+      return ckb_offset;
+    }
+
+    static nd::array helper(const nd::array &n)
+    {
+      return n.replace_dtype(ndt::property_type::make(n.get_dtype(), "imag"));
+    }
+  };
+
+  struct complex_conj_kernel : nd::base_kernel<complex_conj_kernel> {
+    static const size_t data_size = 0;
+
+    array self;
+
+    complex_conj_kernel(const array &self) : self(self)
+    {
+    }
+
+    void single(array *dst, array *const *DYND_UNUSED(src))
+    {
+      *dst = helper(self);
+    }
+
+    static void resolve_dst_type(char *DYND_UNUSED(static_data), size_t DYND_UNUSED(data_size), char *DYND_UNUSED(data),
+                                 ndt::type &dst_tp, intptr_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
+                                 intptr_t DYND_UNUSED(nkwd), const array *kwds,
+                                 const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars))
+    {
+      dst_tp = helper(kwds[0]).get_type();
+    }
+
+    static intptr_t instantiate(char *DYND_UNUSED(static_data), size_t DYND_UNUSED(data_size), char *DYND_UNUSED(data),
+                                void *ckb, intptr_t ckb_offset, const ndt::type &DYND_UNUSED(dst_tp),
+                                const char *DYND_UNUSED(dst_arrmeta), intptr_t DYND_UNUSED(nsrc),
+                                const ndt::type *DYND_UNUSED(src_tp), const char *const *DYND_UNUSED(src_arrmeta),
+                                kernel_request_t kernreq, const eval::eval_context *DYND_UNUSED(ectx),
+                                intptr_t DYND_UNUSED(nkwd), const array *kwds,
+                                const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars))
+    {
+      complex_conj_kernel::make(ckb, kernreq, ckb_offset, kwds[0]);
+      return ckb_offset;
+    }
+
+    static nd::array helper(const nd::array &n)
+    {
+      return n.replace_dtype(ndt::property_type::make(n.get_dtype(), "conj"));
+    }
+  };
+
+} // namespace dynd::nd
+
+namespace ndt {
+
+  template <>
+  struct type::equivalent<nd::complex_real_kernel> {
+    static type make()
+    {
+      return type("(self: Any) -> Any");
+    }
+  };
+
+  template <>
+  struct type::equivalent<nd::complex_imag_kernel> {
+    static type make()
+    {
+      return type("(self: Any) -> Any");
+    }
+  };
+
+  template <>
+  struct type::equivalent<nd::complex_conj_kernel> {
+    static type make()
+    {
+      return type("(self: Any) -> Any");
+    }
+  };
+
+} // namespace dynd::ndt
+} // namespace dynd
+
 static nd::array property_complex_real(const nd::array &n)
 {
-  return n.replace_dtype(ndt::property_type::make(n.get_dtype(), "real"));
+  nd::callable f = nd::callable::make<nd::complex_real_kernel>();
+  return f(kwds("self", n));
 }
 
 static nd::array property_complex_imag(const nd::array &n)
 {
-  return n.replace_dtype(ndt::property_type::make(n.get_dtype(), "imag"));
+  nd::callable f = nd::callable::make<nd::complex_imag_kernel>();
+  return f(kwds("self", n));
 }
 
 static nd::array property_complex_conj(const nd::array &n)
 {
-  return n.replace_dtype(ndt::property_type::make(n.get_dtype(), "conj"));
+  nd::callable f = nd::callable::make<nd::complex_conj_kernel>();
+  return f(kwds("self", n));
 }
 
 static size_t complex_array_properties_size()
