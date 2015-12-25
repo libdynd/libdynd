2c7ef7ae7b14554a9c95f4db6cef2b1828103b27
Irwin Zaid <hi@irwinzaid.com>
2015-12-25 14:30:39 -0600

    Committing progress on var dim init

--- include/dynd/array.hpp
+++ include/dynd/array.hpp
@@ -149,6 +149,167 @@ namespace nd {
     }
   };
 
+  namespace detail {
+
+    template <typename CArrayType, bool IsTriviallyCopyable>
+    struct init_from_c_array;
+
+    /*
+        template <typename ValueType, size_t Size>
+        struct init_from_c_array<ValueType[Size], true> {
+          init_from_c_array(const ndt::type &DYND_UNUSED(tp), const char *DYND_UNUSED(metadata)) {}
+
+          void single(char *data, const ValueType(&values)[Size]) const { memcpy(data, values, Size *
+       sizeof(ValueType)); }
+        };
+    */
+
+    template <typename ValueType, size_t Size>
+    struct init_from_c_array<ValueType[Size], true> {
+      nd::init<ValueType> child;
+      intptr_t stride;
+
+      init_from_c_array(const ndt::type &tp, const char *metadata)
+          : child(tp.extended<ndt::base_dim_type>()->get_element_type(), metadata + sizeof(size_stride_t)),
+            stride(reinterpret_cast<const size_stride_t *>(metadata)->stride)
+      {
+      }
+
+      void single(char *data, const ValueType(&values)[Size]) const
+      {
+        for (const ValueType &value : values) {
+          child.single(data, value);
+          data += stride;
+        }
+      }
+    };
+
+    template <typename ValueType, size_t Size>
+    struct init_from_c_array<ValueType[Size], false> {
+      nd::init<ValueType> child;
+      intptr_t stride;
+
+      init_from_c_array(const ndt::type &tp, const char *metadata)
+          : child(tp.extended<ndt::base_dim_type>()->get_element_type(), metadata + sizeof(size_stride_t)),
+            stride(reinterpret_cast<const size_stride_t *>(metadata)->stride)
+      {
+      }
+
+      void single(char *data, const ValueType(&values)[Size]) const
+      {
+        for (const ValueType &value : values) {
+          child.single(data, value);
+          data += stride;
+        }
+      }
+    };
+
+  } // namespace dynd::nd::detail
+
+  template <typename ValueType, size_t Size>
+  struct init<ValueType[Size]>
+      : detail::init_from_c_array<ValueType[Size],
+                                  std::is_pod<ValueType>::value && ndt::traits<ValueType>::is_same_layout> {
+    using detail::init_from_c_array<ValueType[Size], std::is_pod<ValueType>::value &&
+                                                         ndt::traits<ValueType>::is_same_layout>::init_from_c_array;
+  };
+
+  template <typename ContainerType, size_t Rank>
+  struct container_init {
+    typedef typename ContainerType::value_type value_type;
+
+    void (*func)(const container_init *, char *, const ContainerType &);
+    intptr_t stride;
+    init<value_type> child;
+
+    container_init(const ndt::type &tp, const char *metadata)
+        : child(tp.extended<ndt::base_dim_type>()->get_element_type(),
+                metadata + tp.extended<ndt::base_dim_type>()->get_element_arrmeta_offset())
+    {
+      switch (tp.get_type_id()) {
+      case fixed_dim_type_id:
+        func = [](const container_init *self, char *data, const ContainerType &values) {
+          for (const value_type &value : values) {
+            self->child.single(data, value);
+            data += self->stride;
+          }
+        };
+        stride = reinterpret_cast<const size_stride_t *>(metadata)->stride;
+        break;
+      case var_dim_type_id:
+        func = [](const container_init *self, char *data, const ContainerType &values) {
+          for (const value_type &value : values) {
+            self->child.single(reinterpret_cast<ndt::var_dim_type::data_type *>(data)->begin, value);
+            data += self->stride;
+          }
+        };
+        stride = reinterpret_cast<const ndt::var_dim_type::metadata_type *>(metadata)->stride;
+        break;
+      default:
+        throw std::runtime_error("unexpected type id");
+      }
+    }
+
+    void single(char *data, const ContainerType &values) const { func(this, data, values); }
+  };
+
+  template <typename ValueType>
+  struct container_init<std::initializer_list<ValueType>, 1> {
+    typedef ValueType value_type;
+
+    void (*func)(const container_init *, char *, const std::initializer_list<ValueType> &);
+    init<value_type> child;
+
+    container_init(const ndt::type &tp, const char *metadata)
+        : child(tp.extended<ndt::base_dim_type>()->get_element_type(), metadata + sizeof(size_stride_t))
+    {
+      switch (tp.get_type_id()) {
+      case fixed_dim_type_id:
+        func = [](const container_init *self, char *data, const std::initializer_list<ValueType> &values) {
+          self->child.contiguous(data, values.begin(), values.size());
+        };
+        break;
+      case var_dim_type_id:
+        func = [](const container_init *self, char *data, const std::initializer_list<ValueType> &values) {
+//          memory_block_data::api *allocator = self->memblock->get_api();
+//          dst_vddd->begin = allocator->allocate(memblock, dim_size);
+          self->child.contiguous(reinterpret_cast<ndt::var_dim_type::data_type *>(data)->begin, values.begin(),
+                                 values.size());
+        };
+        break;
+      default:
+        throw std::runtime_error("unexpected type id");
+      }
+    }
+
+    void single(char *data, const std::initializer_list<ValueType> &values) const { func(this, data, values); }
+  };
+
+  template <typename ContainerType>
+  struct container_init<ContainerType, 1> {
+    typedef typename ContainerType::value_type value_type;
+
+    void (*func)(const container_init *, char *, const ContainerType &);
+    init<value_type> child;
+
+    container_init(const ndt::type &tp, const char *metadata)
+        : child(tp.extended<ndt::base_dim_type>()->get_element_type(), metadata + sizeof(size_stride_t))
+    {
+    }
+
+    void single(char *data, const ContainerType &values) const { child.contiguous(data, values.data(), values.size()); }
+  };
+
+  template <typename T>
+  struct init<std::initializer_list<T>> : container_init<std::initializer_list<T>, ndt::traits<T>::ndim + 1> {
+    using container_init<std::initializer_list<T>, ndt::traits<T>::ndim + 1>::container_init;
+  };
+
+  template <typename T>
+  struct init<std::vector<T>> : container_init<std::vector<T>, ndt::traits<T>::ndim + 1> {
+    using container_init<std::vector<T>, ndt::traits<T>::ndim + 1>::container_init;
+  };
+
   template <typename ValueType>
   struct as {
     void single(ValueType &value, char *data) const { value = *reinterpret_cast<ValueType *>(data); }
