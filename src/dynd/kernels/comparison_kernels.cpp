0fc6d95d23d7e0daf78be585d9338ad909828d3b
Irwin Zaid <hi@irwinzaid.com>
2016-01-24 10:50:00 -0600

    Fix for GCC

--- src/dynd/kernels/comparison_kernels.cpp
+++ src/dynd/kernels/comparison_kernels.cpp
@@ -62,15 +62,15 @@ static kernel_single_t compare_kernel_table[builtin_type_id_count - 2][builtin_t
 #undef INNER_LEVEL
 };
 
-void dynd::make_builtin_type_comparison_kernel(nd::kernel_builder *ckb, intptr_t ckb_offset, type_id_t src0_type_id,
-                                               type_id_t src1_type_id, comparison_type_t comptype)
+void dynd::make_builtin_type_comparison_kernel(nd::kernel_builder *ckb, intptr_t DYND_UNUSED(ckb_offset),
+                                               type_id_t src0_type_id, type_id_t src1_type_id,
+                                               comparison_type_t comptype)
 {
   // Do a table lookup for the built-in range of dynd types
   if (src0_type_id >= bool_type_id && src0_type_id <= complex_float64_type_id && src1_type_id >= bool_type_id &&
       src1_type_id <= complex_float64_type_id && comptype >= 0 && comptype <= comparison_type_greater) {
     // No need to reserve more space, the space for a leaf is already there
     ckernel_prefix *result = ckb->alloc_ck<ckernel_prefix>();
-    ckb_offset = ckb->m_size;
     result->function = reinterpret_cast<void *>(
         compare_kernel_table[src0_type_id - bool_type_id][src1_type_id - bool_type_id][comptype]);
   }
