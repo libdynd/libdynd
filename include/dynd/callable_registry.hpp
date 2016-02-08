fee8cd180c9171aac0869d36bf2769231c2862e2
Irwin Zaid <hi@irwinzaid.com>
2016-02-08 15:41:45 -0600

    Another fix for MSVC

--- include/dynd/callable_registry.hpp
+++ include/dynd/callable_registry.hpp
@@ -14,19 +14,17 @@ namespace nd {
     //    std::map<std::string, callable> m_map;
 
   public:
-    callable_registry() {}
-
     /**
      * Returns a reference to the map of registered callables.
      * NOTE: The internal representation will change, this
      *       function will change.
      */
-    std::map<std::string, nd::callable> &get_regfunctions();
+    DYND_API std::map<std::string, nd::callable> &get_regfunctions();
 
     /**
       * Looks up a named callable from the registry.
       */
-    callable &operator[](const std::string &name);
+    DYND_API callable &operator[](const std::string &name);
   } callable_registry;
 
 } // namespace dynd::nd
