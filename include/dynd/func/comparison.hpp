e4f33df94aa2b996f5ef5676c38026d8f3292a21
Irwin Zaid <hi@irwinzaid.com>
2016-02-14 12:48:21 -0600

    Moved comparison_operator functions into src/func/comparison.cpp

--- include/dynd/func/comparison.hpp
+++ include/dynd/func/comparison.hpp
@@ -11,62 +11,10 @@
 namespace dynd {
 namespace nd {
 
-  template <typename FuncType, template <type_id_t...> class KernelType>
-  struct comparison_operator : declfunc<FuncType> {
-    static decltype(auto) make_children()
-    {
-      typedef type_id_sequence<bool_id, int8_id, int16_id, int32_id, int64_id, uint8_id, uint16_id, uint32_id,
-                               uint64_id, float32_id, float64_id> numeric_ids;
-
-      auto children = callable::make_all<KernelType, numeric_ids, numeric_ids>();
-
-      callable self = functional::call<FuncType>(ndt::type("(Any, Any) -> Any"));
-
-      for (type_id_t i0 : i2a<numeric_ids>()) {
-        for (type_id_t i1 : i2a<dim_ids>()) {
-          const ndt::type child_tp = ndt::callable_type::make(ndt::type("Any"), {ndt::type(i0), ndt::type(i1)});
-          children[{{i0, i1}}] = functional::elwise(child_tp, self);
-        }
-      }
-
-      for (type_id_t i : i2a<numeric_ids>()) {
-        children[{{option_id, i}}] = functional::forward_na<0>(self);
-        children[{{i, option_id}}] = functional::forward_na<1>(self);
-      }
-      children[{{option_id, option_id}}] = callable::make<option_comparison_kernel<FuncType, true, true>>();
-
-      for (type_id_t dim_tp_id : i2a<dim_ids>()) {
-        children[{{dim_tp_id, option_id}}] = functional::elwise(self);
-        children[{{option_id, dim_tp_id}}] = functional::elwise(self);
-      }
-
-      for (type_id_t i0 : i2a<dim_ids>()) {
-        typedef join<numeric_ids, dim_ids>::type type_ids;
-        for (type_id_t i1 : i2a<type_ids>()) {
-          const ndt::type child_tp = ndt::callable_type::make(ndt::type("Any"), {ndt::type(i0), ndt::type(i1)});
-          children[{{i0, i1}}] = functional::elwise(child_tp, self);
-        }
-      }
-
-      children[{{string_id, string_id}}] = callable::make<KernelType<string_id, string_id>>();
-
-      return children;
-    }
-
-    static callable make()
-    {
-      auto children = FuncType::make_children();
-      return functional::dispatch(ndt::type("(Any, Any) -> Any"),
-                                  [children](const ndt::type &DYND_UNUSED(dst_tp), intptr_t DYND_UNUSED(nsrc),
-                                             const ndt::type *src_tp) mutable -> callable & {
-                                    callable &child = children[{{src_tp[0].get_id(), src_tp[1].get_id()}}];
-                                    if (child.is_null()) {
-                                      throw std::runtime_error("no child found");
-                                    }
-
-                                    return child;
-                                  });
-    }
+  template <typename SelfType, template <type_id_t...> class KernelType>
+  struct comparison_operator : declfunc<SelfType> {
+    static std::map<std::array<type_id_t, 2>, callable> make_children();
+    static callable make();
   };
 
   extern DYND_API struct DYND_API less : comparison_operator<less, less_kernel> {
--- src/dynd/func/comparison.cpp
+++ src/dynd/func/comparison.cpp
@@ -8,6 +8,63 @@
 using namespace std;
 using namespace dynd;
 
+template <typename SelfType, template <type_id_t...> class KernelType>
+map<array<type_id_t, 2>, nd::callable> nd::comparison_operator<SelfType, KernelType>::make_children()
+{
+  typedef type_id_sequence<bool_id, int8_id, int16_id, int32_id, int64_id, uint8_id, uint16_id, uint32_id, uint64_id,
+                           float32_id, float64_id> numeric_ids;
+
+  auto children = callable::make_all<KernelType, numeric_ids, numeric_ids>();
+
+  callable self = functional::call<SelfType>(ndt::type("(Any, Any) -> Any"));
+
+  for (type_id_t i0 : i2a<numeric_ids>()) {
+    for (type_id_t i1 : i2a<dim_ids>()) {
+      const ndt::type child_tp = ndt::callable_type::make(ndt::type("Any"), {ndt::type(i0), ndt::type(i1)});
+      children[{{i0, i1}}] = functional::elwise(child_tp, self);
+    }
+  }
+
+  for (type_id_t i : i2a<numeric_ids>()) {
+    children[{{option_id, i}}] = functional::forward_na<0>(self);
+    children[{{i, option_id}}] = functional::forward_na<1>(self);
+  }
+  children[{{option_id, option_id}}] = callable::make<option_comparison_kernel<SelfType, true, true>>();
+
+  for (type_id_t dim_tp_id : i2a<dim_ids>()) {
+    children[{{dim_tp_id, option_id}}] = functional::elwise(self);
+    children[{{option_id, dim_tp_id}}] = functional::elwise(self);
+  }
+
+  for (type_id_t i0 : i2a<dim_ids>()) {
+    typedef join<numeric_ids, dim_ids>::type type_ids;
+    for (type_id_t i1 : i2a<type_ids>()) {
+      const ndt::type child_tp = ndt::callable_type::make(ndt::type("Any"), {ndt::type(i0), ndt::type(i1)});
+      children[{{i0, i1}}] = functional::elwise(child_tp, self);
+    }
+  }
+
+  children[{{string_id, string_id}}] = callable::make<KernelType<string_id, string_id>>();
+
+  return children;
+}
+
+template <typename SelfType, template <type_id_t...> class KernelType>
+nd::callable nd::comparison_operator<SelfType, KernelType>::make()
+{
+  auto children = SelfType::make_children();
+  return functional::dispatch(ndt::type("(Any, Any) -> Any"),
+                              [children](const ndt::type &DYND_UNUSED(dst_tp), intptr_t DYND_UNUSED(nsrc),
+                                         const ndt::type *src_tp) mutable -> callable & {
+                                callable &child = children[{{src_tp[0].get_id(), src_tp[1].get_id()}}];
+                                if (child.is_null()) {
+                                  throw std::runtime_error("no child found");
+                                }
+
+                                return child;
+                              });
+}
+
 DYND_API struct nd::less nd::less;
 
 nd::array nd::operator<(const array &a0, const array &a1) { return less(a0, a1); }
