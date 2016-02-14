62d8cfae90a6191fde11ff7c4c113d59349d60d7
Irwin Zaid <hi@irwinzaid.com>
2016-02-14 12:30:33 -0600

    Removed the OS X deployment target

--- benchmarks/benchmark_dispatch_map.cpp
+++ benchmarks/benchmark_dispatch_map.cpp
@@ -18,25 +18,35 @@
 using namespace std;
 using namespace dynd;
 
-static void BM_Unary_Dispatch(benchmark::State &state)
-{
-  default_random_engine generator;
-  uniform_int_distribution<underlying_type_t<type_id_t>> d(bool_id, ndt::type_registry.max());
+class DispatchFixture : public ::benchmark::Fixture {
+  static const size_t N = 100000;
 
+public:
+  type_id_t ids[N];
+
+  void SetUp()
+  {
+    default_random_engine generator;
+    uniform_int_distribution<underlying_type_t<type_id_t>> d(bool_id, float64_id);
+
+    for (type_id_t &id : ids) {
+      id = static_cast<type_id_t>(d(generator));
+    }
+  }
+};
+
+BENCHMARK_F(DispatchFixture, BM_Unary_Dispatch)(benchmark::State &state)
+{
   dispatch_map<int, 1> map{{any_kind_id, 0}, {scalar_kind_id, 1}, {bool_id, 2},     {int8_id, 3},     {int16_id, 4},
                            {int32_id, 5},    {int64_id, 6},       {int128_id, 7},   {uint8_id, 8},    {uint16_id, 9},
                            {uint32_id, 10},  {uint64_id, 11},     {uint128_id, 12}, {float32_id, 13}, {float64_id, 14}};
 
   while (state.KeepRunning()) {
-    state.PauseTiming();
-    type_id_t id = static_cast<type_id_t>(d(generator));
-    state.ResumeTiming();
-
-    benchmark::DoNotOptimize(map[id]);
+    for (const type_id_t &id : ids) {
+      benchmark::DoNotOptimize(map[id]);
+    }
   }
 }
-
-BENCHMARK(BM_Unary_Dispatch);
 
 static void BM_Binary_Dispatch(benchmark::State &state)
 {
