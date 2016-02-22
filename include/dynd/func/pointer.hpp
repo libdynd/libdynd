e4a072898a1d82db34d480a77331c13a47fc2ffb
Stefan Krah <skrah@bytereef.org>
2016-02-22 11:32:09 +0100

    Add new files.

---
+++ include/dynd/func/pointer.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (C) 2011-15 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#pragma once
+
+#include <dynd/callable.hpp>
+
+namespace dynd {
+namespace nd {
+
+  extern DYND_API struct DYND_API dereference : declfunc<dereference> {
+    static callable make();
+  } dereference;
+
+} // namespace dynd::nd
+} // namespace dynd
