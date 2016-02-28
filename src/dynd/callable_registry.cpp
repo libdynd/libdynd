1db9330d1767aebe8df7e57962189c34a3909005
Irwin Zaid <hi@irwinzaid.com>
2016-02-28 19:33:29 +0000

    Removed duplicate functions from the callable_registry

--- src/dynd/callable_registry.cpp
+++ src/dynd/callable_registry.cpp
@@ -11,6 +11,7 @@
 #include <dynd/func/arithmetic.hpp>
 #include <dynd/func/assignment.hpp>
 #include <dynd/io.hpp>
+#include <dynd/math.hpp>
 #include <dynd/option.hpp>
 #include <dynd/func/random.hpp>
 #include <dynd/func/sum.hpp>
@@ -100,49 +101,20 @@ std::map<std::string, nd::callable> &nd::callable_registry::get_regfunctions()
 {
   static map<std::string, nd::callable> registry;
   if (registry.empty()) {
-    registry["divide"] =
-        make_ufunc(::divide<int32_t>(), ::divide<int64_t>(), ::divide<uint32_t>(), ::divide<uint64_t>(),
-                   ::divide<float>(), ::divide<double>(), ::divide<complex<float>>(), ::divide<complex<double>>());
     registry["negative"] = make_ufunc(negative<int32_t>(), negative<int64_t>(), negative<int128>(), negative<float>(),
                                       negative<double>(), negative<complex<float>>(), negative<complex<double>>());
     registry["sign"] = make_ufunc(sign<int32_t>(), sign<int64_t>(), sign<int128>(), sign<float>(), sign<double>());
-    registry["conj"] = make_ufunc(conj_fn<std::complex<float>>(), conj_fn<std::complex<double>>());
 
-#if !(defined(_MSC_VER) && _MSC_VER < 1700)
-    registry["logaddexp"] = make_ufunc(logaddexp<float>(), logaddexp<double>());
-    registry["logaddexp2"] = make_ufunc(logaddexp2<float>(), logaddexp2<double>());
-#endif
-
-    // Trig functions
-    registry["sin"] = make_ufunc(&::sinf, static_cast<double (*)(double)>(&::sin));
-    registry["cos"] = make_ufunc(&::cosf, static_cast<double (*)(double)>(&::cos));
-    registry["tan"] = make_ufunc(&::tanf, static_cast<double (*)(double)>(&::tan));
-    registry["exp"] = make_ufunc(&::expf, static_cast<double (*)(double)>(&::exp));
-    registry["arcsin"] = make_ufunc(&::asinf, static_cast<double (*)(double)>(&::asin));
-    registry["arccos"] = make_ufunc(&::acosf, static_cast<double (*)(double)>(&::acos));
-    registry["arctan"] = make_ufunc(&::atanf, static_cast<double (*)(double)>(&::atan));
-    registry["arctan2"] = make_ufunc(&::atan2f, static_cast<double (*)(double, double)>(&::atan2));
-    registry["hypot"] = make_ufunc(&::hypotf, static_cast<double (*)(double, double)>(&::hypot));
-    registry["sinh"] = make_ufunc(&::sinhf, static_cast<double (*)(double)>(&::sinh));
-    registry["cosh"] = make_ufunc(&::coshf, static_cast<double (*)(double)>(&::cosh));
-    registry["tanh"] = make_ufunc(&::tanhf, static_cast<double (*)(double)>(&::tanh));
-#if !(defined(_MSC_VER) && _MSC_VER < 1700)
-    registry["asinh"] = make_ufunc(&::asinhf, static_cast<double (*)(double)>(&::asinh));
-    registry["acosh"] = make_ufunc(&::acoshf, static_cast<double (*)(double)>(&::acosh));
-    registry["atanh"] = make_ufunc(&::atanhf, static_cast<double (*)(double)>(&::atanh));
-#endif
-
-    registry["power"] = make_ufunc(&powf, static_cast<double (*)(double, double)>(&::pow));
-
-    registry["take"] = nd::take::get();
-    registry["sum"] = nd::sum::get();
-    registry["min"] = nd::min::get();
-    registry["max"] = nd::max::get();
+    registry["take"] = take::get();
+    registry["sum"] = sum::get();
+    registry["min"] = min::get();
+    registry["max"] = max::get();
 
     // dynd/arithmetic.hpp
     registry["add"] = add::get();
     registry["subtract"] = subtract::get();
     registry["multiply"] = multiply::get();
+    registry["divide"] = divide::get();
 
     // dynd/assign.hpp
     registry["assign"] = assign::get();
@@ -154,6 +126,12 @@ std::map<std::string, nd::callable> &nd::callable_registry::get_regfunctions()
 
     // dynd/io.hpp
     registry["serialize"] = serialize::get();
+
+    // dynd/math.hpp
+    registry["sin"] = sin::get();
+    registry["cos"] = cos::get();
+    registry["tan"] = tan::get();
+    registry["exp"] = exp::get();
 
     // dynd/option.hpp
     registry["assign_na"] = assign_na::get();
