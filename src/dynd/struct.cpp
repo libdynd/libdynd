70ea6a5cc9fd3aad989ff8208ccd22594acaab40
Stefan Krah <skrah@bytereef.org>
2016-03-07 17:18:32 +0100

    make_field_access_kernel(): convert assert() into an exception.

--- src/dynd/struct.cpp
+++ src/dynd/struct.cpp
@@ -22,9 +22,11 @@ DYND_API struct nd::field_access nd::field_access;
 // Temporary solution until nd::field_access() handles views.
 DYND_API nd::callable nd::make_field_access_kernel(const ndt::type &dt, const std::string &name)
 {
-  assert(dt.get_id() == struct_id);
-  intptr_t i = dt.extended<ndt::struct_type>()->get_field_index(name);
+  if (dt.get_id() != struct_id) {
+    throw invalid_argument(std::string("no property named '") + name + "'");
+  }
 
+  intptr_t i = dt.extended<ndt::struct_type>()->get_field_index(name);
   if (i < 0) {
     throw std::invalid_argument("no field named '" + name + "'");
   }
