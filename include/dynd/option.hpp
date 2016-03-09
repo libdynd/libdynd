83d97beb9faa3bd492091d2b776408d94c5fe2a2
Irwin Zaid <izaid@users.noreply.github.com>
2016-03-09 12:15:07 +0000

    Update option.hpp

--- include/dynd/option.hpp
+++ include/dynd/option.hpp
@@ -15,16 +15,16 @@ namespace nd {
     static callable &get();
   } assign_na;
 
-  void old_assign_na(const ndt::type &option_tp, const char *arrmeta, char *data);
+  DYND_API void old_assign_na(const ndt::type &option_tp, const char *arrmeta, char *data);
 
   extern DYND_API struct DYND_API is_na : declfunc<is_na> {
     static callable make();
     static callable &get();
   } is_na;
 
-  bool old_is_avail(const ndt::type &option_tp, const char *arrmeta, const char *data);
+  DYND_API bool old_is_avail(const ndt::type &option_tp, const char *arrmeta, const char *data);
 
-  void set_option_from_utf8_string(const ndt::type &option_tp, const char *arrmeta, char *data, const char *utf8_begin,
+  DYND_API void set_option_from_utf8_string(const ndt::type &option_tp, const char *arrmeta, char *data, const char *utf8_begin,
                                    const char *utf8_end, const eval::eval_context *ectx);
 
   inline void set_option_from_utf8_string(const ndt::type &option_tp, const char *arrmeta, char *data,
