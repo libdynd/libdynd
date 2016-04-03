063471e5a3c740a9a45ac13b813ce87cb481c584
Irwin Zaid <hi@irwinzaid.com>
2016-04-03 00:01:26 -0500

    Switched over compose and constant

--- include/dynd/callables/compound_callable.hpp
+++ include/dynd/callables/compound_callable.hpp
@@ -19,23 +19,26 @@ namespace nd {
       left_compound_callable(const ndt::type &tp, const callable &child) : base_callable(tp), m_child(child) {}
 
       ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
-                        const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
-                        size_t DYND_UNUSED(nkwd), const array *DYND_UNUSED(kwds),
-                        const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
+                        const ndt::type &dst_tp, size_t nsrc, const ndt::type *src_tp, size_t nkwd, const array *kwds,
+                        const std::map<std::string, ndt::type> &tp_vars) {
         cg.emplace_back(this);
+
+        ndt::type child_src_tp[2] = {dst_tp, src_tp[0]};
+        m_child->resolve(this, nullptr, cg, dst_tp, nsrc + 1, child_src_tp, nkwd, kwds, tp_vars);
+
         return dst_tp;
       }
 
-      void instantiate(call_node *DYND_UNUSED(node), char *data, kernel_builder *ckb, const ndt::type &dst_tp,
-                       const char *dst_arrmeta, intptr_t nsrc, const ndt::type *src_tp, const char *const *src_arrmeta,
-                       kernel_request_t kernreq, intptr_t nkwd, const nd::array *kwds,
+      void instantiate(call_node *node, char *data, kernel_builder *ckb, const ndt::type &DYND_UNUSED(dst_tp),
+                       const char *dst_arrmeta, intptr_t nsrc, const ndt::type *DYND_UNUSED(src_tp),
+                       const char *const *src_arrmeta, kernel_request_t kernreq, intptr_t nkwd, const nd::array *kwds,
                        const std::map<std::string, ndt::type> &tp_vars) {
         ckb->emplace_back<left_compound_kernel>(kernreq);
 
-        ndt::type child_src_tp[2] = {dst_tp, src_tp[0]};
         const char *child_src_arrmeta[2] = {dst_arrmeta, src_arrmeta[0]};
-        m_child.get()->instantiate(nullptr, data, ckb, dst_tp, dst_arrmeta, nsrc + 1, child_src_tp, child_src_arrmeta,
-                                   kernreq | kernel_request_data_only, nkwd, kwds, tp_vars);
+        node = next(node);
+        node->callee->instantiate(node, data, ckb, ndt::type(), dst_arrmeta, nsrc + 1, nullptr, child_src_arrmeta,
+                                  kernreq | kernel_request_data_only, nkwd, kwds, tp_vars);
       }
     };
 
@@ -46,23 +49,26 @@ namespace nd {
       right_compound_callable(const ndt::type &tp, const callable &child) : base_callable(tp), m_child(child) {}
 
       ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
-                        const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
-                        size_t DYND_UNUSED(nkwd), const array *DYND_UNUSED(kwds),
-                        const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
+                        const ndt::type &dst_tp, size_t nsrc, const ndt::type *src_tp, size_t nkwd, const array *kwds,
+                        const std::map<std::string, ndt::type> &tp_vars) {
         cg.emplace_back(this);
+
+        ndt::type child_src_tp[2] = {src_tp[0], dst_tp};
+        m_child->resolve(this, nullptr, cg, dst_tp, nsrc + 1, child_src_tp, nkwd, kwds, tp_vars);
+
         return dst_tp;
       }
 
-      void instantiate(call_node *DYND_UNUSED(node), char *data, kernel_builder *ckb, const ndt::type &dst_tp,
-                       const char *dst_arrmeta, intptr_t nsrc, const ndt::type *src_tp, const char *const *src_arrmeta,
-                       kernel_request_t kernreq, intptr_t nkwd, const nd::array *kwds,
+      void instantiate(call_node *node, char *data, kernel_builder *ckb, const ndt::type &DYND_UNUSED(dst_tp),
+                       const char *dst_arrmeta, intptr_t nsrc, const ndt::type *DYND_UNUSED(src_tp),
+                       const char *const *src_arrmeta, kernel_request_t kernreq, intptr_t nkwd, const nd::array *kwds,
                        const std::map<std::string, ndt::type> &tp_vars) {
         ckb->emplace_back<right_compound_kernel>(kernreq);
 
-        ndt::type child_src_tp[2] = {src_tp[0], dst_tp};
         const char *child_src_arrmeta[2] = {src_arrmeta[0], dst_arrmeta};
-        m_child.get()->instantiate(nullptr, data, ckb, dst_tp, dst_arrmeta, nsrc + 1, child_src_tp, child_src_arrmeta,
-                                   kernreq | kernel_request_data_only, nkwd, kwds, tp_vars);
+        node = next(node);
+        node->callee->instantiate(node, data, ckb, ndt::type(), dst_arrmeta, nsrc + 1, nullptr, child_src_arrmeta,
+                                  kernreq | kernel_request_data_only, nkwd, kwds, tp_vars);
       }
     };
 
--- include/dynd/callables/constant_callable.hpp
+++ include/dynd/callables/constant_callable.hpp
@@ -25,22 +25,26 @@ namespace nd {
       ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
                         const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
                         size_t DYND_UNUSED(nkwd), const array *DYND_UNUSED(kwds),
-                        const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
+                        const std::map<std::string, ndt::type> &tp_vars) {
         cg.emplace_back(this);
+
+        nd::array error_mode = assign_error_default;
+        assign->resolve(this, nullptr, cg, dst_tp, 1, &dst_tp, 1, &error_mode, tp_vars);
+
         return dst_tp;
       }
 
-      void instantiate(call_node *DYND_UNUSED(node), char *DYND_UNUSED(data), kernel_builder *ckb,
-                       const ndt::type &dst_tp, const char *dst_arrmeta, intptr_t DYND_UNUSED(nsrc),
+      void instantiate(call_node *node, char *DYND_UNUSED(data), kernel_builder *ckb,
+                       const ndt::type &DYND_UNUSED(dst_tp), const char *dst_arrmeta, intptr_t DYND_UNUSED(nsrc),
                        const ndt::type *DYND_UNUSED(src_tp), const char *const *DYND_UNUSED(src_arrmeta),
                        kernel_request_t kernreq, intptr_t DYND_UNUSED(nkwd), const nd::array *DYND_UNUSED(kwds),
                        const std::map<std::string, ndt::type> &tp_vars) {
         ckb->emplace_back<constant_kernel>(kernreq, const_cast<char *>(m_val.cdata()));
 
-        nd::array error_mode = assign_error_default;
+        node = next(node);
         const char *child_src_metadata = m_val.get()->metadata();
-        assign->instantiate(nullptr, NULL, ckb, dst_tp, dst_arrmeta, 1, &dst_tp, &child_src_metadata,
-                            kernreq | kernel_request_data_only, 1, &error_mode, tp_vars);
+        node->callee->instantiate(node, NULL, ckb, ndt::type(), dst_arrmeta, 1, nullptr, &child_src_metadata,
+                                  kernreq | kernel_request_data_only, 0, nullptr, tp_vars);
       }
     };
 
