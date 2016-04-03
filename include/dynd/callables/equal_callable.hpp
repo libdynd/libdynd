7f50857a408dd7461725618cd0e689687d7536a5
Irwin Zaid <hi@irwinzaid.com>
2016-04-03 17:46:52 -0500

    Reenabled test_struct.cpp

--- include/dynd/callables/equal_callable.hpp
+++ include/dynd/callables/equal_callable.hpp
@@ -30,8 +30,6 @@ namespace nd {
     ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
                       const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *src_tp, size_t nkwd,
                       const array *kwds, const std::map<std::string, ndt::type> &tp_vars) {
-      std::cout << "equal_callable<tuple_id, tuple_id>::resolve" << std::endl;
-
       cg.emplace_back(this);
 
       size_t field_count = src_tp[0].extended<ndt::tuple_type>()->get_field_count();
