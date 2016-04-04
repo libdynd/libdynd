03277e22662587e23c9b7459b4b04c8c3fdc8dc6
Irwin Zaid <hi@irwinzaid.com>
2016-04-04 18:45:22 -0500

    Fix for FFT

--- include/dynd/callables/fft_callable.hpp
+++ include/dynd/callables/fft_callable.hpp
@@ -21,10 +21,9 @@ namespace nd {
                                   "?Fixed * int64, flags: ?int32) -> Fixed**N * "
                                   "complex[float64]")) {}
 
-    ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
+    ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &DYND_UNUSED(cg),
                       const ndt::type &dst_tp, size_t nsrc, const ndt::type *src_tp, size_t nkwd, const array *kwds,
                       const std::map<std::string, ndt::type> &tp_vars) {
-      cg.emplace_back(this);
       return resolve_dst_type_<std::is_same<fftw_src_type, double>::value>(dst_tp, nsrc, src_tp, nkwd, kwds, tp_vars);
     }
 
