0d36fbfe327624b7a0cb27bbf6e52ea99a4f7f21
Irwin Zaid <hi@irwinzaid.com>
2016-04-04 17:12:18 -0500

    Testing call graph destructors

--- include/dynd/callables/compose_callable.hpp
+++ include/dynd/callables/compose_callable.hpp
@@ -24,12 +24,13 @@ namespace nd {
       ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
                         const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *src_tp, size_t nkwd,
                         const array *kwds, const std::map<std::string, ndt::type> &tp_vars) {
-        cg.push_back([this](call_node *&node, kernel_builder *ckb, kernel_request_t kernreq, const char *dst_arrmeta,
-                            intptr_t DYND_UNUSED(nsrc), const char *const *src_arrmeta) {
+        cg.push_back([buffer_tp = m_buffer_tp](call_node * &node, kernel_builder * ckb, kernel_request_t kernreq,
+                                               const char *dst_arrmeta, intptr_t DYND_UNUSED(nsrc),
+                                               const char *const *src_arrmeta) {
           intptr_t ckb_offset = ckb->size();
 
           intptr_t root_ckb_offset = ckb_offset;
-          ckb->emplace_back<compose_kernel>(kernreq, this->m_buffer_tp);
+          ckb->emplace_back<compose_kernel>(kernreq, buffer_tp);
           node = next(node);
 
           ckb_offset = ckb->size();
