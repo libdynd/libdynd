90a14f718cb67d479edc6048ba54693cc990c616
Irwin Zaid <hi@irwinzaid.com>
2016-04-04 14:33:45 -0500

    Reenabled test_apply.cpp

--- include/dynd/callables/apply_callable_callable.hpp
+++ include/dynd/callables/apply_callable_callable.hpp
@@ -24,21 +24,17 @@ namespace nd {
 
       ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
                         const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
-                        size_t DYND_UNUSED(nkwd), const array *DYND_UNUSED(kwds),
-                        const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
-        cg.emplace_back(this);
-        return dst_tp;
-      }
-
-      void instantiate(call_node *&node, char *DYND_UNUSED(data), kernel_builder *ckb,
-                       const ndt::type &DYND_UNUSED(dst_tp), const char *DYND_UNUSED(dst_arrmeta),
-                       intptr_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp), const char *const *src_arrmeta,
-                       kernel_request_t kernreq, intptr_t nkwd, const nd::array *kwds,
-                       const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
+                        size_t nkwd, const array *kwds, const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
         typedef apply_callable_kernel<func_type, N> kernel_type;
-        ckb->emplace_back<kernel_type>(kernreq, m_func, typename kernel_type::args_type(src_arrmeta, kwds),
-                                       typename kernel_type::kwds_type(nkwd, kwds));
-        node = next(node);
+
+        cg.push_back([ func = m_func, kwds = typename kernel_type::kwds_type(nkwd, kwds) ](
+            call_node * &node, kernel_builder * ckb, kernel_request_t kernreq, const char *DYND_UNUSED(dst_arrmeta),
+            intptr_t DYND_UNUSED(nsrc), const char *const *src_arrmeta) {
+          ckb->emplace_back<kernel_type>(kernreq, func, typename kernel_type::args_type(src_arrmeta, nullptr), kwds);
+          node = next(node);
+        });
+
+        return dst_tp;
       }
     };
 
