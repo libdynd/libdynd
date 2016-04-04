04cc93617d81482e85aecc854075c9b38911a7ff
Irwin Zaid <hi@irwinzaid.com>
2016-04-03 19:44:49 -0500

    Reenabled test_array_compare

--- include/dynd/callables/dereference_callable.hpp
+++ include/dynd/callables/dereference_callable.hpp
@@ -11,9 +11,9 @@
 namespace dynd {
 namespace nd {
 
-  class dereference_callable : public base_callable {
+  class dereference_callable : public default_instantiable_callable<dereference_kernel> {
   public:
-    dereference_callable() : base_callable(ndt::type("(pointer[Any]) -> Any")) {}
+    dereference_callable() : default_instantiable_callable(ndt::type("(pointer[Any]) -> Any")) {}
 
     array alloc(const ndt::type *dst_tp) const { return empty_shell(*dst_tp); }
 
@@ -23,21 +23,6 @@ namespace nd {
                       const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
       cg.emplace_back(this);
       return src_tp[0].extended<ndt::pointer_type>()->get_target_type();
-    }
-
-    void resolve_dst_type(char *DYND_UNUSED(data), ndt::type &dst_tp, intptr_t DYND_UNUSED(nsrc),
-                          const ndt::type *src_tp, intptr_t DYND_UNUSED(nkwd), const nd::array *DYND_UNUSED(kwds),
-                          const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
-      dst_tp = src_tp[0].extended<ndt::pointer_type>()->get_target_type();
-    }
-
-    void instantiate(call_node *&node, char *DYND_UNUSED(data), kernel_builder *ckb, const ndt::type &dst_tp,
-                     const char *DYND_UNUSED(dst_arrmeta), intptr_t DYND_UNUSED(nsrc),
-                     const ndt::type *DYND_UNUSED(src_tp), const char *const *DYND_UNUSED(src_arrmeta),
-                     kernel_request_t kernreq, intptr_t DYND_UNUSED(nkwd), const array *DYND_UNUSED(kwds),
-                     const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
-      ckb->emplace_back<dereference_kernel>(kernreq, dst_tp);
-      node = next(node);
     }
   };
 
--- include/dynd/callables/serialize_callable.hpp
+++ include/dynd/callables/serialize_callable.hpp
@@ -14,23 +14,31 @@ namespace nd {
 
   template <type_id_t Arg0ID>
   class serialize_callable : public base_callable {
+    struct node_type : call_node {
+      size_t data_size;
+
+      node_type(base_callable *callee, size_t data_size) : call_node(callee), data_size(data_size) {}
+    };
+
   public:
-    serialize_callable() : base_callable(ndt::callable_type::make(ndt::type("bytes"), {ndt::type(Arg0ID)})) {}
+    serialize_callable()
+        : base_callable(ndt::callable_type::make(ndt::type("bytes"), {ndt::type(Arg0ID)}), sizeof(node_type)) {}
 
     ndt::type resolve(base_callable *DYND_UNUSED(caller), char *DYND_UNUSED(data), call_graph &cg,
-                      const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
+                      const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc), const ndt::type *src_tp,
                       size_t DYND_UNUSED(nkwd), const array *DYND_UNUSED(kwds),
                       const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
-      cg.emplace_back(this);
+      cg.emplace_back<node_type>(this, src_tp[0].get_data_size());
+
       return dst_tp;
     }
 
     void instantiate(call_node *&node, char *DYND_UNUSED(data), kernel_builder *ckb,
                      const ndt::type &DYND_UNUSED(dst_tp), const char *DYND_UNUSED(dst_arrmeta),
-                     intptr_t DYND_UNUSED(nsrc), const ndt::type *src_tp, const char *const *DYND_UNUSED(src_arrmeta),
-                     kernel_request_t kernreq, intptr_t DYND_UNUSED(nkwd), const array *DYND_UNUSED(kwds),
-                     const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
-      ckb->emplace_back<serialize_kernel<Arg0ID>>(kernreq, src_tp[0].get_data_size());
+                     intptr_t DYND_UNUSED(nsrc), const ndt::type *DYND_UNUSED(src_tp),
+                     const char *const *DYND_UNUSED(src_arrmeta), kernel_request_t kernreq, intptr_t DYND_UNUSED(nkwd),
+                     const array *DYND_UNUSED(kwds), const std::map<std::string, ndt::type> &DYND_UNUSED(tp_vars)) {
+      ckb->emplace_back<serialize_kernel<Arg0ID>>(kernreq, reinterpret_cast<node_type *>(node)->data_size);
       node = next(node);
     }
   };
