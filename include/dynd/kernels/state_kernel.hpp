720312a9c4ba85718cc9935a39c9a99c0c0769a7
izaid <hi@irwinzaid.com>
2016-04-16 15:30:02 -0500

    Added a strided specialization for state

--- include/dynd/kernels/state_kernel.hpp
+++ include/dynd/kernels/state_kernel.hpp
@@ -35,6 +35,23 @@ namespace nd {
 
       this->get_child()->single(dst, child_src);
     }
+
+    void strided(char *dst, intptr_t dst_stride, char *const *src, const intptr_t *src_stride, size_t end) {
+      char *child_src[NArg + 1];
+      intptr_t child_src_stride[NArg + 1];
+      for (size_t j = 0; j < i; ++j) {
+        child_src[j] = src[j];
+        child_src_stride[j] = src_stride[j];
+      }
+      child_src[i] = reinterpret_cast<char *>(&st);
+      child_src_stride[i] = 0;
+      for (size_t j = i; j < NArg; ++j) {
+        child_src[j + 1] = src[j];
+        child_src_stride[j + 1] = src_stride[j];
+      }
+
+      this->get_child()->strided(dst, dst_stride, child_src, child_src_stride, end);
+    }
   };
 
 } // namespace dynd::nd
