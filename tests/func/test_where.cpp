283bcc8f0094c1d95bcb2b93afce2cd51c7f1124
izaid <hi@irwinzaid.com>
2016-04-17 11:05:39 -0500

    Fix for MSVC

--- tests/func/test_where.cpp
+++ tests/func/test_where.cpp
@@ -19,7 +19,7 @@ using namespace dynd;
 TEST(Where, Untitled) {
   nd::callable f = nd::functional::where([](int x) { return x < 11; });
   nd::array res = f(nd::array{9, 34, 1, -7, 23});
-  EXPECT_ARRAY_EQ(nd::array({0L}), res(0));
-  EXPECT_ARRAY_EQ(nd::array({2L}), res(1));
-  EXPECT_ARRAY_EQ(nd::array({3L}), res(2));
+  EXPECT_ARRAY_EQ(nd::array({static_cast<intptr_t>(0)}), res(0));
+  EXPECT_ARRAY_EQ(nd::array({static_cast<intptr_t>(2)}), res(1));
+  EXPECT_ARRAY_EQ(nd::array({static_cast<intptr_t>(3)}), res(2));
 }
