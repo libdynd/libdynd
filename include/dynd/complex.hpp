7b92e9605ee5166c598af79a41f7629743d5b411
Ian Henriksen <insertinterestingnamehere@gmail.com>
2016-04-19 11:41:56 -0600

    Ran clang-format on complex.hpp.

--- include/dynd/complex.hpp
+++ include/dynd/complex.hpp
@@ -20,71 +20,60 @@ public:
   complex(const T &re = 0.0, const T &im = 0.0) : m_real(re), m_imag(im) {}
 
   template <typename U>
-  complex(const complex<U> &rhs)
-      : m_real(static_cast<T>(rhs.m_real)), m_imag(static_cast<T>(rhs.m_imag))
-  {
-  }
+  complex(const complex<U> &rhs) : m_real(static_cast<T>(rhs.m_real)), m_imag(static_cast<T>(rhs.m_imag)) {}
 
   complex(const std::complex<T> &rhs) : m_real(rhs.real()), m_imag(rhs.imag()) {}
 
   T real() const { return m_real; }
   T imag() const { return m_imag; }
 
-  complex<T> &operator=(const complex<T> &rhs)
-  {
+  complex<T> &operator=(const complex<T> &rhs) {
     m_real = rhs.m_real;
     m_imag = rhs.m_imag;
 
     return *this;
   }
 
-  complex<T> &operator+=(const complex<T> &rhs)
-  {
+  complex<T> &operator+=(const complex<T> &rhs) {
     m_real += rhs.m_real;
     m_imag += rhs.m_imag;
 
     return *this;
   }
 
-  complex<T> &operator+=(const T &rhs)
-  {
+  complex<T> &operator+=(const T &rhs) {
     m_real += rhs;
 
     return *this;
   }
 
-  complex<T> &operator-=(const complex<T> &rhs)
-  {
+  complex<T> &operator-=(const complex<T> &rhs) {
     m_real -= rhs.m_real;
     m_imag -= rhs.m_imag;
 
     return *this;
   }
 
-  complex<T> &operator-=(const T &rhs)
-  {
+  complex<T> &operator-=(const T &rhs) {
     m_real -= rhs;
 
     return *this;
   }
 
-  complex<T> operator*=(const complex<T> &rhs)
-  {
+  complex<T> operator*=(const complex<T> &rhs) {
     new (this) complex<T>(m_real * rhs.m_real - m_imag * rhs.m_imag, m_real * rhs.m_imag + rhs.m_real * m_imag);
 
     return *this;
   }
 
-  complex<T> operator*=(const T &rhs)
-  {
+  complex<T> operator*=(const T &rhs) {
     m_real *= rhs;
     m_imag *= rhs;
 
     return *this;
   }
 
-  complex<T> &operator/=(const complex<T> &rhs)
-  {
+  complex<T> &operator/=(const complex<T> &rhs) {
     T denom = rhs.m_real * rhs.m_real + rhs.m_imag * rhs.m_imag;
     new (this) complex<T>((m_real * rhs.m_real + m_imag * rhs.m_imag) / denom,
                           (rhs.m_real * m_imag - m_real * rhs.m_imag) / denom);
@@ -92,8 +81,7 @@ public:
     return *this;
   }
 
-  complex<T> &operator/=(const T &rhs)
-  {
+  complex<T> &operator/=(const T &rhs) {
     m_real /= rhs;
     m_imag /= rhs;
 
@@ -106,8 +94,7 @@ public:
 
   template <typename U,
             typename = typename std::enable_if<is_mixed_arithmetic<T, U>::value && !std::is_same<U, bool>::value>::type>
-  explicit operator U() const
-  {
+  explicit operator U() const {
     return static_cast<U>(m_real);
   }
 
@@ -115,8 +102,7 @@ public:
 };
 
 template <typename T>
-struct is_complex<complex<T>> : std::true_type {
-};
+struct is_complex<complex<T>> : std::true_type {};
 
 } // namespace dynd
 
@@ -218,8 +204,7 @@ struct common_type<dynd::complex<T>, dynd::complex<U>> {
 };
 
 template <typename T, typename U>
-struct common_type<T, dynd::complex<U>> : common_type<dynd::complex<U>, T> {
-};
+struct common_type<T, dynd::complex<U>> : common_type<dynd::complex<U>, T> {};
 
 } // namespace std
 
@@ -229,227 +214,194 @@ typedef complex<float32> complex64;
 typedef complex<float64> complex128;
 
 template <typename T, typename U>
-bool operator==(complex<T> lhs, complex<U> rhs)
-{
+bool operator==(complex<T> lhs, complex<U> rhs) {
   return (lhs.m_real == rhs.m_real) && (lhs.m_imag == rhs.m_imag);
 }
 
 template <typename T, typename U>
 
 typename std::enable_if<std::is_integral<U>::value || std::is_floating_point<U>::value, bool>::type
-operator==(complex<T> lhs, U rhs)
-{
+operator==(complex<T> lhs, U rhs) {
   return (lhs.m_real == rhs) && !lhs.m_imag;
 }
 
 template <typename T, typename U>
 
 typename std::enable_if<std::is_integral<T>::value || std::is_floating_point<T>::value, bool>::type
-operator==(T lhs, complex<U> rhs)
-{
+operator==(T lhs, complex<U> rhs) {
   return rhs == lhs;
 }
 
 template <typename T>
-bool operator==(complex<T> lhs, std::complex<T> rhs)
-{
+bool operator==(complex<T> lhs, std::complex<T> rhs) {
   return (lhs.m_real == rhs.real()) && (lhs.m_imag == rhs.imag());
 }
 
 template <typename T>
-bool operator==(std::complex<T> lhs, complex<T> rhs)
-{
+bool operator==(std::complex<T> lhs, complex<T> rhs) {
   return rhs == lhs;
 }
 
 template <typename T, typename U>
-bool operator!=(complex<T> lhs, complex<U> rhs)
-{
+bool operator!=(complex<T> lhs, complex<U> rhs) {
   return !(lhs == rhs);
 }
 
 template <typename T, typename U>
-bool operator!=(complex<T> lhs, U rhs)
-{
+bool operator!=(complex<T> lhs, U rhs) {
   return lhs.m_real == rhs && !lhs.m_imag;
 }
 
 template <typename T>
-complex<T> operator+(const complex<T> &rhs)
-{
+complex<T> operator+(const complex<T> &rhs) {
   return complex<T>(+rhs.m_real, +rhs.m_imag);
 }
 
 template <typename T>
-complex<T> operator-(const complex<T> &rhs)
-{
+complex<T> operator-(const complex<T> &rhs) {
   return complex<T>(-rhs.m_real, -rhs.m_imag);
 }
 
 template <typename T>
-complex<T> operator!(const complex<T> &rhs)
-{
+complex<T> operator!(const complex<T> &rhs) {
   return (!rhs.m_real && !rhs.m_imag);
 }
 
 template <typename T>
-complex<T> operator+(complex<T> lhs, complex<T> rhs)
-{
+complex<T> operator+(complex<T> lhs, complex<T> rhs) {
   return lhs += rhs;
 }
 
 template <typename T, typename U>
-complex<typename std::common_type<T, U>::type> operator+(complex<T> lhs, complex<U> rhs)
-{
+complex<typename std::common_type<T, U>::type> operator+(complex<T> lhs, complex<U> rhs) {
   return static_cast<complex<typename std::common_type<T, U>::type>>(lhs) +
          static_cast<complex<typename std::common_type<T, U>::type>>(rhs);
 }
 
 template <typename T>
-complex<T> operator+(complex<T> lhs, T rhs)
-{
+complex<T> operator+(complex<T> lhs, T rhs) {
   return lhs += rhs;
 }
 
 template <typename T, typename U>
 typename std::enable_if<std::is_integral<U>::value || std::is_floating_point<U>::value,
                         complex<typename std::common_type<T, U>::type>>::type
-operator+(complex<T> lhs, U rhs)
-{
+operator+(complex<T> lhs, U rhs) {
   return static_cast<complex<typename std::common_type<T, U>::type>>(lhs) +
          static_cast<typename std::common_type<T, U>::type>(rhs);
 }
 
 template <typename T>
-complex<T> operator+(T lhs, complex<T> rhs)
-{
+complex<T> operator+(T lhs, complex<T> rhs) {
   return complex<T>(lhs + rhs.m_real, rhs.m_imag);
 }
 
 template <typename T, typename U>
 typename std::enable_if<std::is_integral<T>::value || std::is_floating_point<T>::value,
                         complex<typename std::common_type<T, U>::type>>::type
-operator+(T lhs, complex<U> rhs)
-{
+operator+(T lhs, complex<U> rhs) {
   return static_cast<typename std::common_type<T, U>::type>(lhs) +
          static_cast<complex<typename std::common_type<T, U>::type>>(rhs);
 }
 
 template <typename T>
-complex<T> operator-(complex<T> lhs, complex<T> rhs)
-{
+complex<T> operator-(complex<T> lhs, complex<T> rhs) {
   return lhs -= rhs;
 }
 
 template <typename T, typename U>
-complex<typename std::common_type<T, U>::type> operator-(complex<T> lhs, complex<U> rhs)
-{
+complex<typename std::common_type<T, U>::type> operator-(complex<T> lhs, complex<U> rhs) {
   return static_cast<complex<typename std::common_type<T, U>::type>>(lhs) -
          static_cast<complex<typename std::common_type<T, U>::type>>(rhs);
 }
 
 template <typename T>
-complex<T> operator-(complex<T> lhs, T rhs)
-{
+complex<T> operator-(complex<T> lhs, T rhs) {
   return lhs -= rhs;
 }
 
 template <typename T, typename U>
 typename std::enable_if<std::is_integral<U>::value || std::is_floating_point<U>::value,
                         complex<typename std::common_type<T, U>::type>>::type
-operator-(complex<T> lhs, U rhs)
-{
+operator-(complex<T> lhs, U rhs) {
   return static_cast<complex<typename std::common_type<T, U>::type>>(lhs) -
          static_cast<typename std::common_type<T, U>::type>(rhs);
 }
 
 template <typename T>
-complex<T> operator-(T lhs, complex<T> rhs)
-{
+complex<T> operator-(T lhs, complex<T> rhs) {
   return complex<T>(lhs - rhs.m_real, -rhs.m_imag);
 }
 
 template <typename T, typename U>
 typename std::enable_if<std::is_integral<T>::value || std::is_floating_point<T>::value,
                         complex<typename std::common_type<T, U>::type>>::type
-operator-(T lhs, complex<U> rhs)
-{
+operator-(T lhs, complex<U> rhs) {
   return static_cast<typename std::common_type<T, U>::type>(lhs) -
          static_cast<complex<typename std::common_type<T, U>::type>>(rhs);
 }
 
 template <typename T>
-complex<T> operator*(complex<T> lhs, complex<T> rhs)
-{
+complex<T> operator*(complex<T> lhs, complex<T> rhs) {
   return lhs *= rhs;
 }
 
 template <typename T, typename U>
-complex<typename std::common_type<T, U>::type> operator*(complex<T> lhs, complex<U> rhs)
-{
+complex<typename std::common_type<T, U>::type> operator*(complex<T> lhs, complex<U> rhs) {
   return static_cast<complex<typename std::common_type<T, U>::type>>(lhs) *
          static_cast<complex<typename std::common_type<T, U>::type>>(rhs);
 }
 
 template <typename T>
-complex<T> operator*(complex<T> lhs, T rhs)
-{
+complex<T> operator*(complex<T> lhs, T rhs) {
   return lhs *= rhs;
 }
 
 template <typename T, typename U>
 typename std::enable_if<std::is_integral<U>::value || std::is_floating_point<U>::value,
                         complex<typename std::common_type<T, U>::type>>::type
-operator*(complex<T> lhs, U rhs)
-{
+operator*(complex<T> lhs, U rhs) {
   return static_cast<complex<typename std::common_type<T, U>::type>>(lhs) *
          static_cast<typename std::common_type<T, U>::type>(rhs);
 }
 
 template <typename T>
-complex<T> operator*(T lhs, complex<T> rhs)
-{
+complex<T> operator*(T lhs, complex<T> rhs) {
   return complex<T>(lhs * rhs.m_real, lhs * rhs.m_imag);
 }
 
 template <typename T, typename U>
 typename std::enable_if<std::is_integral<T>::value || std::is_floating_point<T>::value,
                         complex<typename std::common_type<T, U>::type>>::type
-operator*(T lhs, complex<U> rhs)
-{
+operator*(T lhs, complex<U> rhs) {
   return static_cast<typename std::common_type<T, U>::type>(lhs) *
          static_cast<complex<typename std::common_type<T, U>::type>>(rhs);
 }
 
 template <typename T>
-complex<T> operator/(complex<T> lhs, complex<T> rhs)
-{
+complex<T> operator/(complex<T> lhs, complex<T> rhs) {
   return lhs /= rhs;
 }
 
 template <typename T, typename U>
-complex<typename std::common_type<T, U>::type> operator/(complex<T> lhs, complex<U> rhs)
-{
+complex<typename std::common_type<T, U>::type> operator/(complex<T> lhs, complex<U> rhs) {
   return static_cast<complex<typename std::common_type<T, U>::type>>(lhs) /
          static_cast<complex<typename std::common_type<T, U>::type>>(rhs);
 }
 
 template <typename T>
-complex<T> operator/(complex<T> lhs, T rhs)
-{
+complex<T> operator/(complex<T> lhs, T rhs) {
   return lhs /= rhs;
 }
 
 template <typename T>
-complex<T> operator/(T lhs, complex<T> rhs)
-{
+complex<T> operator/(T lhs, complex<T> rhs) {
   T denom = rhs.m_real * rhs.m_real + rhs.m_imag * rhs.m_imag;
   return complex<T>(lhs * rhs.m_real / denom, -lhs * rhs.m_imag / denom);
 }
 
 template <typename T>
-std::ostream &operator<<(std::ostream &out, const complex<T> &val)
-{
+std::ostream &operator<<(std::ostream &out, const complex<T> &val) {
   return (out << "(" << val.m_real << " + " << val.m_imag << "j)");
 }
 
@@ -457,20 +409,17 @@ template <typename T>
 complex<T> _i(); // complex<T>(0, 1)
 
 template <>
-inline complex<float> _i<float>()
-{
+inline complex<float> _i<float>() {
   return complex<float>(0.0f, 1.0f);
 }
 
 template <>
-inline complex<double> _i<double>()
-{
+inline complex<double> _i<double>() {
   return complex<double>(0.0, 1.0);
 }
 
 template <typename T>
-complex<T> conj(const complex<T> &z)
-{
+complex<T> conj(const complex<T> &z) {
   return complex<T>(z.real(), -z.imag());
 }
 
