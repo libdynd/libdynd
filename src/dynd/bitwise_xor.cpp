0d77c4736497a51c6683d00b6b5d06d1c59536cd
Ian Henriksen <insertinterestingnamehere@gmail.com>
2016-04-21 16:36:23 -0600

    Add bitwise xor operator and callable.

---
+++ src/dynd/bitwise_xor.cpp
@@ -0,0 +1,10 @@
+//
+// Copyright (C) 2011-16 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <dynd/binary_arithmetic.hpp>
+#include <dynd/callables/bitwise_xor_callable.hpp>
+
+DYND_API nd::callable nd::bitwise_xor =
+    make_binary_arithmetic<nd::bitwise_xor_callable, dynd::detail::isdef_bitwise_xor, integral_ids>();
