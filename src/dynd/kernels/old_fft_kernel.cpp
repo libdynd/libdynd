a01a31baf86b7d0b2c06d5279b876b051912414e
izaid <hi@irwinzaid.com>
2016-04-22 20:38:58 +0100

    Added the old FFT files

---
+++ src/dynd/kernels/old_fft_kernel.cpp
@@ -0,0 +1,34 @@
+//
+// Copyright (C) 2011-16 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <dynd/callable.hpp>
+#include <dynd/kernels/old_fft_kernel.hpp>
+
+using namespace std;
+using namespace dynd;
+
+#ifdef DYND_FFTW
+
+::fftwf_plan nd::detail::fftw_plan_guru_dft(int rank, const fftw_iodim *dims,
+                                        int howmany_rank,
+                                        const fftw_iodim *howmany_dims,
+                                        fftwf_complex *in, fftwf_complex *out,
+                                        int sign, unsigned flags)
+{
+  return ::fftwf_plan_guru_dft(rank, dims, howmany_rank, howmany_dims, in, out,
+                               sign, flags);
+}
+
+::fftw_plan nd::detail::fftw_plan_guru_dft(int rank, const fftw_iodim *dims,
+                                       int howmany_rank,
+                                       const fftw_iodim *howmany_dims,
+                                       fftw_complex *in, fftw_complex *out,
+                                       int sign, unsigned flags)
+{
+  return ::fftw_plan_guru_dft(rank, dims, howmany_rank, howmany_dims, in, out,
+                              sign, flags);
+}
+
+#endif
---
+++ src/dynd/old_fft.cpp
@@ -0,0 +1,87 @@
+//
+// Copyright (C) 2011-16 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <dynd/old_fft.hpp>
+#include <dynd/index.hpp>
+#include <dynd/functional.hpp>
+#include <dynd/callables/old_fft_callable.hpp>
+
+using namespace std;
+using namespace dynd;
+
+#ifdef DYND_FFTW
+
+namespace {
+
+nd::callable make_fft() {
+  std::vector<nd::callable> children;
+
+  typedef nd::fftw_callable<fftw_complex, fftw_complex, FFTW_FORWARD> CKT;
+  children.push_back(nd::make_callable<CKT>());
+
+  return children[0];
+  /*
+    return functional::multidispatch(
+        ndt::type("(M[Fixed**N * complex[float64]], shape: ?N * int64, axes: "
+                  "?Fixed * int64, "
+                  "flags: ?int32) -> M[Fixed**N * complex[float64]]"),
+        children, {"N"});
+  */
+}
+
+nd::callable make_ifft() {
+  std::vector<nd::callable> children;
+
+  children.push_back(nd::make_callable<nd::fftw_callable<fftw_complex, fftw_complex, FFTW_BACKWARD>>());
+
+  return children[0];
+  /*
+    return functional::multidispatch(
+        ndt::type("(M[Fixed**N * complex[float64]], shape: ?N * int64, "
+                  "axes: ?Fixed * int64, "
+                  "flags: ?int32) -> M[Fixed**N * complex[float64]]"),
+        children, {"N"});
+  */
+}
+
+nd::callable make_rfft() { return nd::make_callable<nd::fftw_callable<fftw_complex, double>>(); }
+
+nd::callable make_irfft() { return nd::make_callable<nd::fftw_callable<double, fftw_complex>>(); }
+
+} // unnamed namespace
+
+DYND_API nd::callable nd::fft = make_fft();
+DYND_API nd::callable nd::ifft = make_ifft();
+DYND_API nd::callable nd::rfft = make_rfft();
+DYND_API nd::callable nd::irfft = make_irfft();
+
+#endif
+
+nd::array nd::fftshift(const nd::array &x) {
+  nd::array y = x;
+  for (intptr_t i = 0; i < x.get_ndim(); ++i) {
+    intptr_t p = y.get_dim_size();
+    intptr_t q = (p + 1) / 2;
+    y = take(y, nd::concatenate(nd::old_range(q, p), nd::old_range(q)));
+    y = y.rotate();
+  }
+  return y;
+}
+
+nd::array nd::ifftshift(const nd::array &x) {
+  nd::array y = x;
+  for (intptr_t i = 0; i < x.get_ndim(); ++i) {
+    intptr_t p = y.get_dim_size();
+    intptr_t q = p - (p + 1) / 2;
+    y = take(y, nd::concatenate(nd::old_range(q, p), nd::old_range(q)));
+    y = y.rotate();
+  }
+  return y;
+}
+
+nd::array nd::fftspace(intptr_t count, double step) {
+  // Todo: When casting is fixed, change the ranges below to integer versions
+  return nd::concatenate(nd::old_range((count - 1) / 2 + 1.0), nd::old_range(-count / 2 + 0.0, 0.0)) / (count * step);
+}
