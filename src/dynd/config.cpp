20085321be2db4d9e701ea7d103756568edc2871
izaid <hi@irwinzaid.com>
2016-05-03 23:01:49 +0100

    Added loading of plugins

---
+++ src/dynd/config.cpp
@@ -0,0 +1,35 @@
+//
+// Copyright (C) 2011-16 DyND Developers
+// BSD 2-Clause License, see LICENSE.txt
+//
+
+#include <string>
+#include <regex>
+
+#include <dlfcn.h>
+
+#include <dynd/config.hpp>
+
+using namespace std;
+using namespace dynd;
+
+void dynd::load(const std::string &path) {
+  void *lib = dlopen(path.c_str(), RTLD_LAZY);
+  if (lib == nullptr) {
+    throw runtime_error("could not load plugin");
+  }
+
+  std::string name = path.substr(0, path.find("."));
+  name.erase(0, 3);
+  name += "_init";
+
+  void *sym = dlsym(lib, name.c_str());
+  if (sym == nullptr) {
+    throw runtime_error("could not load init function in plugin");
+  }
+
+  void (*init)() = reinterpret_cast<void (*)()>(sym);
+  init();
+
+  dlclose(lib);
+}
