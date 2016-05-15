f5dcda793ebf811d85f29f25b3ec529d8f00142a
izaid <hi@irwinzaid.com>
2016-05-15 18:58:24 +0100

    Commented out pointer and dereference kernels

--- include/dynd/kernels/dereference_kernel.hpp
+++ include/dynd/kernels/dereference_kernel.hpp
@@ -11,17 +11,21 @@ namespace dynd {
 namespace nd {
 
   struct dereference_kernel : base_strided_kernel<dereference_kernel, 1> {
-    void call(const array *dst, const array *src) {
-      const ndt::type &dst_tp = dst->get_type();
+    void call(const array *DYND_UNUSED(dst), const array *DYND_UNUSED(src)) {
+      throw std::runtime_error("dereference_kernel is not implemented");
+      /*
+            const ndt::type &dst_tp = dst->get_type();
 
-      *const_cast<array *>(dst) =
-          make_array(dst_tp, *reinterpret_cast<char **>(src[0]->get_data()) +
-                                 reinterpret_cast<const pointer_type_arrmeta *>(src[0]->metadata())->offset,
-                     src[0].get_data_memblock(), read_access_flag | write_access_flag);
-      if (!dst_tp.is_builtin()) {
-        dst_tp.extended()->arrmeta_copy_construct((*dst)->metadata(), src[0]->metadata() + sizeof(pointer_type_arrmeta),
-                                                  src[0]);
-      }
+            *const_cast<array *>(dst) =
+                make_array(dst_tp, *reinterpret_cast<char **>(src[0]->get_data()) +
+                                       reinterpret_cast<const pointer_type_arrmeta *>(src[0]->metadata())->offset,
+                           src[0].get_data_memblock(), read_access_flag | write_access_flag);
+            if (!dst_tp.is_builtin()) {
+              dst_tp.extended()->arrmeta_copy_construct((*dst)->metadata(), src[0]->metadata() +
+         sizeof(pointer_type_arrmeta),
+                                                        src[0]);
+            }
+      */
     }
   };
 
