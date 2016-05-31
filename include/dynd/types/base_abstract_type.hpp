9b8f492dc5e0d203aa4f8ecc7903728bb51e0e00
izaid <hi@irwinzaid.com>
2016-05-31 12:06:19 +0100

    Fix for MSVC

--- include/dynd/types/base_abstract_type.hpp
+++ include/dynd/types/base_abstract_type.hpp
@@ -10,7 +10,7 @@
 namespace dynd {
 namespace ndt {
 
-  class base_abstract_type : public base_type {
+  class DYNDT_API base_abstract_type : public base_type {
   public:
     base_abstract_type(type_id_t id, const type &base_tp, uint32_t flags, size_t ndim, size_t fixed_ndim)
         : base_type(id, base_tp, 0, 1, flags | type_flag_symbolic, 0, ndim, fixed_ndim) {}
