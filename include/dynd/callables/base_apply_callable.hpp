dce8f9d937d9b0a965bd26bf3d24f0afdf5dffe0
izaid <hi@irwinzaid.com>
2016-06-15 00:36:57 +0100

    Brought callable type creation into base_apply_callable

--- include/dynd/callables/base_apply_callable.hpp
+++ include/dynd/callables/base_apply_callable.hpp
@@ -21,7 +21,9 @@ namespace nd {
     public:
       template <typename... S>
       base_apply_callable(S &&... names)
-          : base_callable(ndt::make_type<ReturnType(ArgTypes..., KwdTypes...)>(std::forward<S>(names)...)) {}
+          : base_callable(
+                ndt::make_type<ndt::callable_type>(ndt::make_type<ReturnType>(), {ndt::make_type<ArgTypes>()...},
+                                                   {{ndt::make_type<KwdTypes>(), std::forward<S>(names)}...})) {}
 
       ndt::type resolve_return_type(const ndt::type &dst_tp, size_t DYND_UNUSED(nsrc),
                                     const ndt::type *DYND_UNUSED(src_tp)) {
@@ -35,7 +37,9 @@ namespace nd {
     public:
       template <typename... S>
       base_apply_callable(S &&... names)
-          : base_callable(ndt::make_type<ReturnType(ArgTypes..., KwdTypes...)>(std::forward<S>(names)...)) {}
+          : base_callable(
+                ndt::make_type<ndt::callable_type>(ndt::make_type<ReturnType>(), {ndt::make_type<ArgTypes>()...},
+                                                   {{ndt::make_type<KwdTypes>(), std::forward<S>(names)}...})) {}
 
       ndt::type resolve_return_type(const ndt::type &DYND_UNUSED(dst_tp), size_t nsrc, const ndt::type *src_tp) {
         return Resolve(nsrc, src_tp);
