e925274c6ee517bfc66d4bb11e8fa9d22893569c
izaid <hi@irwinzaid.com>
2016-07-17 09:46:50 +0100

    Removed some debugging code

--- tests/test_mkl.cpp
+++ tests/test_mkl.cpp
@@ -21,12 +21,7 @@ using namespace dynd;
 namespace {
 
 template <typename T>
-class FFT : public ::testing::Test {
-protected:
-  //  virtual void SetUp() { std::cout << "setting up" << std::endl; }
-
-  virtual void TearDown() { mkl_free_buffers(); }
-};
+class FFT : public ::testing::Test {};
 
 template <typename T>
 class Conv : public ::testing::Test {};
@@ -91,22 +86,20 @@ TYPED_TEST_P(FFT, Zeros) {
 TYPED_TEST_CASE_P(Conv);
 
 TYPED_TEST_P(Conv, Linear) {
-  /*
-    const ndt::type &arg0_tp = ndt::make_type<TypeParam>();
-    const ndt::type &arg1_tp = ndt::make_type<TypeParam>();
+  const ndt::type &arg0_tp = ndt::make_type<TypeParam>();
+  const ndt::type &arg1_tp = ndt::make_type<TypeParam>();
 
-    nd::array x0 = nd::random::uniform({}, {{"dst_tp", arg0_tp}});
-    nd::array x1 = nd::random::uniform({}, {{"dst_tp", arg0_tp}});
-    nd::array x = x0 + x1;
+  nd::array x0 = nd::random::uniform({}, {{"dst_tp", arg0_tp}});
+  nd::array x1 = nd::random::uniform({}, {{"dst_tp", arg0_tp}});
+  nd::array x = x0 + x1;
 
-    nd::array h = nd::random::uniform({}, {{"dst_tp", arg1_tp}});
+  nd::array h = nd::random::uniform({}, {{"dst_tp", arg1_tp}});
 
-    nd::array y0 = nd::mkl::conv(x0, h);
-    nd::array y1 = nd::mkl::conv(x1, h);
-    nd::array y = nd::mkl::conv(x, h);
+  nd::array y0 = nd::mkl::conv(x0, h);
+  nd::array y1 = nd::mkl::conv(x1, h);
+  nd::array y = nd::mkl::conv(x, h);
 
-    EXPECT_ARRAY_NEAR(y0 + y1, y);
-  */
+  EXPECT_ARRAY_NEAR(y0 + y1, y);
 }
 
 /*
