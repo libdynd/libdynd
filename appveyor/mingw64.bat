c488851d6c9495ae6e9ecd6e085c5641f229c7c8
Ian Henriksen <insertinterestingnamehere@gmail.com>
2018-03-10 13:54:36 -0600

    Make CMake ignore sh.exe when building with MinGW-w64.

--- appveyor/mingw64.bat
+++ appveyor/mingw64.bat
@@ -1,7 +1,9 @@
 SET PATH=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\bin;%PATH%
 mkdir build
 pushd build
-cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_CXX_FLAGS="-Wno-error" -G "MinGW Makefiles" ..
+:: -DCMAKE_SH="CMAKE_SH-NOTFOUND" makes CMake ignore the fact that sh.exe
+:: is on the path of the appveyor build workers.
+cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_CXX_FLAGS="-Wno-error" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -G "MinGW Makefiles" ..
 mingw32-make -j4
 "tests/test_libdynd.exe"
 popd
