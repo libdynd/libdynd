b146389de3c413aac28a40cac816654b0ec19d1d
Ian Henriksen <insertinterestingnamehere@gmail.com>
2018-03-10 14:16:07 -0600

    Find appveyor mingw-w64.

--- appveyor/mingw64.bat
+++ appveyor/mingw64.bat
@@ -1,5 +1,6 @@
 @ECHO ON
-SET MINGW_PREFIX=C:/mingw-w64/x86_64-7.2.0-posix-seh-rt_v5-rev1/bin
+SET PATH=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
+SET MINGW_PREFIX=C:/mingw-w64/x86_64-7.2.0-posix-seh-rt_v5-rev1/mingw64/bin
 
 mkdir build
 pushd build
@@ -8,7 +9,7 @@ pushd build
 :: is on the path of the appveyor build workers.
 cmake ^
     -DCMAKE_C_COMPILER=%MINGW_PREFIX%/gcc.exe ^
-    -DCMAKE_CXX_COMPILER=%MINGW_PREFIX%/gcc.exe ^
+    -DCMAKE_CXX_COMPILER=%MINGW_PREFIX%/g++.exe ^
     -DCMAKE_VERBOSE_MAKEFILE=ON ^
     -DCMAKE_CXX_FLAGS="-Wno-error" ^
     -DCMAKE_SH="CMAKE_SH-NOTFOUND" ^
