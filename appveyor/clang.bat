0dd2ffd78bfa03b29259b858a2389a96b00e3c81
Ian Henriksen <insertinterestingnamehere@gmail.com>
2018-03-10 21:34:59 -0600

    Fix finding ninja for clang build. Also fix clang finding MSVC 2017.

--- appveyor/clang.bat
+++ appveyor/clang.bat
@@ -4,12 +4,13 @@ SET MINICONDA_ROOT=C:\Miniconda3-x64
 SET PATH=%MINICONDA_ROOT%;%MINICONDA_ROOT%\Scripts;%MINICONDA_ROOT%\Library\bin;%PATH%
 
 conda config --set always_yes yes || exit /b 1
-conda update --all || exit /b 1
 
 conda install -c conda-forge clangdev=%CLANG_VERSION% ninja || exit /b 1
 
 mkdir build
 pushd build
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 cmake ^
     -DCMAKE_VERBOSE_MAKEFILE=ON ^
