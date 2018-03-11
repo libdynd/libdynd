4f8a2b1f814aa548b4f11056e2b7a39a3ceb6b24
Ian Henriksen <insertinterestingnamehere@gmail.com>
2018-03-10 23:41:11 -0600

    Try some parallelism in MSVC based builds.

--- appveyor/msvc_2017.bat
+++ appveyor/msvc_2017.bat
@@ -3,7 +3,7 @@
 mkdir build
 pushd build
 
-cmake -G "Visual Studio 14 Win64" -DCMAKE_BUILD_TYPE=%MSVC_BUILD_TYPE% .. || exit /b 1
+cmake -G "Visual Studio 14 Win64" -DCMAKE_BUILD_TYPE=%MSVC_BUILD_TYPE% -DCMAKE_CXX_FLAGS="-MP3" .. || exit /b 1
 
 cmake --build . --config %MSVC_BUILD_TYPE% || exit /b 1
 
