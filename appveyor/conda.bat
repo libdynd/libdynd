80f9ced2f1a067f54e22f8581569f718b6ebcfe4
Ian Henriksen <insertinterestingnamehere@gmail.com>
2018-05-12 23:09:08 -0500

    Switch back to downloading and installing miniconda in the Appveyor builds that require conda. For some reason installing things via conda is broken in the Appveyor-provided miniconda install.

--- appveyor/conda.bat
+++ appveyor/conda.bat
@@ -1,11 +1,10 @@
 @ECHO ON
 
-IF "%PYTHON_ARCH%"=="x86_64" SET MINICONDA_ROOT=C:\Miniconda3-x64
-IF "%PYTHON_ARCH%"=="x86" SET MINICONDA_ROOT=C:\Miniconda3
-SET PATH=%MINICONDA_ROOT%;%MINICONDA_ROOT%\Scripts;%MINICONDA_ROOT%\Library\bin;%PATH%
+Powershell -Command "Start-FileDownload \"https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-$env:PYTHON_ARCH.exe\" C:\Miniconda.exe"
+C:\Miniconda.exe /S /D=C:\Py
+SET PATH=C:\Py;C:\Py\Scripts;C:\Py\Library\bin;%PATH%
 
 conda config --set always_yes yes || exit /b 1
-conda update --all || exit /b 1
 
 conda install conda-build || exit /b 1
 if not defined APPVEYOR_PULL_REQUEST_NUMBER conda install anaconda-client || exit /b 1
