bd53ed257a1aebd8dd47d54eb2bbf5edea8478a8
Ian Henriksen <insertinterestingnamehere@gmail.com>
2020-07-28 17:27:17 -0500

    Add base pointer and access flags to the array metadata header. Also much more clarifying comments.

--- include/dynd/abi/array.h
+++ include/dynd/abi/array.h
@@ -16,9 +16,49 @@ struct dynd_type;
 // to manage type-specific destructors.
 #define dynd_array_header DYND_ABI(array_header)
 typedef struct {
+  // TODO: would it be better to just full-on store
+  // an additional resource pointer here to avoid
+  // having to have a separate resource release
+  // function specific to the array type for each
+  // allocator used? For now, assume there are many
+  // more arrays than allocators.
   dynd_allocated allocated;
-  void *base;
+  // The array metadata object this one was generated
+  // from (if any) via slicing or some similar operation.
+  // If no such array exists, this entry is null.
+  // If an array header has no base_array and it contains
+  // types with nontrivial destruction routines,
+  // it calls the corresponding destructors when
+  // it is destroyed. If it has a base_array, it instead
+  // decrefs its base array.
+  void *base_array;
+  // Access rights associated with this metadata header.
+  // For now this is primarily reserved space until
+  // access flags have a settled design or until something
+  // more sophisticated can be done.
+  dynd_size_t access;
   dynd_type *type;
+  // Flags representing read/write access.
+  // Only a couple of bits of this space are used,
+  // but alignment constraints mean that a full size_t
+  // of space will be used anyway, so there's no reason
+  // to use a smaller int.
+  // TODO: Given that the base_array pointer
+  // will always be an address aligned
+  // to at least 4 bytes, can we use the two bits
+  dynd_size_t access;
+  // Base pointer used for all strided accesses
+  // into the data described by this metadata header.
+  // All accesses are computed as offsets from this
+  // pointer.
+  // In theory this could be moved into the
+  // type-specific metadata and then composing container
+  // types could be done relative some interface that
+  // they all agree on, but the benefits of doing so
+  // seem very small relative to a significant increase
+  // in the corresponding complexity for the code managing
+  // what gets stored in the array-specific metadata.
+  void *base;
 } dynd_array_header;
 
 #define dynd_array DYND_ABI(array)
@@ -29,6 +69,8 @@ typedef struct {
   // by the type. Note: the type-specific deallocation
   // routine will only be called if the base pointer
   // is null.
+  // NOTE: the resource here is also in-charge
+  // of decrefing the corresponding type.
   // TODO: the corresponding C++ interface
   // should automatically tie the lifetime of
   // this reference to the lifetime of this
