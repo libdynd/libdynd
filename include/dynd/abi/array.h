e69d16fa30a2f30279a63798b5aed52fd9860ddd
Ian Henriksen <insertinterestingnamehere@gmail.com>
2020-07-28 13:33:45 -0500

    Switch to typedef struct idiom for struct difinitions.

--- include/dynd/abi/array.h
+++ include/dynd/abi/array.h
@@ -15,14 +15,14 @@ struct dynd_type;
 // but brings in the additional metadata needed
 // to manage type-specific destructors.
 #define dynd_array_header DYND_ABI(array_header)
-struct dynd_array_header {
+typedef struct {
   dynd_allocated allocated;
   void *base;
   dynd_type *type;
-};
+} dynd_array_header;
 
 #define dynd_array DYND_ABI(array)
-struct dynd_array {
+typedef struct {
   // This resource encapsulates the allocation
   // of this particular buffer as well as
   // any additional custom deallocation required
@@ -35,7 +35,7 @@ struct dynd_array {
   // metadata buffer.
   dynd_refcounted refcount;
   dynd_array_header header;
-};
+} dynd_array;
 
 // Note: the arrmeta specified by this type
 // should be laid out in memory immediately
@@ -51,9 +51,9 @@ struct dynd_array {
 // Same as previous, but without reference counting.
 // The layout is kept intentionally compatible.
 #define dynd_array_ref DYND_ABI(array)
-struct dynd_array_ref {
+typedef struct {
   dynd_resource resource;
   dynd_array_header header;
-};
+} dynd_array_ref;
 
 #endif // !defined(DYND_ABI_ARRAY_H)
