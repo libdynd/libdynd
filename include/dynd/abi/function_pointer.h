af02c7b1c4cd1b7d9aeff776e787b9396885c685
Ian Henriksen <insertinterestingnamehere@gmail.com>
2020-08-04 17:14:48 -0500

    Change noexcept typedef idiom into a macro.

--- include/dynd/abi/function_pointer.h
+++ include/dynd/abi/function_pointer.h
@@ -1,6 +1,14 @@
 #if !defined(DYND_ABI_GENERIC_FUNCTION_POINTER_H)
 #define DYND_ABI_GENERIC_FUNCTION_POINTER_H
 
+#if defined(__cplusplus) && __cplusplus >= 201103L && __cplusplus < 201703L
+// Needed for trick to get noexcept into function pointer typedef
+// in C++11 and C++14.
+#include <type_traits>
+#endif // defined(__cplusplus) && __cplusplus >= 201103L && __cplusplus < 201703L
+
+#include "dynd/abi/noexcept.h"
+
 // A function pointer type that's safe to cast
 // other function pointers to/from.
 // Since this struct type is never defined,
@@ -11,5 +19,23 @@
 // more of a useful C idiom that we need.
 struct dynd_abi_never_defined;
 typedef void (*dynd_generic_func_ptr)(dynd_abi_never_defined);
+
+// Defining a function pointer typedef to be noexcept
+// in language standards that permit that
+#if defined(__cplusplus) && __cplusplus >= 201703L
+#define DYND_ABI_NOEXCEPT_FUNC(name, ret_type, ...) typedef ret_type (*name)(__VA_ARGS__) dynd_noexcept;
+#elif defined(__cplusplus) && __cplusplus >= 201103L
+// C++ equivalent of C typedef with the noexcept specifier added.
+// Note: this isn't possible with a plain typedef.
+// Use the technique from: https://stackoverflow.com/a/53674998
+// Noexcept is a full part of the type system in c++17 and later,
+// but this trickery is needed to get the right behavior
+// with earlier versions that still support noexcept.
+#define DYND_ABI_NOEXCEPT_FUNC(name, ret_type, ...) using name = decltype(std::declval<ret_type(*)(__VA_ARGS__) dynd_noexcept>());
+#else
+// No way to include throw() in the function pointer type,
+// so just use the equivalent c typedef.
+#define DYND_ABI_NOEXCEPT_FUNC(name, ret_type, ...) typedef ret_type (*name)(__VA_ARGS__);
+#endif
 
 #endif // !defined(DYND_ABI_GENERIC_FUNCTION_POINTER_H)
--- include/dynd/abi/resource.h
+++ include/dynd/abi/resource.h
@@ -1,51 +1,16 @@
 #if !defined(DYND_ABI_RESOURCE_H)
 #define DYND_ABI_RESOURCE_H
 
-#if defined(__cplusplus) && __cplusplus >= 201103L && __cplusplus < 201703L
-// Needed for function pointer noexcept typedef trick.
-#include <type_traits>
-#endif
-
+#include "dynd/abi/function_pointer.h"
 #include "dynd/abi/version.h"
-
-#if defined(__cplusplus)
-extern "C" {
-#endif // defined(__cplusplus)
 
 // Some abstract resource (usually an allocated buffer)
 // that needs to be destroyed when it is no longer referred to,
 // but which does not track its reference count internally.
-#define dynd_resource DYND_ABI(resource)
-struct dynd_resource;
+#define dynd_resource_impl DYND_ABI(resource_impl);
+struct dynd_resource_impl;
 
-// Call the function pointer provided by the resource and
-// pass it to the resource itself to release the resource.
-#define dynd_resource_release DYND_ABI(resource_release)
-#if defined(__cplusplus) && __cplusplus >= 201103L && __cplusplus < 201703L
-}
-
-#if __cplusplus < 201103L
-// No way to include throw() in the function pointer type
-// with old versions of C++.
-typedef void (*dynd_resource_release)(dynd_resource*);
-#elif __cplusplus < 201703L
-// C++ equivalent of C typedef with the noexcept specifier added.
-// Note: this isn't possible with a plain typedef.
-// Use the technique from: https://stackoverflow.com/a/53674998
-// Noexcept is a full part of the type system in c++17 and later,
-// but this trickery is needed to get the right behavior
-// with earlier versions that still support noexcept.
-using dynd_resource_release = decltype(std::declval<void (*)(dynd_resource*) dynd_noexcept>());
-#elif __cplusplus >= 201703L
-typedef void (*dynd_resource_release)(dynd_resource*) dynd_noexcept;
-#endif
-
-extern "C" {
-#else
-
-typedef void (*dynd_resource_release)(dynd_resource*);
-
-#endif // defined(__cplusplus)
+DYND_ABI_NOEXCEPT_FUNC(dynd_resource_release, void, dynd_resource_impl*)
 
 // Note: The intent is that a given resource
 // be able to store whatever additional metadata it needs
@@ -54,12 +19,11 @@ typedef void (*dynd_resource_release)(dynd_resource*);
 // passed the address of the release member here and
 // any corresponding metadata can be read using offsets
 // from that base address.
-struct dynd_resource {
+struct dynd_resource_impl {
   dynd_resource_release release;
 };
 
-#if defined(__cplusplus)
-}
-#endif // defined(__cplusplus)
+#define dynd_resource DYND_ABI(resource)
+typedef dynd_resource_impl dynd_resource;
 
 #endif // !defined(DYND_ABI_RESOURCE_H)
